2ffc06cb20fb65169733124ae8a4655c7c7cc4d2
Bert Hubert <bert.hubert@netherlabs.nl>
2002-11-27 15:23:16 +0000

    pgmysqlbackend added
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@6 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ modules/gmysqlbackend/smysql.hh
@@ -0,0 +1,31 @@
+/* Copyright 2001 Netherlabs BV, bert.hubert@netherlabs.nl. See LICENSE 
+   for more information.
+   $Id: smysql.hh,v 1.1 2002/11/27 15:23:16 ahu Exp $  */
+#ifndef SMYSQL_HH
+#define SMYSQL_HH
+
+#include <mysql/mysql.h>
+#include "ssql.hh"
+
+class SMySQL : public SSql
+{
+public:
+  SMySQL(const string &database, const string &host="", 
+	 const string &msocket="",const string &user="", 
+	 const string &password="");
+
+  ~SMySQL();
+  
+  SSqlException sPerrorException(const string &reason);
+  int doQuery(const string &query, result_t &result);
+  int doQuery(const string &query);
+  bool getRow(row_t &row);
+  string escape(const string &str);    
+  void setLog(bool state);
+private:
+  MYSQL d_db;
+  MYSQL_RES *d_rres;
+  static bool s_dolog;
+};
+      
+#endif /* SSMYSQL_HH */
