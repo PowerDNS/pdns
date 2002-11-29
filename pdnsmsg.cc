a6cf3b147ce549e4d5b31a8fa635ae7095d68fd1
Bert Hubert <bert.hubert@netherlabs.nl>
2002-11-29 22:15:03 +0000

    voor wimpie
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@32 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ pdnsmsg.cc
@@ -0,0 +1,28 @@
+/*
+    PowerDNS Versatile Database Driven Nameserver
+    Copyright (C) 2002  PowerDNS.COM BV
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#define WINDOWS_LEAN_AND_MEAN
+#include <windows.h>
+
+
+// Message dll used for logging.
+BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved )
+{
+  return TRUE;
+}
