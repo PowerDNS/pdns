f5e9ab7c7420fd42cf982adf77200e1951f4e0ac
Bert Hubert <bert.hubert@netherlabs.nl>
2003-01-13 23:02:11 +0000

    hiero
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@131 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ syncres.hh
@@ -0,0 +1,54 @@
+#ifndef PDNS_SYNCRES_HH
+#define PDNS_SYNCRES_HH
+#include <string>
+#include "dns.hh"
+#include "qtype.hh"
+#include <vector>
+#include <set>
+#include <map>
+#include "lwres.hh"
+
+/* external functions, opaque to us */
+void replaceCache(const string &qname, const QType &qt, const set<DNSResourceRecord>& content);
+int getCache(const string &qname, const QType& qt, set<DNSResourceRecord>* res=0);
+
+
+template<class MultiPlexor=LWRes> class SyncRes
+{
+public:
+  int beginResolve(const string &qname, const QType &qtype, vector<DNSResourceRecord>&ret);
+  
+private:
+  struct GetBestNSAnswer;
+  int doResolveAt(set<string> nameservers, string auth, const string &qname, const QType &qtype, vector<DNSResourceRecord>&ret,
+		  int depth, set<GetBestNSAnswer>&beenthere);
+  int doResolve(const string &qname, const QType &qtype, vector<DNSResourceRecord>&ret, int depth, set<GetBestNSAnswer>& beenthere);
+  bool doCNAMECacheCheck(const string &qname, const QType &qtype, vector<DNSResourceRecord>&ret, int depth, int &res);
+  bool doCacheCheck(const string &qname, const QType &qtype, vector<DNSResourceRecord>&ret, int depth, int &res);
+  void getBestNSFromCache(const string &qname, set<DNSResourceRecord>&bestns, int depth, set<GetBestNSAnswer>& beenthere);
+  void addCruft(const string &qname, vector<DNSResourceRecord>& ret);
+  string getBestNSNamesFromCache(const string &qname,set<string>& nsset, int depth, set<GetBestNSAnswer>&beenthere);
+  void addAuthorityRecords(const string& qname, vector<DNSResourceRecord>& ret, int depth);
+
+  vector<string> shuffle(set<string> &nameservers);
+  bool moreSpecificThan(const string& a, const string &b);
+  string getA(const string &qname, int depth, set<GetBestNSAnswer>& beenthere);
+private:
+  MultiPlexor d_lwr;
+  static map<string,string> s_negcache;
+  struct GetBestNSAnswer
+  {
+    string qname;
+    set<DNSResourceRecord> bestns;
+    bool operator<(const GetBestNSAnswer &b) const
+    {
+      if(qname<b.qname)
+	return true;
+      if(qname==b.qname)
+	return bestns<b.bestns;
+      return false;
+    }
+  };
+
+};
+#endif
