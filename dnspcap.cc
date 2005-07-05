fb678846e47ac98650536231cdaa501545bf99f0
Bert Hubert <bert.hubert@netherlabs.nl>
2005-07-05 07:00:10 +0000

    last bsd-isms
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@429 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- dnspcap.cc
+++ dnspcap.cc
@@ -1,5 +1,7 @@
+#define __FAVOR_BSD
 #include "dnspcap.hh"
 #include <boost/format.hpp>
+#include <fcntl.h>
 
 using namespace boost;
 PcapPacketReader::PcapPacketReader(const string& fname) : d_fname(fname)
@@ -8,6 +10,8 @@ PcapPacketReader::PcapPacketReader(const string& fname) : d_fname(fname)
   if(!d_fp)
     unixDie("Unable to open file");
   
+  int flags=fcntl(fileno(d_fp),F_GETFL,0);
+  fcntl(fileno(d_fp), F_SETFL,flags&(~O_NONBLOCK)); // bsd needs this in stdin (??)
   
   checkedFread(&d_pfh);
   
@@ -65,7 +69,7 @@ try
     if(ntohs(d_ether->ether_type)==0x0800 && d_ip->ip_p==17) { // udp
       d_udp=reinterpret_cast<const struct udphdr*>(d_buffer + sizeof(struct ether_header) + 4 * d_ip->ip_hl);
       d_payload = (unsigned char*)d_udp + sizeof(struct udphdr);
-      d_len = ntohs(d_udp->len) - sizeof(struct udphdr);
+      d_len = ntohs(d_udp->uh_ulen) - sizeof(struct udphdr);
       d_correctpackets++;
       return true;
     }
