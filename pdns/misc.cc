1ab67463050181e3293f92d21f67489bb50bbacc
Bert Hubert <bert.hubert@netherlabs.nl>
2005-08-14 20:14:13 +0000

    massively improve dnsreplay performance fix timeout logic make sure it compiles w/o threads
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@459 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/misc.cc
+++ pdns/misc.cc
@@ -125,7 +125,7 @@ int sendData(const char *buffer, int replen, int outsock)
   iov[0].iov_len=2;
   iov[1].iov_base=(char*)buffer;
   iov[1].iov_len=replen;
-  int ret=Utility::writev(outsock,iov,2);
+  int ret=writev(outsock,iov,2);
 
   if(ret<0) {
     return -1;
@@ -289,7 +289,7 @@ bool IpToU32(const string &str, u_int32_t *ip)
   }
   
   struct in_addr inp;
-  if(Utility::inet_aton(str.c_str(), &inp)) {
+  if(inet_aton(str.c_str(), &inp)) {
     *ip=inp.s_addr;
     return true;
   }
@@ -307,7 +307,7 @@ const string sockAddrToString(struct sockaddr_in *remote, Utility::socklen_t soc
   else {
     char tmp[128];
     
-    if(!Utility::inet_ntop(AF_INET6, ( const char * ) &((struct sockaddr_in6 *)remote)->sin6_addr, tmp, sizeof(tmp)))
+    if(!inet_ntop(AF_INET6, ( const char * ) &((struct sockaddr_in6 *)remote)->sin6_addr, tmp, sizeof(tmp)))
       return "IPv6 untranslateable";
 
     return tmp;
