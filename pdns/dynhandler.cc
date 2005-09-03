ea44d9d33871f421d5a2011f452e72a9a04c7290
Bert Hubert <bert.hubert@netherlabs.nl>
2005-09-03 14:02:16 +0000

    update copyrights, license clarification fix wrong pdns_control notify-host help message
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@475 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dynhandler.cc
+++ pdns/dynhandler.cc
@@ -1,11 +1,10 @@
 /*
     PowerDNS Versatile Database Driven Nameserver
-    Copyright (C) 2002  PowerDNS.COM BV
+    Copyright (C) 2002 - 2005  PowerDNS.COM BV
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU General Public License version 2 as 
+    published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -203,7 +202,7 @@ string DLNotifyHostHandler(const vector<string>&parts, Utility::pid_t ppid)
   extern CommunicatorClass Communicator;
   ostringstream os;
   if(parts.size()!=3)
-    return "syntax: notify domain ip";
+    return "syntax: notify-host domain ip";
   L<<Logger::Warning<<"Notification request to host "<<parts[2]<<" for domain '"<<parts[1]<<"' received"<<endl;
   Communicator.notify(parts[1],parts[2]);
   return "Added to queue";
