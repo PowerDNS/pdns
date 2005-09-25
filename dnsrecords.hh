dfcf7d308536e649539e5e9d7467357e0be2171f
Bert Hubert <bert.hubert@netherlabs.nl>
2005-09-25 20:38:59 +0000

    move over more records to the new unified parsing/generating setup add SRV record support to MOADNSParser add a SRV record regression test
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@499 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- dnsrecords.hh
+++ dnsrecords.hh
@@ -7,46 +7,95 @@
 using namespace std;
 using namespace boost;
 
+#define includeboilerplate(RNAME)   RNAME##RecordContent(const DNSRecord& dr, PacketReader& pr); \
+  RNAME##RecordContent(const string& zoneData);                                                  \
+  static void report(void);                                                                      \
+  static DNSRecordContent* make(const DNSRecord &dr, PacketReader& pr);                          \
+  static DNSRecordContent* make(const string& zonedata);                                         \
+  string getZoneRepresentation() const;                                                          \
+  void toPacket(DNSPacketWriter& pw);                                                            \
+  template<class Convertor> void xfrPacket(Convertor& conv);                             
 
 class NAPTRRecordContent : public DNSRecordContent
 {
 public:
   NAPTRRecordContent(uint16_t order, uint16_t preference, string flags, string services, string regexp, string replacement);
-  NAPTRRecordContent(const DNSRecord& dr, PacketReader& pr);
-  NAPTRRecordContent(const string& zoneData);
 
-  static void report(void);
-  static DNSRecordContent* make(const DNSRecord &dr, PacketReader& pr);
-  string getZoneRepresentation() const;
-
-  void toPacket(DNSPacketWriter& pw);
-
-  template<class Convertor> void xfrPacket(Convertor& conv);
+  includeboilerplate(NAPTR);
 
 private:
   uint16_t d_order, d_preference;
   string d_flags, d_services, d_regexp, d_replacement;
 };
 
+
 class ARecordContent : public DNSRecordContent
 {
 public:
-  ARecordContent(const DNSRecord& dr, PacketReader& pr);
-  ARecordContent(const string& zone);
-
-  static void report(void);
-
-  static DNSRecordContent* make(const DNSRecord& dr, PacketReader& pr);
-
-  template<class Convertor> void xfrPacket(Convertor& conv);
+  includeboilerplate(A);
+  void doRecordCheck(const DNSRecord& dr);
   uint32_t getIP() const;
-  
-  void toPacket(DNSPacketWriter& pw);
-  string getZoneRepresentation() const;
 
 private:
   uint32_t d_ip;
 };
 
+class MXRecordContent : public DNSRecordContent
+{
+public:
+  MXRecordContent(uint16_t preference, const string& mxname);
+
+  includeboilerplate(MX)
+
+private:
+  uint16_t d_preference;
+  string d_mxname;
+};
+
+class SRVRecordContent : public DNSRecordContent
+{
+public:
+  SRVRecordContent(uint16_t preference, uint16_t weight, uint16_t port, const string& target);
+
+  includeboilerplate(SRV)
+
+private:
+  uint16_t d_preference, d_weight, d_port;
+  string d_target;
+};
+
+
+class TXTRecordContent : public DNSRecordContent
+{
+public:
+  includeboilerplate(TXT)
+
+private:
+  string d_text;
+};
+
+namespace {
+  struct soatimes 
+  {
+    uint32_t serial;
+    uint32_t refresh;
+    uint32_t retry;
+    uint32_t expire;
+    uint32_t minimum;
+  };
+}
+
+
+class SOARecordContent : public DNSRecordContent
+{
+public:
+  includeboilerplate(SOA)
+  SOARecordContent(const string& mname, const string& rname, const struct soatimes& st);
+
+private:
+  string d_mname;
+  string d_rname;
+  struct soatimes d_st;
+};
 
 #endif 
