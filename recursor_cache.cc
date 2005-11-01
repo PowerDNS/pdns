cb6c89c942392108c03d8b130d5788f3b1722465
Bert Hubert <bert.hubert@netherlabs.nl>
2005-11-01 10:35:37 +0000

    memory hole you can drive a truck through..
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@534 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- recursor_cache.cc
+++ recursor_cache.cc
@@ -61,7 +61,12 @@ int MemRecursorCache::get(time_t now, const string &qname, const QType& qt, set<
 void MemRecursorCache::replace(const string &qname, const QType& qt,  const set<DNSResourceRecord>& content)
 {
   set<StoredRecord>& stored=d_cache[toLowerCanonic(qname)+"|"+qt.getName()];
+
+  for(set<StoredRecord>::iterator k=stored.begin();k!=stored.end();++k) 
+    k->d_string.prune();
+
   stored.clear();
+
   for(set<DNSResourceRecord>::const_iterator i=content.begin(); i != content.end(); ++i) {
     StoredRecord dr;
     dr.d_ttd=i->ttl;
@@ -88,7 +93,6 @@ void MemRecursorCache::doPrune(void)
     if(j->second.empty()) { // everything is gone
       d_cache.erase(j++);
       names++;
-      
     }
     else {
       ++j;
