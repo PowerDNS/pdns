fb98077bd2b584866883bf777735cd4c19aad7a9
Bert Hubert <bert.hubert@netherlabs.nl>
2006-03-27 13:06:00 +0000

    improve error messages some more Update README for the recursor separate package Make trailing garbage no longer be fatal Make separate recursor do skip-locking by default
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@637 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns_recursor.cc
+++ pdns_recursor.cc
@@ -289,6 +289,7 @@ void startDoResolve(void *p)
     int res=sr.beginResolve(dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret);
     if(res<0) {
       pw.getHeader()->rcode=RCode::ServFail;
+      pw.commit();
       g_stats.servFails++;
     }
     else {
@@ -864,7 +865,7 @@ int main(int argc, char **argv)
 	      L<<Logger::Warning<<"Ignoring question on outgoing socket from "<<dc.getRemote()<<endl;
 	  }
 	  catch(MOADNSException& mde) {
-	    L<<Logger::Error<<"Unparseable packet from remote server "<< sockAddrToString((struct sockaddr_in*) &fromaddr, addrlen) <<": "<<mde.what()<<endl;
+	    L<<Logger::Error<<"Unable to parse packet from remote UDP server "<< sockAddrToString((struct sockaddr_in*) &fromaddr, addrlen) <<": "<<mde.what()<<endl;
 	  }
 	}
       }
@@ -889,7 +890,7 @@ int main(int argc, char **argv)
 	      }
             }
 	    catch(MOADNSException& mde) {
-	      L<<Logger::Error<<"Unparseable packet from remote client "<< sockAddrToString((struct sockaddr_in*) &fromaddr, addrlen) <<": "<<mde.what()<<endl;
+	      L<<Logger::Error<<"Unable to parse packet from remote udp client "<< sockAddrToString((struct sockaddr_in*) &fromaddr, addrlen) <<": "<<mde.what()<<endl;
 	    }
           }
 	}
@@ -1021,7 +1022,7 @@ int main(int argc, char **argv)
 		dc=new DNSComboWriter(i->data, i->qlen, now);
 	      }
 	      catch(MOADNSException &mde) {
-		L<<Logger::Error<<"Unparseable packet from remote TCP client "<<sockAddrToString(&i->remote,sizeof(i->remote))<<endl;
+		L<<Logger::Error<<"Unable to parse packet from remote TCP client "<<sockAddrToString(&i->remote,sizeof(i->remote))<<endl;
 		close(i->fd);
 		tcpconnections.erase(i);
 		break;
