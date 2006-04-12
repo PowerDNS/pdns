e81759a282de0a85d71818f137fa02baca8cfa5c
Bert Hubert <bert.hubert@netherlabs.nl>
2006-04-12 07:38:45 +0000

    consolidate license change previously announced here: http://mailman.powerdns.com/pipermail/pdns-announce/2005-February/000022.html
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@681 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- backends/bind/bindparser.cc
+++ backends/bind/bindparser.cc
@@ -4,8 +4,8 @@
    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
+   it under the terms of the GNU General Public License version 2
+   as published by the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
