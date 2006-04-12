22dc646a7494a67ebdcd74163a101ff4958354d9
Bert Hubert <bert.hubert@netherlabs.nl>
2006-04-12 07:38:45 +0000

    consolidate license change previously announced here: http://mailman.powerdns.com/pipermail/pdns-announce/2005-February/000022.html
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@681 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/backends/bind/zone2ldap.cc
+++ pdns/backends/bind/zone2ldap.cc
@@ -3,9 +3,9 @@
  *  Copyright (C) 2003  Norbert Sendetzky
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  it under the terms of the GNU General Public License version 2
+ *  the Free Software Foundation
+ *  
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
