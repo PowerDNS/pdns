9458f7861581620eb4bfdd0e9691497ff8c55dd7
Bert Hubert <bert.hubert@netherlabs.nl>
2006-04-20 10:00:01 +0000

    remove bashisms fix install on Solaris, FreeBSD add fake ./configure script to catch Boost or lack thereof update documentation
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@751 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ pdns_hw.cc
@@ -0,0 +1,19 @@
+#include <boost/lexical_cast.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
+#include <boost/multi_index/key_extractors.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+
+using namespace std;
+int main()
+{
+	ostringstream str;
+	str << "Everything is ok!"<< boost::lexical_cast<string>("") <<"\n";
+	exit(0);
+}
