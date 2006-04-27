9f1e3a593cdb37956c65f765869bb6eb4ee97d68
Bert Hubert <bert.hubert@netherlabs.nl>
2006-04-27 17:06:49 +0000

    make the zoneparser tng no longer suck
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@778 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- zoneparser-tng.hh
+++ zoneparser-tng.hh
@@ -1,6 +1,6 @@
 /*
     PowerDNS Versatile Database Driven Nameserver
-    Copyright (C) 2005  PowerDNS.COM BV
+    Copyright (C) 2005 - 2006 PowerDNS.COM BV
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2 
@@ -20,22 +20,27 @@
 #define PDNS_ZONEPARSER_TNG
 #include <string>
 #include <cstdio>
+#include <stdexcept>
+
 using namespace std;
 
-// The API is fine, the implementation still sucks
 class ZoneParserTNG
 {
 public:
-  ZoneParserTNG(const string& fname);
+  ZoneParserTNG(const string& fname, const string& zname="");
 
   ~ZoneParserTNG();
 
   bool get(DNSResourceRecord& rr);
-
+  typedef runtime_error exception;
 private:
+  bool getLine();
+
   FILE *d_fp;
   string d_line;
-  bool getLine();
+  string d_prevqname;
+  string d_zonename;
+  int d_defaultttl;
 };
 
 #endif
