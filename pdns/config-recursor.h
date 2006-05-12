a1a344bfa0b8b50ea682e4266e2e18bd8f3b0a5d
Bert Hubert <bert.hubert@netherlabs.nl>
2006-05-12 12:16:55 +0000

    raise version, fix compilation on solaris10
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@830 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/config-recursor.h
+++ pdns/config-recursor.h
@@ -1,6 +1,6 @@
 #define SYSCONFDIR "/etc/powerdns/" 
 #define LOCALSTATEDIR "/var/run/" 
-#define VERSION "3.1-pre1"
+#define VERSION "3.1-pre2"
 #define RECURSOR
 #ifndef WIN32
 #define GCC_SKIP_LOCKING
