aef07816f0777b22f08b2a4f6bf21d29e9f59cbd
Bert Hubert <bert.hubert@netherlabs.nl>
2006-06-25 12:10:51 +0000

    change version
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@861 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/config-recursor.h
+++ pdns/config-recursor.h
@@ -1,6 +1,6 @@
 #define SYSCONFDIR "/etc/powerdns/" 
 #define LOCALSTATEDIR "/var/run/" 
-#define VERSION "3.1.2-rc1"
+#define VERSION "3.1.2"
 #define RECURSOR
 #ifndef WIN32
 #define GCC_SKIP_LOCKING
