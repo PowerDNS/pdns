4c5d6e089687254377aa81138958609dea40b9d0
Bert Hubert <bert.hubert@netherlabs.nl>
2006-08-24 22:24:35 +0000

    small improvement for resolving ipv6
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@885 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/nameserver.cc
+++ pdns/nameserver.cc
@@ -145,9 +145,9 @@ void UDPNameserver::bindIPv6()
     }
 
     sockaddr_in6 locala;
-    locala.sin6_port=ntohs(arg().asNum("local-port"));
+    memset(&locala, 0, sizeof(locala));
+    locala.sin6_port=htons(arg().asNum("local-port"));
     locala.sin6_family=AF_INET6;
-    locala.sin6_flowinfo=0;
 
     if(!inet_pton(AF_INET6, localname.c_str(), (void *)&locala.sin6_addr)) {
       addrinfo *addrinfos;
