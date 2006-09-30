db22cda7c9cc09e85e9f5e2c880560e1017ed090
Bert Hubert <bert.hubert@netherlabs.nl>
2006-09-30 12:18:52 +0000

    release related version number updates
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@894 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- config-recursor.h
+++ config-recursor.h
@@ -1,6 +1,6 @@
 #define SYSCONFDIR "/etc/powerdns/" 
 #define LOCALSTATEDIR "/var/run/" 
-#define VERSION "3.1.3-pre"
+#define VERSION "3.1.3"
 #define RECURSOR
 #ifndef WIN32
 #define GCC_SKIP_LOCKING
