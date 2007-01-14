7d6a58901d8e446eb05078ec46b37519850befb6
Bert Hubert <bert.hubert@netherlabs.nl>
2007-01-14 13:21:46 +0000

    close ticket #110 and improve chroot efficacy by chdir("/"), thanks "somebody", no name was attached to the ticket.
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@944 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- common_startup.cc
+++ common_startup.cc
@@ -278,7 +278,7 @@ void mainthread()
    if(!arg()["chroot"].empty()) {  
      if(arg().mustDo("master") || arg().mustDo("slave"))
 	gethostbyname("a.root-servers.net"); // this forces all lookup libraries to be loaded
-     if(chroot(arg()["chroot"].c_str())<0) {
+     if(chroot(arg()["chroot"].c_str())<0 || chdir("/")<0) {
        L<<Logger::Error<<"Unable to chroot to '"+arg()["chroot"]+"': "<<strerror(errno)<<", exiting"<<endl; 
        exit(1);
      }   
