2e83ba09fcfdc0ba5a02b4fbb0e04549d08857eb
Bert Hubert <bert.hubert@netherlabs.nl>
2007-02-18 13:25:23 +0000

    teach zoneparser-tng about $ORIGIN and $GENERATE, is now feature complete to take over zoneparser in main pdns auth server
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@952 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/zoneparser-tng.hh
+++ pdns/zoneparser-tng.hh
@@ -1,6 +1,6 @@
 /*
     PowerDNS Versatile Database Driven Nameserver
-    Copyright (C) 2005 - 2006 PowerDNS.COM BV
+    Copyright (C) 2005 - 2007 PowerDNS.COM BV
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2 
@@ -34,14 +34,19 @@ public:
 
   bool get(DNSResourceRecord& rr);
   typedef runtime_error exception;
+  typedef deque<pair<string::size_type, string::size_type> > parts_t;
 private:
   bool getLine();
+  bool getTemplateLine();
   void stackFile(const std::string& fname);
   stack<FILE *> d_fps;
   string d_line;
   string d_prevqname;
   string d_zonename;
   int d_defaultttl;
+  uint32_t d_templatecounter, d_templatestop, d_templatestep;
+  string d_templateline;
+  parts_t d_templateparts;
 };
 
 #endif
