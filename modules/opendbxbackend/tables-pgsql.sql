6cd9edc5c756be8d067753e6d97cf6caee30f7b9
Bert Hubert <bert.hubert@netherlabs.nl>
2007-04-21 08:59:01 +0000

    update opendbx tables in source, closing ticket 134, thanks to Ralf van der Enden
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1040 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- modules/opendbxbackend/tables-pgsql.sql
+++ modules/opendbxbackend/tables-pgsql.sql
@@ -1,50 +1,57 @@
-CREATE TABLE domains (
-	id SERIAL,
-	type VARCHAR(6) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	master VARCHAR(40) DEFAULT NULL,
-	account VARCHAR(40) DEFAULT NULL
-	notified_serial INT DEFAULT NULL,
-	last_check INT DEFAULT NULL,
-CONSTRAINT pk_id
-	PRIMARY KEY (id),
-CONSTRAINT unq_name
-	UNIQUE (name)
+CREATE TABLE "domains" (
+	"id" SERIAL NOT NULL,
+	"name" VARCHAR(255) NOT NULL,
+	"type" VARCHAR(6) NOT NULL,
+	"master" VARCHAR(40) NOT NULL DEFAULT '',
+	"account" VARCHAR(40) NOT NULL DEFAULT '',
+	"notified_serial" INTEGER DEFAULT NULL,
+	"last_check" INTEGER DEFAULT NULL,
+	"status" CHAR(1) NOT NULL DEFAULT 'A',
+CONSTRAINT "pk_domains_id"
+	PRIMARY KEY ("id"),
+CONSTRAINT "unq_domains_name"
+	UNIQUE ("name")
 );
 
+CREATE INDEX "idx_domains_status_type" ON "domains" ("status","type");
 
-CREATE TABLE records (
-	id SERIAL,
-	domain_id INT DEFAULT NULL,
-	name VARCHAR(255) DEFAULT NULL,
-	type VARCHAR(6) DEFAULT NULL,
-	ttl INT DEFAULT NULL,
-	prio INT DEFAULT NULL,
-	content VARCHAR(255) DEFAULT NULL,
-	change_date INT DEFAULT NULL,
-CONSTRAINT pk_id
-	PRIMARY KEY (id),
-CONSTRAINT fk_domainid
-	FOREIGN KEY (domain_id)
-	REFERENCES domains(id)
+
+
+CREATE TABLE "records" (
+	"id" SERIAL NOT NULL,
+	"domain_id" INTEGER NOT NULL,
+	"name" VARCHAR(255) NOT NULL,
+	"type" VARCHAR(6) NOT NULL,
+	"ttl" INTEGER DEFAULT NULL,
+	"prio" INTEGER DEFAULT NULL,
+	"content" VARCHAR(255) NOT NULL,
+	"change_date" INTEGER DEFAULT NULL,
+CONSTRAINT "pk_records_id"
+	PRIMARY KEY ("id"),
+CONSTRAINT "fk_records_domainid"
+	FOREIGN KEY ("domain_id")
+	REFERENCES domains ("id")
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 );
 
-CREATE INDEX idx_rname ON records(name);
-CREATE INDEX idx_rname_rtype ON records(name,type);
-CREATE INDEX idx_rdomainid ON records(domain_id);
+CREATE INDEX "idx_records_name_type" ON "records" ("name","type");
+CREATE INDEX "idx_records_type" ON "records" ("type");
 
 
-CREATE TABLE supermasters (
-	ip VARCHAR(40) NOT NULL,
-	nameserver VARCHAR(255) NOT NULL,
-	account VARCHAR(40) DEFAULT NULL
+
+CREATE TABLE "supermasters" (
+	"ip" VARCHAR(40) NOT NULL,
+	"nameserver" VARCHAR(255) NOT NULL,
+	"account" VARCHAR(40) NOT NULL DEFAULT ''
 );
 
+CREATE INDEX "idx_smaster_ip_ns" ON "supermasters" ("ip","nameserver");
 
-GRANT SELECT ON supermasters TO powerdns;
-GRANT ALL ON domains TO powerdns;
-GRANT ALL ON domains_id_seq TO powerdns;
-GRANT ALL ON records TO powerdns;
-GRANT ALL ON records_id_seq TO powerdns;
+
+
+GRANT SELECT ON "supermasters" TO "powerdns";
+GRANT ALL ON "domains" TO "powerdns";
+GRANT ALL ON "domains_id_seq" TO "powerdns";
+GRANT ALL ON "records" TO "powerdns";
+GRANT ALL ON "records_id_seq" TO "powerdns";
--- modules/opendbxbackend/tables-sqlite.sql
+++ modules/opendbxbackend/tables-sqlite.sql
@@ -1,43 +1,51 @@
-CREATE TABLE domains (
-	id INT AUTOINCREMENT,
-	type VARCHAR(6) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	master VARCHAR(40) DEFAULT NULL,
-	account VARCHAR(40) DEFAULT NULL,
-	notified_serial INT DEFAULT NULL,
-	last_check INT DEFAULT NULL,
-CONSTRAINT pk_id
-	PRIMARY KEY (id),
-CONSTRAINT unq_name
-	UNIQUE (name)
+CREATE TABLE "domains" (
+	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"name" VARCHAR(255) NOT NULL,
+	"type" VARCHAR(6) NOT NULL,
+	"master" VARCHAR(40) NOT NULL DEFAULT '',
+	"account" VARCHAR(40) NOT NULL DEFAULT '',
+	"notified_serial" INTEGER DEFAULT NULL,
+	"last_check" INTEGER DEFAULT NULL,
+	"status" CHAR(1) NOT NULL DEFAULT 'A',
+CONSTRAINT "unq_domains_name"
+	UNIQUE ("name")
 );
 
+CREATE INDEX "idx_domains_status_type" ON "domains" ("status","type");
 
-CREATE TABLE records (
-	id INT AUTOINCREMENT,
-	domain_id INT DEFAULT NULL,
-	name VARCHAR(255) DEFAULT NULL,
-	type VARCHAR(6) DEFAULT NULL,
-	ttl INT DEFAULT NULL,
-	prio INT DEFAULT NULL,
-	content VARCHAR(255) DEFAULT NULL,
-	change_date INT DEFAULT NULL,
-CONSTRAINT pk_id
-	PRIMARY KEY (id),
-CONSTRAINT fk_domainid
-	FOREIGN KEY (domain_id)
-	REFERENCES domains(id)
+
+
+CREATE TABLE "records" (
+	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"domain_id" INTEGER NOT NULL,
+	"name" VARCHAR(255) NOT NULL,
+	"type" VARCHAR(6) NOT NULL,
+	"ttl" INTEGER DEFAULT NULL,
+	"prio" INTEGER DEFAULT NULL,
+	"content" VARCHAR(255) NOT NULL,
+	"change_date" INTEGER DEFAULT NULL,
+CONSTRAINT "fk_records_domainid"
+	FOREIGN KEY ("domain_id")
+	REFERENCES "domains" ("id")
 	ON UPDATE CASCADE
 	ON DELETE CASCADE
 );
 
-CREATE INDEX idx_rname ON records(name);
-CREATE INDEX idx_rname_rtype ON records(name,type);
-CREATE INDEX idx_rdomainid ON records(domain_id);
+CREATE INDEX "idx_records_name_type" ON "records" ("name","type");
+CREATE INDEX "idx_records_type" ON "records" ("type");
 
 
-CREATE TABLE supermasters (
-	ip VARCHAR(40) NOT NULL,
-	nameserver VARCHAR(255) NOT NULL,
-	account VARCHAR(40) DEFAULT NULL
+
+CREATE TABLE "supermasters" (
+	"ip" VARCHAR(40) NOT NULL,
+	"nameserver" VARCHAR(255) NOT NULL,
+	"account" VARCHAR(40) NOT NULL DEFAULT ''
 );
+
+CREATE INDEX "idx_smip_smns" ON "supermasters" ("ip","nameserver");
+
+
+
+GRANT SELECT ON "supermasters" TO "powerdns";
+GRANT ALL ON "domains" TO "powerdns";
+GRANT ALL ON "records" TO "powerdns";
