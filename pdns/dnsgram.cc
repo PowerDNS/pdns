97b1caa0d6527991ba1329a79e67eadba8f71b39
Bert Hubert <bert.hubert@netherlabs.nl>
2008-02-03 16:24:25 +0000

    tweak dnsgram to no longer write pcap file, while being more verbose about what it does do
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1125 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dnsgram.cc
+++ pdns/dnsgram.cc
@@ -23,7 +23,7 @@ void makeReport(const struct timeval& tv)
   int64_t clientdiff = g_clientQuestions - g_clientResponses;
   int64_t serverdiff = g_serverQuestions - g_serverResponses;
 
-  if(clientdiff > 0.01*g_clientQuestions) {
+  if(clientdiff > 5 && clientdiff > 0.02*g_clientQuestions) {
     char tmp[80];
     struct tm tm=*localtime_r(&tv.tv_sec, &tm);
     strftime(tmp, sizeof(tmp) - 1, "%F %H:%M:%S", &tm);
@@ -42,15 +42,13 @@ void makeReport(const struct timeval& tv)
     cout<<"Last question: "<<tmp<<"."<<g_lastquestionTime.tv_usec/1000000.0<<endl;
   }
 
-  if(serverdiff > 0.01*g_serverQuestions) {
+  if(serverdiff > 5 && serverdiff > 0.02*g_serverQuestions) {
     char tmp[80];
     struct tm tm=*localtime_r(&tv.tv_sec, &tm);
     strftime(tmp, sizeof(tmp) - 1, "%F %H:%M:%S", &tm);
 
     cout << tmp << ": Auth server dropped too many questions (" 
 	 << g_serverQuestions <<" vs " << g_serverResponses << "), diff: " <<serverdiff<<endl;
-
-    cout << tv.tv_sec<<endl;
 
     tm=*localtime_r(&g_lastanswerTime.tv_sec, &tm);
     strftime(tmp, sizeof(tmp) - 1, "%F %H:%M:%S", &tm);
@@ -66,7 +64,7 @@ void makeReport(const struct timeval& tv)
 //    ", server questions: "<<g_serverQuestions<<", server responses: "<<g_serverResponses<<endl;
 
 
-  cerr << tv.tv_sec << " " <<g_clientQuestions<<" " << g_clientResponses<< " "<<g_serverQuestions<<" "<<g_serverResponses<<" "<<g_skipped<<endl;
+//  cerr << tv.tv_sec << " " <<g_clientQuestions<<" " << g_clientResponses<< " "<<g_serverQuestions<<" "<<g_serverResponses<<" "<<g_skipped<<endl;
   g_clientQuestions=g_clientResponses=g_serverQuestions=g_serverResponses=0;
   g_skipped=0;
 }
@@ -79,7 +77,7 @@ try
     cout<<argv[n]<<endl;
     unsigned int parseErrors=0, totalQueries=0, skipped=0;
     PcapPacketReader pr(argv[n]);
-    PcapPacketWriter pw(argv[n]+string(".out"), pr);
+    //    PcapPacketWriter pw(argv[n]+string(".out"), pr);
     /* four sorts of packets: 
        "rd": question from a client pc
        "rd qr": answer to a client pc
@@ -95,7 +93,7 @@ try
     typedef set<pair<string, uint16_t> > queries_t;
     queries_t questions, answers;
 
-    unsigned int count = 50000;
+    //    unsigned int count = 50000;
     
     map<pair<string, uint16_t>, int> counts;
 
@@ -117,13 +115,14 @@ try
 	    lastreport = pr.d_pheader.ts;
 	  }
 	  
-	  if(pr.d_pheader.ts.tv_sec > 1176897290 && pr.d_pheader.ts.tv_sec < 1176897310 ) 
-	    pw.write();
+	  //	  if(pr.d_pheader.ts.tv_sec > 1176897290 && pr.d_pheader.ts.tv_sec < 1176897310 ) 
+	  //	    pw.write();
 
 	  if(mdp.d_header.rd && !mdp.d_header.qr) {
 	    g_lastquestionTime=pr.d_pheader.ts;
 	    g_clientQuestions++;
 	    totalQueries++;
+	    counts[make_pair(mdp.d_qname, mdp.d_qtype)]++;
 	    questions.insert(make_pair(mdp.d_qname, mdp.d_qtype));
 	  }
 	  else if(mdp.d_header.rd && mdp.d_header.qr) {
@@ -143,7 +142,7 @@ try
 	    g_serverResponses++;
 	  }
 	  
-	  if(pr.d_pheader.ts.tv_sec - lastreport.tv_sec > 2) {
+	  if(pr.d_pheader.ts.tv_sec - lastreport.tv_sec > 5) {
 	    makeReport(pr.d_pheader.ts);
 	    lastreport = pr.d_pheader.ts;
 
@@ -168,9 +167,12 @@ try
     set_difference(questions.begin(), questions.end(), answers.begin(), answers.end(), back_inserter(diff));
     cerr<<questions.size()<<" different rd questions, "<< answers.size()<<" different rd answers, diff: "<<diff.size()<<endl;
     cerr<<skipped<<" skipped\n";
+
+
+    cerr<<"Generating 'failed' file with failed queries and counts\n";
     ofstream failed("failed");
     for(diff_t::const_iterator i = diff.begin(); i != diff.end() ; ++i) {
-      failed << i->first << "\t" << i->second << "\t"<<counts[make_pair(i->first, i->second)]<<"\n";
+      failed << i->first << "\t" << i->second << "\t"<< counts[make_pair(i->first, i->second)]<<"\n";
     }
 
     diff.clear();
@@ -178,9 +180,10 @@ try
     set_difference(answers.begin(), answers.end(), questions.begin(), questions.end(), back_inserter(diff));
     cerr<<diff.size()<<" answers w/o questions\n";
 
+    cerr<<"Generating 'succeeded' file with failed queries and counts\n";
     ofstream succeeded("succeeded");
     for(queries_t::const_iterator i = answers.begin(); i != answers.end() ; ++i) {
-      succeeded << i->first << "\t" << i->second << "\n";
+      succeeded << i->first << "\t" << i->second << counts[make_pair(i->first, i->second)]<<"\n";
     }
   }
 }
