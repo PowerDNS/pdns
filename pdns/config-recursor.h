d54a098193df96a3ca22cebf51e60d587a125e61
Bert Hubert <bert.hubert@netherlabs.nl>
2008-02-05 23:17:02 +0000

    add skip locking prevention for more recent g++
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1134 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/config-recursor.h
+++ pdns/config-recursor.h
@@ -1,7 +1,12 @@
 #define SYSCONFDIR "/etc/powerdns/" 
 #define LOCALSTATEDIR "/var/run/" 
-#define VERSION "3.1.5-snapshot"
+#define VERSION "3.1.5-snapshot2"
 #define RECURSOR
 #ifndef WIN32
+
+#if __GNUC__ == 4 &&  __GNUC_MINOR__ < 2
 #define GCC_SKIP_LOCKING
+#endif
+
+
 #endif
