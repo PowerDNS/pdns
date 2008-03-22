950f78df749100ef815da1a67f5619a144c6cd2c
Bert Hubert <bert.hubert@netherlabs.nl>
2008-03-22 16:03:15 +0000

    properly convey empty TXT strings (why??) through the recursor, closing ticket 178
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1166 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dnsparser.cc
+++ pdns/dnsparser.cc
@@ -400,9 +400,10 @@ string PacketReader::getText(bool multi)
     unsigned char labellen=d_content.at(d_pos++);
     
     ret.append(1,'"');
-    string val(&d_content.at(d_pos), &d_content.at(d_pos+labellen-1)+1);
-    
-    ret.append(txtEscape(val)); // the end is one beyond the packet
+    if(labellen) { // no need to do anything for an empty string
+      string val(&d_content.at(d_pos), &d_content.at(d_pos+labellen-1)+1);
+      ret.append(txtEscape(val)); // the end is one beyond the packet
+    }
     ret.append(1,'"');
     d_pos+=labellen;
     if(!multi)
