059062cbbb33780926333cc7f38203e54836e63a
Bert Hubert <bert.hubert@netherlabs.nl>
2008-06-12 13:06:06 +0000

    initial Lua support
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@1191 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ lua-pdns-recursor.hh
@@ -0,0 +1,22 @@
+#ifndef PDNS_LUA_PDNS_RECURSOR_HH
+#define PDNS_LUA_PDNS_RECURSOR_HH
+#include "dns.hh"
+#include "iputils.hh"
+
+struct lua_State;
+
+class PowerDNSLua
+{
+public:
+  PowerDNSLua();
+  ~PowerDNSLua();
+  void reload();
+  bool prequery(const ComboAddress& remote, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret);
+  bool nxdomain(const ComboAddress& remote, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret);
+private:
+  lua_State* d_lua;
+  bool passthrough(const string& func, const ComboAddress& remote, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res);
+  bool d_failed;
+};
+
+#endif
