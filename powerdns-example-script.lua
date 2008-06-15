e206293f5e100486ad51994c791fc52eec3ad48c
Bert Hubert <bert.hubert@netherlabs.nl>
2008-06-15 19:49:45 +0000

    update README, add 'pdns.A', 'pdns.AAAA', 'pdns.NXDOMAIN', etc symbols, plus add matchnetmask support for multiple netmasks
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@1202 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- powerdns-example-script.lua
+++ powerdns-example-script.lua
@@ -4,22 +4,22 @@ function preresolve ( ip, domain, qtype )
 	if domain == "www.ds9c.nl." 
 	then
 		ret={}
-		ret[1]= {qtype=1, content="9.8.7.6", ttl=3601}
-		ret[2]= {qtype=1, content="1.2.3.4", ttl=3601}
+		ret[1]= {qtype=pdns.A, content="9.8.7.6", ttl=3601}
+		ret[2]= {qtype=pdns.A, content="1.2.3.4", ttl=3601}
 		print "dealing!"
 		return 0, ret
 	elseif domain == "www.baddomain.com."
 	then
 		print "dealing - faking nx"
-	       	return 3, {}
+	       	return pdns.NXDOMAIN, {}
 	elseif domain == "echo."
 	then
 		print "dealing with echo!"
-		return 0, {{qtype=1, content=ip}}
+		return 0, {{qtype=pdns.A, content=ip}}
 	elseif domain == "echo6."
 	then
 		print "dealing with echo6!"
-		return 0, {{qtype=28, content=ip}}
+		return 0, {{qtype=pdns.AAAA, content=ip}}
 	else
 		print "not dealing!"
 		return -1, {}
@@ -27,17 +27,17 @@ function preresolve ( ip, domain, qtype )
 end
 
 function nxdomain ( ip, domain, qtype )
-	print ("nxhandler called for: ", ip, domain, qtype)
-	if qtype ~= 1 then return -1, {} end  --  only A records
+	print ("nxhandler called for: ", ip, domain, qtype, pdns.AAAA)
+	if qtype ~= pdns.A then return -1, {} end  --  only A records
 	if not string.find(domain, "^www.") then return -1, {} end  -- only things that start with www.
 	
-	if matchnetmask(ip, "127.0.0.1/8")
+	if matchnetmask(ip, "10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12", "::/0")
 	then 
 		print "dealing"
 		ret={}
-		ret[1]={qtype="5", content="www.webserver.com", ttl=3602}
-		ret[2]={qname="www.webserver.com", qtype="1", content="1.2.3.4", ttl=3602}
-		ret[3]={qname="webserver.com", qtype="2", content="ns1.webserver.com", place=2}
+		ret[1]={qtype=pdns.CNAME, content="www.webserver.com", ttl=3602}
+		ret[2]={qname="www.webserver.com", qtype=pdns.A, content="1.2.3.4", ttl=3602}
+		ret[3]={qname="webserver.com", qtype=pdns.NS, content="ns1.webserver.com", place=2}
 --		ret[1]={15, "25 ds9a.nl", 3602}
 		return 0, ret
 	else
