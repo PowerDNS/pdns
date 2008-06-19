affd46f28f56124c34f496506b793ceff83153f1
Bert Hubert <bert.hubert@netherlabs.nl>
2008-06-19 12:12:27 +0000

    fix up Solaris compilation - noted by Marcus Goller
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@1207 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- qtype.hh
+++ qtype.hh
@@ -71,7 +71,8 @@ public:
   int getCode() const; //!< Get the integer representation of this type
 
   static int chartocode(const char *p); //!< convert a character string to a code
-  
+// more solaris fun
+#undef DS   
   enum typeenum {A=1,NS=2,CNAME=5,SOA=6, MR=9, PTR=12,HINFO=13,MX=15,TXT=16,RP=17,AFSDB=18,KEY=25,AAAA=28,LOC=29,SRV=33,NAPTR=35, KX=36, 
 		 CERT=37,OPT=41, DS=43, SSHDP=44, IPSECKEY=45, RRSIG=46, NSEC=47, DNSKEY=48, DHCID=49, 
 		 SPF=99, AXFR=252, IXFR=251, ANY=255, URL=256, MBOXFW=257, CURL=258, ADDR=259} types;
