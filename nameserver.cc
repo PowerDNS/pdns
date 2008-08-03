e21a86152510241772ad0163351eebbf541dcd29
Bert Hubert <bert.hubert@netherlabs.nl>
2008-08-03 18:42:04 +0000

    improve message about errors binding to a port
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@1245 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- nameserver.cc
+++ nameserver.cc
@@ -117,7 +117,7 @@ void UDPNameserver::bindIPv4()
     locala.sin_port=htons(arg().asNum("local-port"));
     
     if(bind(s, (sockaddr*)&locala,sizeof(locala))<0) {
-      L<<Logger::Error<<"binding UDP socket to '"+localname+"': "<<strerror(errno)<<endl;
+      L<<Logger::Error<<"binding UDP socket to '"+localname+"' port "+lexical_cast<string>(ntohs(locala.sin_port))+": "<<strerror(errno)<<endl;
       throw AhuException("Unable to bind to UDP socket");
     }
     d_highfd=max(s,d_highfd);
