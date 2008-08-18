f2a0be6b1bf6f5a531a3008c176b7785cfadfdde
Bert Hubert <bert.hubert@netherlabs.nl>
2008-08-18 18:12:26 +0000

    initial spoofing policy class load
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns/pdns@1257 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ spoofpol.hh
@@ -0,0 +1,28 @@
+#ifndef PDNS_SPOOFPOL_HH
+#define PDNS_SPOOFPOL_HH
+#include <string>
+#include <sys/time.h>
+#include <time.h>
+#include "iputils.hh"
+#include <map>
+
+class SpoofPolicy
+{
+public:
+  void report(const ComboAddress& remote, 
+	      const std::string& auth, 
+	      int policy,
+	      const struct timeval& );
+  int getPolicy(const ComboAddress& remote, const std::string& aith, const struct timeval& );
+
+private:
+  struct SpoofEntry
+  {
+    time_t ttd;
+    int policy;
+  };
+  typedef std::map<std::pair<ComboAddress, string>, SpoofEntry > spoofmap_t;
+  spoofmap_t d_spoofmap;
+};
+
+#endif
