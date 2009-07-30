cc31e2387401cdf6f966167a7bcb5d7b975f6561
Bert Hubert <bert.hubert@netherlabs.nl>
2009-07-30 19:52:01 +0000

    cherry-pick for 3.1.171: 1258 performance
    git-svn-id: svn://svn.powerdns.com/pdns/tags/pdns-3.1.7.1@1378 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dnswriter.cc
+++ pdns/dnswriter.cc
@@ -67,7 +67,7 @@ void DNSPacketWriter::startRecord(const string& name, uint16_t qtype, uint32_t t
   d_stuff = 0; 
   d_rollbackmarker=d_content.size();
 
-  if(iequals(d_qname,d_recordqname)) {  // don't do the whole label compression thing if we *know* we can get away with "see question"
+  if(!strcasecmp(d_qname.c_str(), d_recordqname.c_str())) {  // don't do the whole label compression thing if we *know* we can get away with "see question"
     static char marker[2]={0xc0, 0x0c};
     d_content.insert(d_content.end(), &marker[0], &marker[2]);
   }
@@ -153,7 +153,7 @@ DNSPacketWriter::lmap_t::iterator find(DNSPacketWriter::lmap_t& lmap, const stri
 {
   DNSPacketWriter::lmap_t::iterator ret;
   for(ret=lmap.begin(); ret != lmap.end(); ++ret)
-    if(iequals(ret->first,label))
+    if(!strcasecmp(ret->first.c_str() ,label.c_str()))
       break;
   return ret;
 }
