d89592cd25fe34ad76189205ad1d5a7670305e3b
Bert Hubert <bert.hubert@netherlabs.nl>
2010-05-12 19:12:04 +0000

    fix notify compilation
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1608 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/notify.cc
+++ pdns/notify.cc
@@ -11,6 +11,7 @@
 #include <boost/multi_index/key_extractors.hpp>
 
 #include "mplexer.hh"
+#include "statbag.hh"
 
 #include "namespaces.hh"
 using namespace ::boost::multi_index;
@@ -19,17 +20,24 @@ using namespace std;
 namespace po = boost::program_options;
 po::variables_map g_vm;
 
+StatBag S;
 
 int main(int argc, char** argv)
 try
 {
+  if(argc!=3) {
+    cerr<<"Syntax: notify ip domain"<<endl;
+    exit(1);
+  }
+
   int sock = socket(AF_INET, SOCK_DGRAM, 0);
   if(sock < 0)
     throw runtime_error("Creating socket for incoming packets: "+stringerror());
 
-  ComboAddress local("127.0.0.1", (int)0);
-  if(::bind(sock, (struct sockaddr*) &local, local.getSocklen()) < 0) 
-    throw runtime_error("Failed to bind local socket to address "+local.toString()+": "+stringerror());
+
+ // ComboAddress local("127.0.0.1", (int)0);
+//  if(::bind(sock, (struct sockaddr*) &local, local.getSocklen()) < 0) 
+//    throw runtime_error("Failed to bind local socket to address "+local.toString()+": "+stringerror());
 
   ComboAddress pdns(argv[1], 53);
   if(connect(sock, (struct sockaddr*) &pdns, pdns.getSocklen()) < 0) 
@@ -61,41 +69,3 @@ catch(std::exception& e)
   cerr<<"Fatal: "<<e.what()<<endl;
 }
 
-/* added so we don't have to link in most of powerdns */
-
-const char *Utility::inet_ntop(int af, const char *src, char *dst, size_t size)
-{
-  return ::inet_ntop(af,src,dst,size);
-}
-
-// Converts an address from presentation format to network format.
-int Utility::inet_pton( int af, const char *src, void *dst )
-{
-  return ::inet_pton(af, src, dst);
-}
-
-// Returns the current time.
-int Utility::gettimeofday( struct timeval *tv, void *tz )
-{
-  return ::gettimeofday(tv,0);
-}
-
-string stringerror()
-{
-  return strerror(errno);
-}
-
-bool IpToU32(const string &str, uint32_t *ip)
-{
-  if(str.empty()) {
-    *ip=0;
-    return true;
-  }
-  
-  struct in_addr inp;
-  if(inet_aton(str.c_str(), &inp)) {
-    *ip=inp.s_addr;
-    return true;
-  }
-  return false;
-}
