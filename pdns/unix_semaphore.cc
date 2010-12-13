440191e19f0f13d36787f8234b72b04b1e02f1aa
Bert Hubert <bert.hubert@netherlabs.nl>
2010-12-13 20:30:15 +0000

    Anders Kaseorg has fixed our semaphore code against returning EINTR, solving crashes when backgrounding pdns
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1747 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/unix_semaphore.cc
+++ pdns/unix_semaphore.cc
@@ -156,7 +156,11 @@ int Semaphore::post()
 
 int Semaphore::wait()
 {
-  return sem_wait(m_pSemaphore);
+  int ret;
+  do
+    ret = sem_wait(m_pSemaphore);
+  while (ret == -1 && errno == EINTR);
+  return ret;
 }
 int Semaphore::tryWait()
 {
