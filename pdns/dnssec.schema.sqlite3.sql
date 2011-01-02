27b2903bed29114359af3fc076c222ebe841bba6
Bert Hubert <bert.hubert@netherlabs.nl>
2011-01-02 21:49:31 +0000

    add the code to add the new fields to the sqlite3 sql schema
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@1794 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dnssec.schema.sqlite3.sql
+++ pdns/dnssec.schema.sqlite3.sql
@@ -1,3 +1,7 @@
+alter table records add ordername      VARCHAR(255);
+alter table records add auth bool;
+create index orderindex on records(ordername);
+
 create table domainmetadata (
  id		 INTEGER PRIMARY KEY,
  domain_id       INT NOT NULL,
