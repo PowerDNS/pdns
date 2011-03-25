86113ac91cb8e597a927b0cc9837062a18f494d9
Bert Hubert <bert.hubert@netherlabs.nl>
2011-03-25 15:08:39 +0000

    catch database error in time and not on the floor
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2100 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/packethandler.cc
+++ pdns/packethandler.cc
@@ -1353,6 +1353,12 @@ DNSPacket *PacketHandler::questionOrRecurse(DNSPacket *p, bool *shouldRecurse)
     S.inc("servfail-packets");
     S.ringAccount("servfail-queries",p->qdomain);
   }
+  catch(AhuException &e) {
+    L<<Logger::Error<<"Database module reported condition which prevented lookup ("+e.reason+") sending out servfail"<<endl;
+    r->setRcode(RCode::ServFail);
+    S.inc("servfail-packets");
+    S.ringAccount("servfail-queries",p->qdomain);
+  }
   catch(std::exception &e) {
     L<<Logger::Error<<"Exception building answer packet ("<<e.what()<<") sending out servfail"<<endl;
     delete r;
