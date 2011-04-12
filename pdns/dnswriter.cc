a6e93c0f349a83b3a5e17939bb996a0c1569719a
Bert Hubert <bert.hubert@netherlabs.nl>
2011-04-12 16:18:56 +0000

    when the year 2106 comes to TSIG, dnswriter.cc will be ready for it!
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2156 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dnswriter.cc
+++ pdns/dnswriter.cc
@@ -106,7 +106,7 @@ void DNSPacketWriter::addOpt(int udpsize, int extRCode, int Z, const vector<pair
 void DNSPacketWriter::xfr48BitInt(uint64_t val)
 {
   unsigned char bytes[6];
-  uint16_t theLeft = htons(val >> 32);
+  uint16_t theLeft = htons((val >> 32)&0xffffU);
   uint32_t theRight = htonl(val & 0xffffffffU);
   memcpy(bytes, (void*)&theLeft, 2);
   memcpy(bytes+2, (void*)&theRight, 4);
