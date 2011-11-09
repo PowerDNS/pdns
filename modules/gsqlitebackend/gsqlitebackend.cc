53e451678def7193832b41920a96530541e71383
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2011-11-09 09:13:20 +0000

    add zone-lastchange-query to gsqlitebackend, making it loadable again
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2299 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- modules/gsqlitebackend/gsqlitebackend.cc
+++ modules/gsqlitebackend/gsqlitebackend.cc
@@ -73,6 +73,7 @@ public:
     declare( suffix, "insert-record-query", "", "insert into records (content,ttl,prio,type,domain_id,name) values ('%s',%d,%d,'%s',%d,'%s')");
     declare( suffix, "update-serial-query", "", "update domains set notified_serial=%d where id=%d");
     declare( suffix, "update-lastcheck-query", "", "update domains set last_check=%d where id=%d");
+    declare( suffix, "zone-lastchange-query", "", "select max(change_date) from records where domain_id=%d");
     declare( suffix, "info-all-master-query", "", "select id,name,master,last_check,notified_serial,type from domains where type='MASTER'");
     declare( suffix, "delete-zone-query", "", "delete from records where domain_id=%d");
     declare(suffix,"check-acl-query","", "select value from acls where acl_type='%s' and acl_key='%s'");
