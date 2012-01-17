4372f8305a2e326e9c4495bffe3298a479fe18ae
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-01-17 11:40:15 +0000

    Fix TSLA certificate format. Patch by Pieter Lexis, confirmed by James Cloos.
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2347 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/dnsrecords.cc
+++ pdns/dnsrecords.cc
@@ -227,7 +227,7 @@ boilerplate_conv(TLSA, 65468,
         	 conv.xfr8BitInt(d_certusage); 
         	 conv.xfr8BitInt(d_selector); 
         	 conv.xfr8BitInt(d_matchtype); 
-        	 conv.xfrBlob(d_cert);
+        	 conv.xfrHexBlob(d_cert);
         	 )		 
 		 
 #undef DS
