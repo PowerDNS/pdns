7dbdd1e7685706282b3c73bf2e5988108257d9b9
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-01-19 12:36:32 +0000

    more testing/xml updates
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2353 d19b8d6e-7fed-0310-83ef-9ca221ded41b

---
+++ pdns/no-dnssec.schema.mysql.sql
@@ -0,0 +1,34 @@
+create table domains (
+ id		 INT auto_increment,
+ name		 VARCHAR(255) NOT NULL,
+ master		 VARCHAR(128) DEFAULT NULL,
+ last_check	 INT DEFAULT NULL,
+ type		 VARCHAR(6) NOT NULL,
+ notified_serial INT DEFAULT NULL, 
+ account         VARCHAR(40) DEFAULT NULL,
+ primary key (id)
+) Engine=InnoDB;
+
+CREATE UNIQUE INDEX name_index ON domains(name);
+
+CREATE TABLE records (
+  id              INT auto_increment,
+  domain_id       INT DEFAULT NULL,
+  name            VARCHAR(255) DEFAULT NULL,
+  type            VARCHAR(10) DEFAULT NULL,
+  content         VARCHAR(4096) DEFAULT NULL,
+  ttl             INT DEFAULT NULL,
+  prio            INT DEFAULT NULL,
+  change_date     INT DEFAULT NULL,
+  primary key(id)
+) Engine=InnoDB;
+
+CREATE INDEX rec_name_index ON records(name);
+CREATE INDEX nametype_index ON records(name,type);
+CREATE INDEX domain_id ON records(domain_id);
+
+create table supermasters (
+  ip VARCHAR(25) NOT NULL, 
+  nameserver VARCHAR(255) NOT NULL, 
+  account VARCHAR(40) DEFAULT NULL
+) Engine=InnoDB;
