6e06ce794db8ce8b8767e21d5bd32ec4931249ba
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-06-11 16:48:13 +0200

    tolerate domain list failure in master or slave mode

--- pdns/ueberbackend.cc
+++ pdns/ueberbackend.cc
@@ -67,6 +67,15 @@ int UeberBackend::s_s=-1; // ?
 #define RTLD_NOW RTLD_LAZY
 #endif
 
+// Helper function for contexts that conditionally want to rethrow AhuExceptions
+static void maybeThrowAhu(AhuException &ae, bool dothrow)
+{
+  if(dothrow)
+    throw;
+  else
+    L<<Logger::Warning<<"Ignoring exception from consistent backend: "<<ae.reason<<endl;
+}
+
 //! Loads a module and reports it to all UeberBackend threads
 bool UeberBackend::loadmodule(const string &name)
 {
@@ -242,7 +251,12 @@ void UeberBackend::getUnfreshSlaveInfos(vector<DomainInfo>* domains)
 {
   for ( vector< DNSBackend * >::iterator i = backends.begin(); i != backends.end(); ++i )
   {
-    ( *i )->getUnfreshSlaveInfos( domains );
+    try {
+      ( *i )->getUnfreshSlaveInfos( domains );
+    }
+    catch (AhuException &ae) {
+      maybeThrowAhu(ae, !::arg().mustDo("experimental-consistent-backends"));
+    }
   }  
 }
 
@@ -252,7 +266,12 @@ void UeberBackend::getUpdatedMasters(vector<DomainInfo>* domains)
 {
   for ( vector< DNSBackend * >::iterator i = backends.begin(); i != backends.end(); ++i )
   {
-    ( *i )->getUpdatedMasters( domains );
+    try {
+      ( *i )->getUpdatedMasters( domains );
+    }
+    catch (AhuException &ae) {
+      maybeThrowAhu(ae, !::arg().mustDo("experimental-consistent-backends"));
+    }
   }
 }
 
