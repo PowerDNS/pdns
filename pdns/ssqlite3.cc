4419dfc6934c5e6542f2db6463552ece7ecb8537
Bert Hubert <bert.hubert@netherlabs.nl>
2012-11-08 12:05:38 +0000

    slightly more helpful error message on not finding our sqlite3 database
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2866 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/ssqlite3.cc
+++ pdns/ssqlite3.cc
@@ -23,7 +23,7 @@ SSQLite3::SSQLite3( const std::string & database, bool creat )
   // Open the database connection.
   if(!creat) 
     if ( access( database.c_str(), F_OK ) == -1 )
-      throw sPerrorException( "SQLite database does not exist yet" );
+      throw sPerrorException( "SQLite database '"+database+"' does not exist yet" );
 
   if ( sqlite3_open( database.c_str(), &m_pDB)!=SQLITE_OK )
     throw sPerrorException( "Could not connect to the SQLite database '" + database + "'" );
