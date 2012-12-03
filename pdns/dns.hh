751ccfffd88f16107886ab01a17c407f3f916be0
Ruben d'Arco <cyclops@prof-x.net>
2012-12-03 04:51:25 +0100

    Add RCodes for RFC2136
    This adds the needed RCodes to support RFC2136 (YXDomain, NotZone and others)

--- pdns/dns.hh
+++ pdns/dns.hh
@@ -57,7 +57,7 @@ struct SOAData
 class RCode
 {
 public:
-  enum rcodes_ { NoError=0, FormErr=1, ServFail=2, NXDomain=3, NotImp=4, Refused=5, NotAuth=9 };
+  enum rcodes_ { NoError=0, FormErr=1, ServFail=2, NXDomain=3, NotImp=4, Refused=5, YXDomain=6, YXRRSet=7, NXRRSet=8, NotAuth=9, NotZone=10};
 };
 
 class Opcode
