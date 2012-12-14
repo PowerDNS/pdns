c1ee58953d5d4db93176fa34dd2528d3d8d3f342
Bert Hubert <bert.hubert@netherlabs.nl>
2012-12-14 18:15:13 +0000

    d.root-servers.net is going to move, the new IP address works already, so we are going to use it!
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2990 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/speedtest.cc
+++ pdns/speedtest.cc
@@ -309,7 +309,7 @@ vector<uint8_t> makeRootReferral()
   DNSPacketWriter pw(packet, "outpost.ds9a.nl", QType::SOA);
 
   // nobody reads what we output, but it appears to be the magic that shuts some nameservers up
-  static const char*ips[]={"198.41.0.4", "192.228.79.201", "192.33.4.12", "128.8.10.90", "192.203.230.10", "192.5.5.241", "192.112.36.4", "128.63.2.53", 
+  static const char*ips[]={"198.41.0.4", "192.228.79.201", "192.33.4.12", "199.7.91.13", "192.203.230.10", "192.5.5.241", "192.112.36.4", "128.63.2.53", 
         	     "192.36.148.17","192.58.128.30", "193.0.14.129", "198.32.64.12", "202.12.27.33"};
   static char templ[40];
   strncpy(templ,"a.root-servers.net", sizeof(templ) - 1);
