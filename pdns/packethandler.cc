f89fb8979f1cb6b6bf9c7f1214cf993bd4b86c49
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-01-05 13:00:10 +0000

    clarify error message, suggested by Michelle Sullivan and Jose Arthur Benetasso Villanova
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@3022 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/packethandler.cc
+++ pdns/packethandler.cc
@@ -1383,13 +1383,13 @@ DNSPacket *PacketHandler::questionOrRecurse(DNSPacket *p, bool *shouldRecurse)
       PC.insert(p, r, r->getMinTTL()); // in the packet cache
   }
   catch(DBException &e) {
-    L<<Logger::Error<<"Database module reported condition which prevented lookup ("+e.reason+") sending out servfail"<<endl;
+    L<<Logger::Error<<"Backend reported condition which prevented lookup ("+e.reason+") sending out servfail"<<endl;
     r->setRcode(RCode::ServFail);
     S.inc("servfail-packets");
     S.ringAccount("servfail-queries",p->qdomain);
   }
   catch(AhuException &e) {
-    L<<Logger::Error<<"Database module reported permanent error which prevented lookup ("+e.reason+") sending out servfail"<<endl;
+    L<<Logger::Error<<"Backend reported permanent error which prevented lookup ("+e.reason+") sending out servfail"<<endl;
     throw; // we WANT to die at this point
   }
   catch(std::exception &e) {
