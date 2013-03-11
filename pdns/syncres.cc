450292c01f9b35f7ff99abcf5a13ae731f58bc2d
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-03-11 09:32:28 +0000

    accept ANY responses during recursive forwarding, thanks Jan-Piet Mens
    git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@3116 d19b8d6e-7fed-0310-83ef-9ca221ded41b

--- pdns/syncres.cc
+++ pdns/syncres.cc
@@ -1053,10 +1053,10 @@ int SyncRes::doResolveAt(set<string, CIStringCompare> nameservers, string auth, 
           ret.push_back(*i);
           newtarget=i->content;
         }
-        // for ANY answers we *must* have an authoritive answer
+        // for ANY answers we *must* have an authoritive answer, unless we are forwarding recursively
         else if(i->d_place==DNSResourceRecord::ANSWER && pdns_iequals(i->qname, qname) && 
         	(
-        	 i->qtype==qtype || (lwr.d_aabit && (qtype==QType(QType::ANY) || magicAddrMatch(qtype, i->qtype) ) )
+        	 i->qtype==qtype || (lwr.d_aabit && (qtype==QType(QType::ANY) || magicAddrMatch(qtype, i->qtype) ) ) || sendRDQuery
         	) 
                )   
           {
