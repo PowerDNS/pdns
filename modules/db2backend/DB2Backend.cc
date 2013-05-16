d8ec70ff32fba32c914a21c5f1e0c41048b898b1
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-05-16 15:38:53 +0200

    fix db2 backend includes (#653)

--- modules/db2backend/DB2Backend.cc
+++ modules/db2backend/DB2Backend.cc
@@ -7,13 +7,13 @@
 
 #include "pdns/namespaces.hh"
 
-#include <dns.hh>
-#include <dnsbackend.hh>
-#include <dnspacket.hh>
-#include <ueberbackend.hh>
-#include <ahuexception.hh>
-#include <logger.hh>
-#include <arguments.hh>
+#include <pdns/dns.hh>
+#include <pdns/dnsbackend.hh>
+#include <pdns/dnspacket.hh>
+#include <pdns/ueberbackend.hh>
+#include <pdns/ahuexception.hh>
+#include <pdns/logger.hh>
+#include <pdns/arguments.hh>
 
 #include "DB2Exception.hh"
 #include "DB2Backend.hh"
