942f9f8aa52d73ac792b675049fbe3b5829404a8
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-07-04 20:26:29 +0200

    drop useless index

--- pdns/no-dnssec.schema.mysql.sql
+++ pdns/no-dnssec.schema.mysql.sql
@@ -23,7 +23,6 @@ CREATE TABLE records (
   primary key(id)
 ) Engine=InnoDB;
 
-CREATE INDEX rec_name_index ON records(name);
 CREATE INDEX nametype_index ON records(name,type);
 CREATE INDEX domain_id ON records(domain_id);
 
