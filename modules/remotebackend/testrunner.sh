663fedf90107a5d2b0c394a637a8703496b49b87
Aki Tuomi <cmouse@desteem.org>
2013-07-09 10:29:09 +0300

    Test suite for unix connector

--- modules/remotebackend/testrunner.sh
+++ modules/remotebackend/testrunner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
 webrick_pid=""
+socat_pid=""
+socat=/usr/bin/socat
 
 function start_web() {
   if [ x"$REMOTEBACKEND_HTTP" == "xyes" ]; then
@@ -30,12 +32,46 @@ function stop_web() {
  fi
 }
 
+function start_unix() {
+  if [ ! -x $socat ]; then
+     echo "Cannot find socat - cannot test (non-fatal)"
+     exit 0
+  fi
+  
+  $socat unix-listen:/tmp/remotebackend.sock exec:./unittest_pipe.rb &
+  socat_pid=$!
+}
+
+function stop_unix() {
+ if [ ! -z "$socat_pid" ]; then
+   kill -TERM $socat_pid 2>/dev/null
+   if [ $? -ne 0 ]; then
+     # already dead
+     return 
+   fi
+   # wait a moment for it to die
+   i=0
+   while [ $i -lt 5 ]; do
+     sleep 1
+     kill -0 $socat_pid 2>/dev/null
+     if [ $? -ne 0 ]; then break; fi
+     let i=i+1
+   done
+ fi
+}
+
 mode=`basename "$1"`
 
 case "$mode" in
   test_remotebackend_pipe)
     ./test_remotebackend_pipe
     rv=$?
+  ;;
+  test_remotebackend_unix)
+    start_unix
+    ./test_remotebackend_unix
+    rv=$?
+    stop_unix
   ;;
   test_remotebackend_http)
     start_web "http"
