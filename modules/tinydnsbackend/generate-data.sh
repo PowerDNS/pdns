e3243e7b911f39c87442cac3a4e9103cec512c09
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-11-04 08:22:54 +0100

    fix tinydns generate-data.sh

--- modules/tinydnsbackend/generate-data.sh
+++ modules/tinydnsbackend/generate-data.sh
@@ -32,7 +32,8 @@ cd ../../regression-tests
 ../pdns/pdns_server --daemon=no --local-port=5300 --socket-dir=./ \
 	--no-shuffle --launch=bind --bind-config=../regression-tests/named.conf \
 	--fancy-records --query-logging --send-root-referral --loglevel=0 \
-	--cache-ttl=0 --no-config --local-address=127.0.0.1 &
+	--cache-ttl=0 --no-config --local-address=127.0.0.1 \
+	--bind-ignore-broken-records=yes &
 
 # wait for pdns to start up
 sleep 5
