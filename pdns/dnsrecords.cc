c86e1f204dbe2c759513634c593fd63622d9a58c
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2013-11-12 11:25:37 +0100

    fix CHAOS TXT registration for recursor (making version.bind work)

--- pdns/dnsrecords.cc
+++ pdns/dnsrecords.cc
@@ -484,7 +484,7 @@ void reportBasicTypes()
   SOARecordContent::report();
   SRVRecordContent::report();
   PTRRecordContent::report();
-  //DNSRecordContent::regist(3, ns_t_txt, &TXTRecordContent::make, &TXTRecordContent::make, "TXT");
+  DNSRecordContent::regist(3, ns_t_txt, &TXTRecordContent::make, &TXTRecordContent::make, "TXT");
   TXTRecordContent::report();
   DNSRecordContent::regist(1, QType::ANY, 0, 0, "ANY");
 }
