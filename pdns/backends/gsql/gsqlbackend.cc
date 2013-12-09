shangyanjin/powerdns

fix error message in GSQLBackend::isMaster

pdns/backends/gsql/gsqlbackend.cc
@@ -89,7 +89,7 @@ bool GSQLBackend::isMaster(const string &domain, const string &ip)
     d_db->doQuery(output, d_result);
   }
   catch (SSqlException &e) {
-    throw AhuException("GSQLBackend unable to retrieve list of master domains: "+e.txtReason());
+    throw AhuException("GSQLBackend unable to check master status of domain "+domain+": "+e.txtReason());
   }
 
   if(d_result.empty())

PowerDNS/pdns

pdns/backends/gsql/gsqlbackend.cc
@@ -90,7 +90,7 @@ bool GSQLBackend::isMaster(const string &domain, const string &ip)
     d_db->doQuery(output, d_result);
   }
   catch (SSqlException &e) {
-    throw PDNSException("GSQLBackend unable to retrieve list of master domains: "+e.txtReason());
+    throw PDNSException("GSQLBackend unable to check master status of domain "+domain+": "+e.txtReason());
   }
 
   if(d_result.empty())