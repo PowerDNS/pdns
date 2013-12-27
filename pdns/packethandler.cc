9245fd9fd47b2eb7a02b9076f3eedc15083883ca
Kees Monshouwer <mind04@monshouwer.org>
2013-12-27 15:54:20 +0100

    don't addSuckRequest after supermaster zone creation to avoid simultaneous AXFR for the new zone

--- pdns/packethandler.cc
+++ pdns/packethandler.cc
@@ -793,8 +793,7 @@ int PacketHandler::trySuperMasterSynchronous(DNSPacket *p)
     L<<Logger::Error<<"Database error trying to create "<<p->qdomain<<" for potential supermaster "<<p->getRemote()<<": "<<ae.reason<<endl;
     return RCode::ServFail;
   }
-  Communicator.addSuckRequest(p->qdomain, p->getRemote());  
-  L<<Logger::Warning<<"Created new slave zone '"<<p->qdomain<<"' from supermaster "<<p->getRemote()<<", queued AXFR"<<endl;
+  L<<Logger::Warning<<"Created new slave zone '"<<p->qdomain<<"' from supermaster "<<p->getRemote()<<endl;
   return RCode::NoError;
 }
 
