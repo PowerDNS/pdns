acfe262418aa0d4da94767c96fb69e0adc250732
Zack Allen <zack@riskive.com>
2013-12-30 12:10:15 -0500

    Fixed spacing hiccup in git

--- pdns/lua-recursor.hh
+++ pdns/lua-recursor.hh
@@ -13,7 +13,9 @@ public:
   bool nxdomain(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   bool nodata(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   bool postresolve(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
+
 private:
   bool passthrough(const string& func, const ComboAddress& remote,const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable);
 };
+
 #endif
