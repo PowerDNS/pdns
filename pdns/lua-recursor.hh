29edbc473777914365d7b8a22fbf04ab7a7cef1d
Zack Allen <zack@riskive.com>
2013-12-30 12:09:06 -0500

    Added enum namespace to lua-pdns to prevent magic number usage when referring to pdns_recursor return types for pdns.DROP and pdns.PASS

--- pdns/lua-recursor.hh
+++ pdns/lua-recursor.hh
@@ -13,9 +13,7 @@ public:
   bool nxdomain(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   bool nodata(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   bool postresolve(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
-
 private:
   bool passthrough(const string& func, const ComboAddress& remote,const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable);
 };
-
 #endif
