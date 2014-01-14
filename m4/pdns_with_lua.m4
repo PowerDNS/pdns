PowerDNS/pdns

commit 051f0aaa9e4c826d5c1b9283a61eb81e9f975d20

Move lua check to separate macro

m4/pdns_with_lua.m4
@@ -1,0 +1,37 @@
+AC_DEFUN([PDNS_WITH_LUA],[
+  dnl Check for lua
+  AC_ARG_WITH([lua],
+    [AS_HELP_STRING([--with-lua],
+      [Build Lua Bindings @<:@default=yes@:>@])],
+    [with_lua=$withval],
+    [with_lua=yes])
+
+  AS_IF([test "x$with_lua" != "xno"],[
+    AS_IF([test "x$with_lua" = "xyes"],
+      [LUAPC=lua],
+      [LUAPC=$with_lua])
+
+    PKG_CHECK_MODULES([LUA], $LUAPC >= 5.1, [
+      AC_DEFINE([HAVE_LUA], [1], [liblua])
+      AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
+      with_lua=yes
+    ],[
+      LUAPC=lua5.1
+      PKG_CHECK_MODULES([LUA], $LUAPC >= 5.1, [
+        AC_DEFINE([HAVE_LUA], [1], [liblua])
+        AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
+        with_lua=yes
+      ],[
+        LUAPC=lua-5.1
+        PKG_CHECK_MODULES([LUA], $LUAPC >= 5.1, [
+          AC_DEFINE([HAVE_LUA], [1], [liblua])
+          AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
+          with_lua=yes
+        ],[
+          AC_DEFINE([HAVE_LUA],["x"],["x"])
+          with_lua=no
+        ])
+      ])
+    ])
+  ])
+])

configure.ac
@@ -24,42 +24,13 @@ AC_PROG_MAKE_SET
 AC_PROG_LIBTOOL
 AC_LANG([C++])
 
-AC_CHECK_PROG([RAGEL], [ragel], [ragel])
-AC_CHECK_PROG([ASCIIDOC], [asciidoc], [asciidoc])
-
-dnl Check for lua
-AC_MSG_CHECKING(if with lua)
-AC_ARG_WITH(lua, AS_HELP_STRING([--with-lua],[use Lua]), [WITH_LUA=$withval],[WITH_LUA=yes])
-
 # detect pkg-config explicitly
 PKG_PROG_PKG_CONFIG
 
-AC_MSG_RESULT($WITH_LUA)
-if test "$WITH_LUA" != "no"; then
- # try pkgconfig
- if test "$WITH_LUA" = "yes"; then
-      LUAPC=lua
- else
-      LUAPC=$WITH_LUA
- fi
- PKG_CHECK_MODULES(LUA, $LUAPC >= 5.1, [
- 	AC_DEFINE([HAVE_LUA], [1], [liblua])
-        AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
-        ],[
-        PKG_CHECK_MODULES(LUA, lua5.1 >= 5.1, [
-        	AC_DEFINE([HAVE_LUA], [1], [liblua])
-                AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
-        ],[
-        PKG_CHECK_MODULES(LUA, lua-5.1 >= 5.1, [
-        	AC_DEFINE([HAVE_LUA], [1], [liblua])
-                AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
-                ]) 
-                ]) 
-        ])
- AC_SUBST(LUA_CFLAGS)
- AC_SUBST(LUA_LIBS)
-fi
+AC_CHECK_PROG([RAGEL], [ragel], [ragel])
+AC_CHECK_PROG([ASCIIDOC], [asciidoc], [asciidoc])
 
+PDNS_WITH_LUA
 
 AM_CONDITIONAL([RELEASE_BUILD], [test "$PACKAGE_VERSION" != "git"])
 
@@ -73,6 +44,8 @@ if test x"$enable_unit_tests" = "xyes"; then
    BOOST_TEST([mt])
 fi
 #BOOST_SYSTEM([mt])
+
+
 
 dnl Allow user to require SQLite3
 AC_MSG_CHECKING(whether user requires sqlite3)

commit 1d34e101981ad6aac5116ca03f132655a8e5ba58

Fix build without Lua
Broken since 051f0aaa9e4c826d5c1b9283a61eb81e9f975d20.

m4/pdns_with_lua.m4
@@ -27,7 +27,6 @@ AC_DEFUN([PDNS_WITH_LUA],[
           AC_DEFINE([HAVE_LUA_H], [1], [lua.h])
           with_lua=yes
         ],[
-          AC_DEFINE([HAVE_LUA],["x"],["x"])
           with_lua=no
         ])
       ])