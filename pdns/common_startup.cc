18179c616ccf1187c4d87a24be467941194e3c5e
Christian Hofstaedtler <christian@hofstaedtler.name>
2014-03-03 20:18:44 +0100

    Add api-readonly switch
    This disables all JSON-API-level changes to backend data.

--- pdns/common_startup.cc
+++ pdns/common_startup.cc
@@ -65,6 +65,7 @@ void declareArguments()
   
   ::arg().set("retrieval-threads", "Number of AXFR-retrieval threads for slave operation")="2";
   ::arg().setSwitch("experimental-json-interface", "If the webserver should serve JSON data")="no";
+  ::arg().setSwitch("experimental-api-readonly", "If the JSON API should disallow data modification")="no";
 
   ::arg().setCmd("help","Provide a helpful message");
   ::arg().setCmd("version","Output version and compilation date");
