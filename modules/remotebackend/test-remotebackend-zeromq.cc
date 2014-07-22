085132062264b348616885105f7faec8786f976a
Aki Tuomi <cmouse@desteem.org>
2014-07-22 22:59:10 +0300

    Use zeromq library C API, based on work by @wtfuzz and @hexwave. Fixes #1760.

--- modules/remotebackend/test-remotebackend-zeromq.cc
+++ modules/remotebackend/test-remotebackend-zeromq.cc
@@ -49,7 +49,7 @@ struct RemotebackendSetup {
                 new RemoteLoader();
 		BackendMakers().launch("remote");
                 // then get us a instance of it 
-                ::arg().set("remote-connection-string")="zeromq:endpoint=tcp://127.0.0.1:43622";
+                ::arg().set("remote-connection-string")="zeromq:endpoint=ipc:///tmp/remotebackend.0";
                 ::arg().set("remote-dnssec")="yes";
                 be = BackendMakers().all()[0];
 		// load few record types to help out
