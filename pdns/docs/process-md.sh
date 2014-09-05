ec34002f850be0bfb02c0a910f40f866db80b3da
Pieter Lexis <pieter@plexis.eu>
2014-09-06 00:32:26 +0200

    Make the tables pretty

--- pdns/docs/process-md.sh
+++ pdns/docs/process-md.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-for file in `find doc-build -name '*.md' -type f -print`; do
-  pandoc -f markdown_github+pipe_tables -t markdown_github+pipe_tables -F markdown/process-includes.py $file -o $file
-done
+pre() {
+  for file in `find doc-build -name '*.md' -type f -print`; do
+    pandoc -f markdown_github+pipe_tables -t markdown_github+pipe_tables -F markdown/process-includes.py $file -o $file
+  done
+}
+
+post() {
+  find html-new -type f -name '*.html' -exec sed -i 's/<table>/<table class="table-bordered">/' {} +
+}
+
+$1
