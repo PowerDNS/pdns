e11f73c60bf9802ca49a6d3b6dd16705515173da
Pieter Lexis <pieter@plexis.eu>
2014-12-19 07:40:59 +0100

    Process docs differently (closes #1962)

--- pdns/docs/process-md.sh
+++ pdns/docs/process-md.sh
@@ -3,18 +3,17 @@
 pre() {
   for file in `find doc-build -name '*.md' -type f -print`; do
     pandoc -f markdown_github+pipe_tables -t markdown_github+pipe_tables -F markdown/process-includes.py $file -o $file
+    perl -i -p \
+    -e 's/\\([\$\*\^><])/\1/g;' \
+    $file
   done
 }
 
 post() {
   # Change the following:
-  # 1. Add class="table-bordered" to tables
-  # 2. Fix &gt; and &lt; escaping fuckery
-  # 3. Fix $-sign escaping insanity
+  # Add class="table-bordered" to tables
   find html-new -type f -name '*.html' -exec perl -i -p \
     -e 's/\<table>/<table class="table-bordered">/;' \
-    -e 's/\\&(gt|lt)/&\1/;' \
-    -e 's/\\\\/\\/g;' \
     {} +
 }
 
