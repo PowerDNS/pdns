18b7333828a1275ae5f5574a9c8330290d8557ff
Kees Monshouwer <mind04@monshouwer.org>
2015-01-13 12:54:45 +0100

    recursor: limit long version strings to 63 characters and catch exceptions in secpoll

--- pdns/pdns_recursor.cc
+++ pdns/pdns_recursor.cc
@@ -1317,7 +1317,10 @@ try
     }
 
     if(now.tv_sec - last_secpoll >= 3600) {
-      doSecPoll(&last_secpoll);
+      try {
+        doSecPoll(&last_secpoll);
+      }
+      catch(...) {}
     }
   }
 }
