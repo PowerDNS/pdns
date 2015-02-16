013824a0ecd83ca57a9bec9e615aa62e617cb248
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2015-02-16 10:29:11 +0100

    Avoid warning: ISO C++11 does not allow conversion from string literal to 'char *' [-Wc++11-compat-deprecated-writable-strings]

--- pdns/test-bindparser_cc.cc
+++ pdns/test-bindparser_cc.cc
@@ -15,7 +15,7 @@ using std::string;
 BOOST_AUTO_TEST_SUITE(bindparser_cc)
 
 BOOST_AUTO_TEST_CASE(test_parser) {
-        char *srcdir;
+        const char *srcdir;
         std::ostringstream pathbuf;
         BindParser BP;
         BOOST_CHECK_THROW( BP.parse("../regression-tests/named.confx"), PDNSException);
