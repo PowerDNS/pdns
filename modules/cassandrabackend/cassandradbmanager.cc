ce728984671efeabc4c08f38db57a16e824fd566
sksumit1 <sksumit1@gmail.com>
2015-08-29 16:45:42 +0530

    setting retry policy

--- modules/cassandrabackend/cassandradbmanager.cc
+++ modules/cassandrabackend/cassandradbmanager.cc
@@ -82,6 +82,8 @@ cassandradbmanager::cassandradbmanager()
 	  cass_cluster_set_max_concurrent_requests_threshold(cluster,concurrent_requests_threshold);
 	  cass_cluster_set_connect_timeout(cluster,connect_timeout);
 	  cass_cluster_set_request_timeout(cluster,request_timeout);
+	  CassRetryPolicy* downgrading_policy = cass_retry_policy_downgrading_consistency_new();
+	  cass_cluster_set_retry_policy(cluster, downgrading_policy);
 	  if(enable_load_balance_round_robin == 1) {
 		  cass_cluster_set_load_balance_round_robin(cluster);
 	  }
