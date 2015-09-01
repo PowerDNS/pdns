e6a67b97c228b40c33775bfb819866e95f7493af
Aki Tuomi <cmouse@desteem.org>
2015-09-01 11:47:30 +0300

    Use std::distance in dnsname, fix #2721

--- pdns/dnsname.cc
+++ pdns/dnsname.cc
@@ -112,8 +112,8 @@ bool DNSName::isPartOf(const DNSName& parent) const
     return false;
 
   // this is slightly complicated since we can't start from the end, since we can't see where a label begins/ends then
-  for(auto us=d_storage.cbegin(); us<d_storage.cend() && d_storage.cend()-us >= (unsigned int)parent.d_storage.size(); us+=*us+1) {
-    if (d_storage.cend()-us == (unsigned int)parent.d_storage.size()) {
+  for(auto us=d_storage.cbegin(); us<d_storage.cend() && std::distance(us,d_storage.cend()) >= static_cast<unsigned int>(parent.d_storage.size()); us+=*us+1) {
+    if (std::distance(us,d_storage.cend()) == static_cast<unsigned int>(parent.d_storage.size())) {
       auto p = parent.d_storage.cbegin();
       for(; us != d_storage.cend(); ++us, ++p) {
         if(tolower(*p) != tolower(*us))
