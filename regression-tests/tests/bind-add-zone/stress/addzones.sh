d9c6538ebc184bf724e4b04a9c592508dcd243b2
Pieter Lexis <pieter.lexis@powerdns.com>
2015-09-09 15:56:48 +0200

    regression tests: Use environment vars to determine program location
    This wil allow us to run the regression tests on installed PowerDNS
    versions.

--- regression-tests/tests/bind-add-zone/stress/addzones.sh
+++ regression-tests/tests/bind-add-zone/stress/addzones.sh
@@ -3,8 +3,8 @@
 for f in $(seq 1 $AMOUNT); do
     f=addzone$f.com
     dig ns1.$f @localhost -p $port > /dev/null 2>&1
-    ../pdns/pdns_control --config-dir=. bind-add-zone $f $TMP/$f
-    ../pdns/pdns_control --config-dir=. purge $f
+    $PDNSCONTROL --config-dir=. bind-add-zone $f $TMP/$f
+    $PDNSCONTROL --config-dir=. purge $f
     sleep 0.5;
     dig ns1.$f @localhost -p $port > /dev/null 2>&1
 
