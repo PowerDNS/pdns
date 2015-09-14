c0f5a61ff42b3da9b5555131fecf33ae8989186e
Aki Tuomi <cmouse@cmouse.fi>
2015-09-14 11:37:09 +0300

    Set scopemask for result during reply

--- modules/geoipbackend/geoipbackend.cc
+++ modules/geoipbackend/geoipbackend.cc
@@ -198,6 +198,7 @@ void GeoIPBackend::lookup(const QType &qtype, const DNSName& qdomain, DNSPacket 
     map<DNSName, vector<DNSResourceRecord> >::iterator i = dom.records.find(format);
     BOOST_FOREACH(DNSResourceRecord rr, i->second) {
       if (qtype == QType::ANY || rr.qtype == qtype) {
+        rr.scopeMask = (v6 ? 128 : 32);
         d_result.push_back(rr);
         d_result.back().qname = qdomain;
       }
