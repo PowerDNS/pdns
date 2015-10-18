748fc8db2a61045befc8b2c40e6373dd5776d72e
Aki Tuomi <cmouse@cmouse.fi>
2015-10-18 21:56:46 +0300

    Initialize DNSRecord
    Fixes CID#1325471

--- pdns/dnsparser.hh
+++ pdns/dnsparser.hh
@@ -265,7 +265,13 @@ protected:
 
 struct DNSRecord
 {
-  DNSRecord() = default;
+  DNSRecord() {
+    d_type = 0;
+    d_class = QClass::IN;
+    d_ttl = 0;
+    d_clen = 0;
+    d_place = DNSResourceRecord::ANSWER;
+  }
   explicit DNSRecord(const DNSResourceRecord& rr);
   DNSName d_name;
   std::shared_ptr<DNSRecordContent> d_content;
