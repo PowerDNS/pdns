9153c7cfa40f4ba99040136ea298775eccec39ff
Aki Tuomi <cmouse@cmouse.fi>
2015-10-19 08:42:04 +0300

    Convert tabs to spaces

--- modules/tinydnsbackend/generate-data.sh
+++ modules/tinydnsbackend/generate-data.sh
@@ -12,28 +12,28 @@ tinydnsdata=$2
 
 if [ ! -x $axfrget ] || [ "$axfrget" = help ] || [ ! -x $tinydnsdata ] || [ "$tinydnsdata" = help ]
 then
-	echo "Usage: ./generate-data.sh <axfr-get location> <tinydns-data location>";
-	exit 1
+  echo "Usage: ./generate-data.sh <axfr-get location> <tinydns-data location>";
+  exit 1
 fi
 
 
 # Copy original zones because the test might modify them (well only the dyndns stuff, but let's make this work for others as well)
 for zone in $(grep 'zone ' ../../regression-tests/named.conf | cut -f2 -d\")
 do
-	if [ -f ../../regression-tests/zones/$zone.orig ]
-	then
-		cp -f ../../regression-tests/zones/$zone.orig ../../regression-tests/zones/$zone
-	fi
+  if [ -f ../../regression-tests/zones/$zone.orig ]
+  then
+    cp -f ../../regression-tests/zones/$zone.orig ../../regression-tests/zones/$zone
+  fi
 done
 
 
 # CD to regression testt because named.conf has relative paths.
 cd ../../regression-tests
 ../pdns/pdns_server --daemon=no --local-port=5300 --socket-dir=./ \
-	--no-shuffle --launch=bind --bind-config=../regression-tests/named.conf \
-	--query-logging --send-root-referral --loglevel=0 \
-	--cache-ttl=0 --no-config --local-address=127.0.0.1 \
-	--bind-ignore-broken-records=yes &
+  --no-shuffle --launch=bind --bind-config=../regression-tests/named.conf \
+  --query-logging --send-root-referral --loglevel=0 \
+  --cache-ttl=0 --no-config --local-address=127.0.0.1 \
+  --bind-ignore-broken-records=yes &
 
 # wait for pdns to start up
 sleep 5
@@ -44,9 +44,9 @@ cd $startdir
 
 for zone in $(grep 'zone ' ../../regression-tests/named.conf | cut -f2 -d\")
 do
-	$tcpclient 127.0.0.1 5300 $axfrget $zone $zone.out $zone.out.tmp
-	cat $zone.out >> data
-	rm $zone.out
+  $tcpclient 127.0.0.1 5300 $axfrget $zone $zone.out $zone.out.tmp
+  cat $zone.out >> data
+  rm $zone.out
 done
 
 $tinydnsdata  
