7da873e9210bdaff73e9b436a4d1657f19bae600
Aki Tuomi <cmouse@cmouse.fi>
2015-10-26 18:12:35 +0200

    Add remark about not removing the throw

--- pdns/distributor.hh
+++ pdns/distributor.hh
@@ -203,6 +203,7 @@ template<class Answer, class Question, class Backend>void *MultiThreadDistributo
 
 	delete QD->Q;
 	QD->callback(a);
+	// do NOT remove this. We want to die on PDNSException
 	throw;
       }
       catch(...) {
