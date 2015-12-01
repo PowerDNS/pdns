ad2ad9017abf4eb2a74d4898f0710eddf02885c8
bert hubert <bert.hubert@netherlabs.nl>
2015-12-01 17:39:20 +0100

    default zone for zoneparser should be the root, not the "empty DNSName"

--- pdns/zoneparser-tng.hh
+++ pdns/zoneparser-tng.hh
@@ -1,6 +1,6 @@
 /*
     PowerDNS Versatile Database Driven Nameserver
-    Copyright (C) 2005 - 2007 PowerDNS.COM BV
+    Copyright (C) 2005 - 2015 PowerDNS.COM BV
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2 
@@ -32,7 +32,7 @@
 class ZoneParserTNG
 {
 public:
-  ZoneParserTNG(const string& fname, const DNSName& zname=DNSName(), const string& reldir="");
+  ZoneParserTNG(const string& fname, const DNSName& zname=DNSName("."), const string& reldir="");
   ZoneParserTNG(const vector<string> zonedata, const DNSName& zname);
 
   ~ZoneParserTNG();
