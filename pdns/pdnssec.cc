ce92ff1b221209d7ddd2fbe1fa0ad09b52fcd634
Kees Monshouwer <mind04@monshouwer.org>
2016-02-02 18:46:56 +0100

    pdnssec: check all records (including disabled ones) in verbose mode

--- pdns/pdnssec.cc
+++ pdns/pdnssec.cc
@@ -446,7 +446,7 @@ int checkZone(DNSSECKeeper &dk, UeberBackend &B, const std::string& zone)
   ostringstream content;
   pair<map<string, unsigned int>::iterator,bool> ret;
 
-  sd.db->list(zone, sd.domain_id, false);
+  sd.db->list(zone, sd.domain_id, g_verbose);
 
   while(sd.db->get(rr)) {
     if(!rr.qtype.getCode())
