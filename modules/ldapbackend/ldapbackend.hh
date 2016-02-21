c7478be41df2089f0eff0ddec4bbcee14b900301
Peter van Dijk <peter.van.dijk@powerdns.com>
2016-02-21 22:17:00 +0100

    fix strict mode autoptr

--- modules/ldapbackend/ldapbackend.hh
+++ modules/ldapbackend/ldapbackend.hh
@@ -113,6 +113,7 @@ class LdapBackend : public DNSBackend
         vector<string>::iterator m_value;
         vector<DNSName>::iterator m_adomain;
         vector<DNSName> m_adomains;
+        QType m_qtype;
 
         bool (LdapBackend::*m_list_fcnt)( const DNSName&, int );
         void (LdapBackend::*m_lookup_fcnt)( const QType&, const DNSName&, DNSPacket*, int );
