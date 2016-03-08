26255889083f0e5b759a45be2644b2a3478bf4ce
Christian Hofstaedtler <christian.hofstaedtler@deduktiva.com>
2016-03-08 07:53:00 -0300

    Remove unused function simpleExpandTo

--- pdns/dnsparser.hh
+++ pdns/dnsparser.hh
@@ -343,7 +343,6 @@ private:
 };
 
 string simpleCompress(const string& label, const string& root="");
-void simpleExpandTo(const string& label, unsigned int frompos, string& ret);
 void ageDNSPacket(char* packet, size_t length, uint32_t seconds);
 void ageDNSPacket(std::string& packet, uint32_t seconds);
 uint32_t getDNSPacketMinTTL(const char* packet, size_t length);
