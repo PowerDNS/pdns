e512f4f99403601638a08f738a55d9854caa8680
Andrew Nelless <andrew@nelless.net>
2016-03-10 19:59:50 +0000

    Fix Boost component library detection.
    On some platforms the Boost context headers are being shipped on
    architectures where the library can't be built. Here we patch
    boost.m4 to set HAVE_BOOST_<component> definitions based on
    whether the library actually exists.
    
    This should also fix builds against Boost 1.58.0, which shipped
    a broken context/all.hpp header when compiling in modes before
    C++14 (see: https://svn.boost.org/trac/boost/ticket/11223 )

--- pdns/mtasker_context.cc
+++ pdns/mtasker_context.cc
@@ -1,10 +1,7 @@
-#include <boost/version.hpp>
+#include "config.h"
 
-/* Boost Context was introduced in 1.51 (Aug 2012), but it's probably not worth
- * supporting it because there was an immediate API break in 1.52 (Nov 2012)
- */
-#if BOOST_VERSION <= 105100
-#include "mtasker_ucontext.cc"
-#else
+#if defined(HAVE_BOOST_CONTEXT)
 #include "mtasker_fcontext.cc"
+#else
+#include "mtasker_ucontext.cc"
 #endif
