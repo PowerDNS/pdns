798dedd18c467d6a5d708dd8e8e4d6ceb493f0b3
Kees Monshouwer <mind04@monshouwer.org>
2016-04-01 22:06:37 +0200

    Revert "Make pdns die on fatal error"
    This reverts commit ff1ac70002cc705285a58daf4dcb6ccbb4b7e312.

--- pdns/distributor.hh
+++ pdns/distributor.hh
@@ -202,8 +202,6 @@ template<class Answer, class Question, class Backend>void *MultiThreadDistributo
         S.ringAccount("servfail-queries",QD->Q->qdomain.toString());
 
 	delete QD->Q;
-	QD->callback(a);
-	throw;
       }
       catch(...) {
         L<<Logger::Error<<"Caught unknown exception in Distributor thread "<<(long)pthread_self()<<endl;
@@ -225,7 +223,6 @@ template<class Answer, class Question, class Backend>void *MultiThreadDistributo
   }
   catch(const PDNSException &AE) {
     L<<Logger::Error<<"Distributor caught fatal exception: "<<AE.reason<<endl;
-    throw;
   }
   catch(...) {
     L<<Logger::Error<<"Caught an unknown exception when creating backend, probably"<<endl;
@@ -247,8 +244,6 @@ template<class Answer, class Question, class Backend>int SingleThreadDistributor
     a->setRcode(RCode::ServFail);
     S.inc("servfail-packets");
     S.ringAccount("servfail-queries",q->qdomain.toString());
-    callback(a);
-    throw;
   }
   catch(...) {
     L<<Logger::Error<<"Caught unknown exception in Distributor thread "<<(unsigned long)pthread_self()<<endl;
