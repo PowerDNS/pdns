fc3c07b4745fd08d01bf20250ecb9ef5acb46890
Pieter Lexis <pieter.lexis@powerdns.com>
2016-07-01 10:41:41 +0200

    Hopefully appease the License Gods (again)

--- pdns/sodcrypto.cc
+++ pdns/sodcrypto.cc
@@ -1,3 +1,23 @@
+/*  Copyright (C) 2016 PowerDNS.COM BV
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  Additionally, the license of this program contains a special
+ *  exception which allows to distribute the program in binary form when
+ *  it is linked against OpenSSL.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <iostream>
 #include "namespaces.hh"
 #include "misc.hh"
