115f658ee2000a4cdcc13e999da50b3634c6a907
Remi Gacogne <remi.gacogne@powerdns.com>
2016-08-12 09:52:08 +0200

    Fix build with LibreSSL, for which OPENSSL_VERSION_NUMBER is irrelevant

--- pdns/opensslsigners.cc
+++ pdns/opensslsigners.cc
@@ -12,7 +12,7 @@
 #include "opensslsigners.hh"
 #include "dnssecinfra.hh"
 
-#if OPENSSL_VERSION_NUMBER < 0x1010000fL
+#if (OPENSSL_VERSION_NUMBER < 0x1010000fL || defined LIBRESSL_VERSION_NUMBER)
 /* OpenSSL < 1.1.0 needs support for threading/locking in the calling application. */
 static pthread_mutex_t *openssllocks;
 
