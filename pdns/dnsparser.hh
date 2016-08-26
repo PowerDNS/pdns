9fdc08346ad2680d0885f555454c024e8b4d3107
bert hubert <bert.hubert@powerdns.com>
2016-08-26 15:36:54 +0200

    dnsparser was using an empty dnsname when it meant to use a root.

--- pdns/dnsparser.hh
+++ pdns/dnsparser.hh
@@ -177,8 +177,7 @@ public:
   virtual string serialize(const DNSName& qname, bool canonic=false, bool lowerCase=false) // it would rock if this were const, but it is too hard
   {
     vector<uint8_t> packet;
-    DNSName empty;
-    DNSPacketWriter pw(packet, empty, 1);
+    DNSPacketWriter pw(packet, g_rootdnsname, 1);
     if(canonic)
       pw.setCanonic(true);
 
