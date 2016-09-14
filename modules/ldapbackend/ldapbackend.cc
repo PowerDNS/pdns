216f1639f129a623308462e79fd93c6ef1bd3ef0
Grégory Oestreicher <greg@kamago.net>
2016-09-14 22:55:16 +0200

    Export KRB5CCNAME from the ldap backend

--- modules/ldapbackend/ldapbackend.cc
+++ modules/ldapbackend/ldapbackend.cc
@@ -26,6 +26,7 @@
 #include "exceptions.hh"
 #include "ldapauthenticator_p.hh"
 #include "ldapbackend.hh"
+#include <cstdlib>
 
 unsigned int ldap_host_index = 0;
 
@@ -85,6 +86,7 @@ LdapBackend::LdapBackend( const string &suffix )
 
         	string bindmethod = getArg( "bindmethod" );
         	if ( bindmethod == "gssapi" ) {
+                        setenv( "KRB5CCNAME", getArg( "krb5-ccache" ).c_str(), 1 );
         		m_authenticator = new LdapGssapiAuthenticator( getArg( "krb5-keytab" ), getArg( "krb5-ccache" ), getArgAsNum( "timeout" ) );
         	}
         	else {
