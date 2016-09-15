109d4633dc65d6a46194534a2c61db5887e35095
Remi Gacogne <remi.gacogne@powerdns.com>
2016-09-15 15:28:45 +0200

    Check TSIG signature on IXFR
    (cherry picked from commit 16c7f7823221d5d75282a77b2e9043b3f60e1ad2)

--- pdns/dnsparser.cc
+++ pdns/dnsparser.cc
@@ -284,8 +284,12 @@ void MOADNSParser::init(bool query, const char *packet, unsigned int len)
 
       d_answers.push_back(make_pair(dr, pr.d_pos));
 
-      if(dr.d_type == QType::TSIG && dr.d_class == 0xff) 
+      if(dr.d_type == QType::TSIG && dr.d_class == QClass::ANY) {
+        if(dr.d_place != DNSResourceRecord::ADDITIONAL || n != (unsigned int)(d_header.ancount + d_header.nscount + d_header.arcount) - 1) {
+          throw MOADNSException("Packet ("+d_qname.toString()+"|#"+std::to_string(d_qtype)+") has a TSIG record in an invalid position.");
+        }
         d_tsigPos = recordStartPos + sizeof(struct dnsheader);
+      }
     }
 
 #if 0    
