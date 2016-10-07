aaf7799ac97262936053e947075b52bbef6454a0
Christian Hofstaedtler <christian.hofstaedtler@deduktiva.com>
2016-10-07 02:42:05 +0200

    packethandler: fix easy cases of var shadowing

--- pdns/slavecommunicator.cc
+++ pdns/slavecommunicator.cc
@@ -101,9 +101,9 @@ void CommunicatorClass::ixfrSuck(const DNSName &domain, const TSIGTriplet& tt, c
     memset(&st, 0, sizeof(st));
     st.serial=di.serial;
 
-    DNSRecord dr;
-    dr.d_content = std::make_shared<SOARecordContent>(g_rootdnsname, g_rootdnsname, st);
-    auto deltas = getIXFRDeltas(remote, domain, dr, tt, laddr.sin4.sin_family ? &laddr : 0, ((size_t) ::arg().asNum("xfr-max-received-mbytes")) * 1024 * 1024);
+    DNSRecord drsoa;
+    drsoa.d_content = std::make_shared<SOARecordContent>(g_rootdnsname, g_rootdnsname, st);
+    auto deltas = getIXFRDeltas(remote, domain, drsoa, tt, laddr.sin4.sin_family ? &laddr : 0, ((size_t) ::arg().asNum("xfr-max-received-mbytes")) * 1024 * 1024);
     zs.numDeltas=deltas.size();
     //    cout<<"Got "<<deltas.size()<<" deltas from serial "<<di.serial<<", applying.."<<endl;
     
