f82f0f6e6ca31d7a1f2a35a10ea3f40fe53cfb8e
Remi Gacogne <remi.gacogne@powerdns.com>
2016-10-25 17:40:09 +0200

    auth: Correctly check unknown record content size

--- pdns/dnsparser.cc
+++ pdns/dnsparser.cc
@@ -63,8 +63,8 @@ public:
       throw MOADNSException("Unknown record was stored incorrectly, need 3 fields, got "+lexical_cast<string>(parts.size())+": "+tmp );
     const string& relevant=(parts.size() > 2) ? parts[2] : "";
     unsigned int total=atoi(parts[1].c_str());
-    if(relevant.size()!=2*total)
-      throw MOADNSException((boost::format("invalid unknown record length for label %s: size not equal to length field (%d != %d)") % d_dr.d_label.c_str() % relevant.size() % (2*total)).str());
+    if(relevant.size() % 2 || relevant.size() / 2 != total)
+      throw MOADNSException((boost::format("invalid unknown record length for label %s: size not equal to length field (%d != 2 * %d)") % d_dr.d_label.c_str() % relevant.size() % total).str());
     string out;
     out.reserve(total+1);
     for(unsigned int n=0; n < total; ++n) {
