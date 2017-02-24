PowerDNS/pdns

commit d86e1bf7048a216a6e5c49de991c8b61684e7ae4

synthesise ALIAS during outgoing AXFR

pdns/tcpreceiver.cc
@@ -737,6 +737,22 @@ int TCPNameserver::doAXFR(const DNSName &target, shared_ptr<DNSPacket> q, int ou
 
   while(sd.db->get(rr)) {
     if(rr.qname.isPartOf(target)) {
+      if (rr.qtype.getCode() == QType::ALIAS) {
+        FindNS fns;
+        vector<string> ips=fns.lookup(DNSName(rr.content), (DNSBackend *) NULL);
+        for(const auto& ip: ips) {
+          if(ip.find(":") == string::npos)
+            rr.qtype = QType(QType::A);
+          else
+            rr.qtype = QType(QType::AAAA);
+          rr.content = ip;
+          rrs.push_back(rr);
+        }
+      }
+      else {
+        rrs.push_back(rr);
+      }
+
       if (rectify) {
         if (rr.qtype.getCode()) {
           qnames.insert(rr.qname);
@@ -747,7 +763,6 @@ int TCPNameserver::doAXFR(const DNSName &target, shared_ptr<DNSPacket> q, int ou
           continue;
         }
       }
-      rrs.push_back(rr);
     } else {
       if (rr.qtype.getCode())
         L<<Logger::Warning<<"Zone '"<<target<<"' contains out-of-zone data '"<<rr.qname<<"|"<<rr.qtype.getName()<<"', ignoring"<<endl;

commit 56335ad7b0e8b2f55d768989bb8a9f84690db100

fix a regression in axfr-rectify introduced by commit d86e1bf7

pdns/tcpreceiver.cc
@@ -836,9 +836,7 @@ int TCPNameserver::doAXFR(const DNSName &target, shared_ptr<DNSPacket> q, int ou
           zrr.dr.d_content = ip.dr.d_content;
           zrrs.push_back(zrr);
         }
-      }
-      else {
-        zrrs.push_back(zrr);
+        continue;
       }
 
       if (rectify) {
@@ -851,6 +849,7 @@ int TCPNameserver::doAXFR(const DNSName &target, shared_ptr<DNSPacket> q, int ou
           continue;
         }
       }
+      zrrs.push_back(zrr);
     } else {
       if (zrr.dr.d_type)
         L<<Logger::Warning<<"Zone '"<<target<<"' contains out-of-zone data '"<<zrr.dr.d_name<<"|"<<DNSRecordContent::NumberToType(zrr.dr.d_type)<<"', ignoring"<<endl;