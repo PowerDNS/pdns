f42440341a9ea0c276e777562b2e4b0b718517ed
phonedph1 <phoned@gmail.com>
2017-03-23 18:49:44 +0000

    Duplicate the shuffle code and let it work with ComboAddresses. Also consider the source port in all the tracking.

--- pdns/syncres.cc
+++ pdns/syncres.cc
@@ -889,6 +889,45 @@ struct speedOrder
   map<DNSName, double>& d_speeds;
 };
 
+inline vector<ComboAddress> SyncRes::shuffleForwardSpeed(vector<ComboAddress> &nameservers, const string &prefix)
+{
+  vector<ComboAddress> out_nameservers;
+  vector<DNSName> rnameservers;
+  for(const auto& ns:nameservers) {
+    rnameservers.push_back(DNSName(ns.toStringWithPort()));
+  }
+
+  map<DNSName, double> speeds;
+
+  for(const auto& val: rnameservers) {
+    double speed;
+    speed=t_sstorage->nsSpeeds[val].get(&d_now);
+    speeds[val]=speed;
+  }
+  random_shuffle(rnameservers.begin(),rnameservers.end(), dns_random);
+  speedOrder so(speeds);
+  stable_sort(rnameservers.begin(),rnameservers.end(), so);
+
+  for(vector<DNSName>::const_iterator i=rnameservers.begin();i!=rnameservers.end();++i) {
+    out_nameservers.push_back(ComboAddress(i->toStringNoDot()));
+  }
+
+  if(doLog()) {
+    LOG(prefix<<"Nameservers: ");
+    for(vector<DNSName>::const_iterator i=rnameservers.begin();i!=rnameservers.end();++i) {
+      if(i!=rnameservers.begin()) {
+        LOG(", ");
+        if(!((i-rnameservers.begin())%3)) {
+          LOG(endl<<prefix<<"             ");
+        }
+      }
+      LOG((i->empty() ? string("<empty>") : i->toStringNoDot())<<"(" << (boost::format("%0.2f") % (speeds[*i]/1000.0)).str() <<"ms)");
+    }
+    LOG(endl);
+  }
+  return out_nameservers;
+}
+
 inline vector<DNSName> SyncRes::shuffleInSpeedOrder(NsSet &tnameservers, const string &prefix)
 {
   vector<DNSName> rnameservers;
@@ -1301,6 +1340,7 @@ int SyncRes::doResolveAt(NsSet &nameservers, DNSName auth, bool flawedNSSet, con
 
       typedef vector<ComboAddress> remoteIPs_t;
       remoteIPs_t remoteIPs;
+      remoteIPs_t remoteIPs_tmp;
       remoteIPs_t::const_iterator remoteIP;
       bool doTCP=false;
       bool pierceDontQuery=false;
@@ -1338,28 +1378,10 @@ int SyncRes::doResolveAt(NsSet &nameservers, DNSName auth, bool flawedNSSet, con
             return -2;
         }
 
-        if(remoteIPs.size() > 1) {
-          bool swap = true;
-          while(swap) {
-            swap = false;
-            for(unsigned int i = 0; i < remoteIPs.size() - 1; i++) {
-              if(t_sstorage->nsSpeeds[DNSName(remoteIPs[i+1].toString())].get(&d_now) < t_sstorage->nsSpeeds[DNSName(remoteIPs[i].toString())].get(&d_now)) {
-                std::swap(remoteIPs[i+1], remoteIPs[i]);
-                swap = true;
-              }
-            }
-          }
-
-          LOG(prefix<<qname<<": Adjusted '"<<auth<<"' NS "<<*tns<<" based on speed: ");
-          for(remoteIP = remoteIPs.begin(); remoteIP != remoteIPs.end(); ++remoteIP) {
-            if(remoteIP != remoteIPs.begin()) {
-              LOG(", ");
-            }
-            LOG(remoteIP->toStringWithPort() <<" ("<< std::to_string(t_sstorage->nsSpeeds[DNSName(remoteIP->toString())].get(&d_now)/1000)<< "msec)");
-          }
-          LOG(endl);
-        }
-
+        remoteIPs_tmp = remoteIPs;
+        remoteIPs.clear();
+        remoteIPs = shuffleForwardSpeed(remoteIPs_tmp, doLog() ? (prefix+qname.toString()+": ") : string() );
+	
         for(remoteIP = remoteIPs.cbegin(); remoteIP != remoteIPs.cend(); ++remoteIP) {
           LOG(prefix<<qname<<": Trying IP "<< remoteIP->toStringWithPort() <<", asking '"<<qname<<"|"<<qtype.getName()<<"'"<<endl);
           if (throttledOrBlocked(prefix, *remoteIP, qname, qtype, pierceDontQuery)) {
@@ -1419,7 +1441,7 @@ int SyncRes::doResolveAt(NsSet &nameservers, DNSName auth, bool flawedNSSet, con
               }
 
               if(resolveret!=-2) { // don't account for resource limits, they are our own fault
-		t_sstorage->nsSpeeds[tns->empty()? DNSName(remoteIP->toString()) : *tns].submit(*remoteIP, 1000000, &d_now); // 1 sec
+		t_sstorage->nsSpeeds[tns->empty()? DNSName(remoteIP->toStringWithPort()) : *tns].submit(*remoteIP, 1000000, &d_now); // 1 sec
 
 		// code below makes sure we don't filter COM or the root
                 if (s_serverdownmaxfails > 0 && (auth != g_rootdnsname) && t_sstorage->fails.incr(*remoteIP) >= s_serverdownmaxfails) {
@@ -1471,7 +1493,7 @@ int SyncRes::doResolveAt(NsSet &nameservers, DNSName auth, bool flawedNSSet, con
         */
         //        cout<<"msec: "<<lwr.d_usec/1000.0<<", "<<g_avgLatency/1000.0<<'\n';
 
-        t_sstorage->nsSpeeds[tns->empty()? DNSName(remoteIP->toString()) : *tns].submit(*remoteIP, lwr.d_usec, &d_now);
+        t_sstorage->nsSpeeds[tns->empty()? DNSName(remoteIP->toStringWithPort()) : *tns].submit(*remoteIP, lwr.d_usec, &d_now);
       }
 
       if(s_minimumTTL) {
