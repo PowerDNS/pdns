86c4ed0b63fe7a191443bf397344c37d408b3815
Remi Gacogne <remi.gacogne@powerdns.com>
2017-04-10 18:33:30 +0200

    rec: Clear the RPZ NS IP table when clearing the policy
    Be it the entire policy object or a single zone.
    
    (cherry picked from commit 2ae5639865ca29aa0c7b722e55e9098b81627700)

--- pdns/filterpo.cc
+++ pdns/filterpo.cc
@@ -156,7 +156,19 @@ void DNSFilterEngine::clear(size_t zone)
   z.qpolAddr.clear();
   z.postpolAddr.clear();
   z.propolName.clear();
+  z.propolNSAddr.clear();
   z.qpolName.clear();
+}
+
+void DNSFilterEngine::clear()
+{
+  for(auto& z : d_zones) {
+    z.qpolAddr.clear();
+    z.postpolAddr.clear();
+    z.propolName.clear();
+    z.propolNSAddr.clear();
+    z.qpolName.clear();
+  }
 }
 
 void DNSFilterEngine::addClientTrigger(const Netmask& nm, Policy pol, size_t zone)
