df17411a709a61cb9e437fe843f25225a344d48f
Grégory Oestreicher <greg@kamago.net>
2017-06-25 00:00:11 +0200

    Add PowerLDAP::del() and PowerLDAP::add()

--- modules/ldapbackend/powerldap.hh
+++ modules/ldapbackend/powerldap.hh
@@ -90,7 +90,9 @@ class PowerLDAP
     void bind( const string& ldapbinddn = "", const string& ldapsecret = "", int method = LDAP_AUTH_SIMPLE );
     void simpleBind( const string& ldapbinddn = "", const string& ldapsecret = "" );
     SearchResult::Ptr search( const string& base, int scope, const string& filter, const char** attr = 0 );
+    void add( const string &dn, LDAPMod *mods[] );
     void modify( const string& dn, LDAPMod *mods[], LDAPControl **scontrols = 0, LDAPControl **ccontrols = 0 );
+    void del( const string& dn );
   
     bool getSearchEntry( int msgid, sentry_t& entry, bool dn = false );
     void getSearchResults( int msgid, sresult_t& result, bool dn = false );
