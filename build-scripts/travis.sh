b9cd90e70a64443f15a3cc312dd73600371e7fe9
Remi Gacogne <remi.gacogne@powerdns.com>
2017-07-05 12:02:17 +0200

    travis: Build the rec with Botan and libsodium
    (cherry picked from commit 18a93d3812bfbf2e2dbc9b6353e3619d626283fd)

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -382,6 +382,8 @@ build_recursor() {
   run "cd pdns-recursor-*"
   run "CFLAGS='-O1' CXXFLAGS='-O1' ./configure \
     --prefix=$PDNS_RECURSOR_DIR \
+    --enable-botan \
+    --enable-libsodium \
     --enable-unit-tests \
     --disable-silent-rules"
   run "make -k -j3"
@@ -569,6 +571,7 @@ run "sudo apt-get -qq --no-install-recommends install \
   liblua5.1-dev \
   libedit-dev \
   libprotobuf-dev \
+  libbotan1.10-dev \
   pandoc\
   protobuf-compiler"
 
