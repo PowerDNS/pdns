9c741511d5878130ce52dccb8446334f3ad8984a
Aki Tuomi <cmouse@cmouse.fi>
2018-01-03 23:45:45 +0200

    travis: Add libmaxminddb-dev to auth dependencies

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -207,9 +207,14 @@ install_auth() {
     libp11-kit-dev"
 
   # geoip-backend
+  run "sudo add-apt-repository -y ppa:maxmind/ppa"
+  run "gpg --keyserver keyserver.ubuntu.com --recv-keys DE742AFA"
+  run "gpg --export DE742AFA | sudo apt-key add -"
+  run "sudo apt-get update"
   run "sudo apt-get -qq --no-install-recommends install \
     libgeoip-dev \
-    libyaml-cpp-dev"
+    libyaml-cpp-dev \
+    libmaxminddb-dev"
 
   # ldap-backend
   run "sudo apt-get -qq --no-install-recommends install \
