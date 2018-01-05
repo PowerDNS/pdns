4d39d7f34cf2406a9f16e28b2f5d21138aecd422
Tom Ivar Helbekkmo <tih@hamartun.priv.no>
2018-01-05 14:24:08 +0100

    Changes to compile and run on NetBSD

--- pdns/dns.hh
+++ pdns/dns.hh
@@ -130,7 +130,7 @@ struct EDNS0Record
 
 static_assert(sizeof(EDNS0Record) == 4, "EDNS0Record size must be 4");
 
-#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__)
+#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
 #include <machine/endian.h>
 #elif __linux__ || __GNU__
 # include <endian.h>
