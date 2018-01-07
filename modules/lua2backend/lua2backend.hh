63c1a37cd2e1a937bde3b01eab0be4056c350550
Aki Tuomi <cmouse@cmouse.fi>
2018-01-07 19:39:47 +0200

    lua2backend: Add new backend
    This is API version 1 of lua2 backend.
    
    It provides improved interface for Lua script to act as backends.
    
    Configuration
     - `lua2-filename` - path to script
     - `lua2-query-logging` - log lua queries and results
     - `lua2-api' - API version (default 2)

---
+++ modules/lua2backend/lua2backend.hh
@@ -0,0 +1,30 @@
+/*
+ * This file is part of PowerDNS or dnsdist.
+ * Copyright -- PowerDNS.COM B.V. and its contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * In addition, for the avoidance of any doubt, permission is granted to
+ * link this program with OpenSSL and to (re)distribute the binaries
+ * produced as the result of such linking.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#pragma once
+#ifndef LUA2BACKEND_HH
+#define LUA2BACKEND_HH 1
+
+#include "pdns/dnsbackend.hh"
+#include "pdns/lua-auth4.hh"
+#include "lua2api2.hh"
+
+#endif
