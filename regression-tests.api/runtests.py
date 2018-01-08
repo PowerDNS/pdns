0567c609dbe8f4da5702534baf56f2951c45d554
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2018-01-08 16:32:20 +0100

    tests: use sdig from PATH if available

--- regression-tests.api/runtests.py
+++ regression-tests.api/runtests.py
@@ -100,7 +100,14 @@ common_args = [
     "--api-key="+APIKEY
 ]
 
-run_check_call(["make", "-C", "../pdns", "sdig"])
+# Take sdig if it exists (recursor in travis), otherwise build it from Authoritative source.
+sdig = os.environ.get("SDIG", "")
+if sdig:
+    sdig = os.path.abspath(sdig)
+if not sdig or not os.path.exists(sdig):
+    run_check_call(["make", "-C", "../pdns", "sdig"])
+    sdig = "../pdns/sdig"
+
 
 if daemon == 'authoritative':
 
@@ -165,7 +172,7 @@ if not available:
     sys.exit(2)
 
 print "Query for example.com/A to create statistic data..."
-run_check_call(["../pdns/sdig", "127.0.0.1", str(DNSPORT), "example.com", "A"])
+run_check_call([sdig, "127.0.0.1", str(DNSPORT), "example.com", "A"])
 
 print "Running tests..."
 returncode = 0
