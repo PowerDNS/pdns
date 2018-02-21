2c68abe75d908b4a32a7de229c75af137dc3ec5a
Pieter Lexis <pieter.lexis@powerdns.com>
2018-02-21 17:37:02 +0100

    rec: Ensure we don't hammer the RPZ master server
    Before, if the `refresh` was not set in the lua-config file's
    `rpzMaster` statement, we would keep trying to get delta's the whole
    time. This commit ensures we update the zone's refresh config to the
    value from the AXFR'd zone (if not set in the config).

--- pdns/reczones.cc
+++ pdns/reczones.cc
@@ -327,8 +327,8 @@ void RPZIXFRTracker(const ComboAddress& master, boost::optional<DNSFilterEngine:
   while (!sr) {
     try {
       sr=loadRPZFromServer(master, zoneName, zone, defpol, maxTTL, tt, maxReceivedBytes, localAddress);
-      if(refresh) {
-        sr->d_st.refresh=refresh;
+      if(refresh == 0) {
+        refresh = sr->d_st.refresh;
       }
       zone->setSerial(sr->d_st.serial);
     }
@@ -340,7 +340,11 @@ void RPZIXFRTracker(const ComboAddress& master, boost::optional<DNSFilterEngine:
     }
 
     if (!sr) {
-      sleep(refresh);
+      if (refresh == 0) {
+        sleep(10);
+      } else {
+        sleep(refresh);
+      }
     }
   }
 
