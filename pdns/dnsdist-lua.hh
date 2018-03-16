c134cbaac845a7b5d32ce825725ae5fea177fcca
Charles-Henri Bruyand <charles-henri.bruyand@open-xchange.com>
2018-03-16 04:43:13 +0100

    dnsdist: make lua actions second return value optional

--- pdns/dnsdist-lua.hh
+++ pdns/dnsdist-lua.hh
@@ -24,7 +24,7 @@
 class LuaAction : public DNSAction
 {
 public:
-  typedef std::function<std::tuple<int, string>(DNSQuestion* dq)> func_t;
+  typedef std::function<std::tuple<int, boost::optional<string> >(DNSQuestion* dr)> func_t;
   LuaAction(LuaAction::func_t func) : d_func(func)
   {}
   Action operator()(DNSQuestion* dq, string* ruleresult) const override;
@@ -39,7 +39,7 @@ private:
 class LuaResponseAction : public DNSResponseAction
 {
 public:
-  typedef std::function<std::tuple<int, string>(DNSResponse* dr)> func_t;
+  typedef std::function<std::tuple<int, boost::optional<string> >(DNSResponse* dr)> func_t;
   LuaResponseAction(LuaResponseAction::func_t func) : d_func(func)
   {}
   Action operator()(DNSResponse* dr, string* ruleresult) const override;
