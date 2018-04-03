57353b736ac659d419164f32ea6c6ef44c230156
Peter van Dijk <peter.van.dijk@powerdns.com>
2018-04-03 14:37:01 +0200

    count stored signatures

--- pdns/recursor_cache.cc
+++ pdns/recursor_cache.cc
@@ -315,6 +315,9 @@ void MemRecursorCache::replace(time_t now, const DNSName &qname, const QType& qt
     ce.d_bytes += i.d_content->d_size_in_bytes;
   }
 
+  for(const auto i : signatures) {
+    ce.d_bytes += i->d_size_in_bytes;
+  }
   if (!isNew) {
     moveCacheItemToBack(d_cache, stored);
   }
