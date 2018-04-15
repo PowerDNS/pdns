PowerDNS/pdns

commit a2dfc9ea78614a9eef514a612117e507d70d4664

auth: one query and a huge amount of code down the drain

pdns/dnsbackend.hh
@@ -42,6 +42,7 @@ class DNSPacket;
 #include "comment.hh"
 #include "dnsname.hh"
 #include "dnsrecords.hh"
+#include "iputils.hh"
 
 class DNSBackend;  
 struct DomainInfo
@@ -51,7 +52,7 @@ struct DomainInfo
   DNSName zone;
   time_t last_check;
   string account;
-  vector<string> masters;
+  vector<string> masters; // FIXME use ComboAddress
   DNSBackend *backend;
 
   uint32_t id;
@@ -84,6 +85,16 @@ struct DomainInfo
       return DomainInfo::Master;
     else
       return DomainInfo::Native;
+  }
+
+  const bool isMaster(const ComboAddress& ip)
+  {
+    for( const auto& master: masters) {
+      const ComboAddress caMaster(master);
+      if(ip == caMaster)
+        return true;
+    }
+    return false;
   }
 
 };
@@ -236,11 +247,6 @@ public:
   }
 
   //! returns true if master ip is master for domain name.
-  virtual bool isMaster(const DNSName &name, const string &ip)
-  {
-    return false;
-  }
-  
   //! starts the transaction for updating domain qname (FIXME: what is id?)
   virtual bool startTransaction(const DNSName &qname, int id=-1)
   {

modules/bindbackend/bindbackend2.cc
@@ -1215,22 +1215,6 @@ bool Bind2Backend::handle::get_list(DNSResourceRecord &r)
   return false;
 }
 
-bool Bind2Backend::isMaster(const DNSName& name, const string &ip)
-{
-  BB2DomainInfo bbd;
-  if(!safeGetBBDomainInfo(name, &bbd))
-    return false;
-
-  if(bbd.d_kind != DomainInfo::Slave)
-    return false;
-
-  for(vector<string>::const_iterator iter = bbd.d_masters.begin(); iter != bbd.d_masters.end(); ++iter)
-    if(*iter==ip)
-      return true;
-  
-  return false;
-}
-
 bool Bind2Backend::superMasterBackend(const string &ip, const DNSName& domain, const vector<DNSResourceRecord>&nsset, string *nameserver, string *account, DNSBackend **db)
 {
   // Check whether we have a configfile available.

modules/bindbackend/bindbackend2.hh
@@ -239,7 +239,6 @@ public:
   void insertRecord(BB2DomainInfo& bbd, const DNSName &qname, const QType &qtype, const string &content, int ttl, const std::string& hashed=string(), bool *auth=0);
   void rediscover(string *status=0) override;
 
-  bool isMaster(const DNSName &name, const string &ip) override;
 
   // for supermaster support
   bool superMasterBackend(const string &ip, const DNSName &domain, const vector<DNSResourceRecord>&nsset, string *nameserver, string *account, DNSBackend **db) override;

modules/gmysqlbackend/gmysqlbackend.cc
@@ -94,8 +94,6 @@ public:
     declare(suffix, "remove-empty-non-terminals-from-zone-query", "remove all empty non-terminals from zone", "delete from records where domain_id=? and type is null");
     declare(suffix, "delete-empty-non-terminal-query", "delete empty non-terminal from zone", "delete from records where domain_id=? and name=? and type is null");
 
-    declare(suffix,"master-zone-query","Data", "select master from domains where name=? and type='SLAVE'");
-
     declare(suffix,"info-zone-query","","select id,name,master,last_check,notified_serial,type,account from domains where name=?");
 
     declare(suffix,"info-all-slaves-query","","select id,name,master,last_check from domains where type='SLAVE'");

modules/godbcbackend/godbcbackend.cc
@@ -81,8 +81,6 @@ public:
     declare(suffix, "remove-empty-non-terminals-from-zone-query", "remove all empty non-terminals from zone", "delete from records where domain_id=? and type is null");
     declare(suffix, "delete-empty-non-terminal-query", "delete empty non-terminal from zone", "delete from records where domain_id=? and name=? and type is null");
 
-    declare(suffix,"master-zone-query","Data", "select master from domains where name=? and type='SLAVE'");
-
     declare(suffix,"info-zone-query","","select id,name,master,last_check,notified_serial,type,account from domains where name=?");
 
     declare(suffix,"info-all-slaves-query","","select id,name,master,last_check from domains where type='SLAVE'");

modules/goraclebackend/goraclebackend.cc
@@ -100,8 +100,6 @@ public:
     declare(suffix, "remove-empty-non-terminals-from-zone-query", "remove all empty non-terminals from zone", "delete from records where domain_id=:domain_id and type is null");
     declare(suffix, "delete-empty-non-terminal-query", "delete empty non-terminal from zone", "delete from records where domain_id=:domain_id and name=:qname and type is null");
 
-    declare(suffix, "master-zone-query", "Data", "select master from domains where name=:domain and type='SLAVE'");
-
     declare(suffix, "info-zone-query", "","select id,name,master,last_check,notified_serial,type,account from domains where name=:domain");
 
     declare(suffix, "info-all-slaves-query", "","select id,name,master,last_check from domains where type='SLAVE'");

modules/gpgsqlbackend/gpgsqlbackend.cc
@@ -101,8 +101,6 @@ public:
     declare(suffix,"remove-empty-non-terminals-from-zone-query", "remove all empty non-terminals from zone", "delete from records where domain_id=$1 and type is null");
     declare(suffix,"delete-empty-non-terminal-query", "delete empty non-terminal from zone", "delete from records where domain_id=$1 and name=$2 and type is null");
 
-    declare(suffix,"master-zone-query","Data", "select master from domains where name=$1 and type='SLAVE'");
-
     declare(suffix,"info-zone-query","","select id,name,master,last_check,notified_serial,type,account from domains where name=$1");
 
     declare(suffix,"info-all-slaves-query","","select id,name,master,last_check from domains where type='SLAVE'");

modules/gsqlite3backend/gsqlite3backend.cc
@@ -92,8 +92,6 @@ public:
 
     declare(suffix, "remove-empty-non-terminals-from-zone-query", "remove all empty non-terminals from zone", "delete from records where domain_id=:domain_id and type is null");
     declare(suffix, "delete-empty-non-terminal-query", "delete empty non-terminal from zone", "delete from records where domain_id=:domain_id and name=:qname and type is null");
-    
-    declare(suffix, "master-zone-query", "Data", "select master from domains where name=:domain and type='SLAVE'");
 
     declare(suffix, "info-zone-query", "","select id,name,master,last_check,notified_serial,type,account from domains where name=:domain");
 

commit 2f538e82638cf469697657cb543377d6bf7443a3

dnsbackend: Make isMaster a const
The const keyword fell to wrong side of the function,
so move it where it belongs to.

Broken in a2dfc9ea78614a9eef514a612117e507d70d4664

pdns/dnsbackend.hh
@@ -87,7 +87,7 @@ struct DomainInfo
       return DomainInfo::Native;
   }
 
-  const bool isMaster(const ComboAddress& ip)
+  bool isMaster(const ComboAddress& ip) const
   {
     for( const auto& master: masters) {
       if(ComboAddress::addressOnlyEqual()(ip, master))