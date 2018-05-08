9adbe79034783bc5f2e20a57d425eb690035e107
Charles-Henri Bruyand <charles-henri.bruyand@open-xchange.com>
2018-05-09 01:14:42 +0200

    rec: ensure lua-dns-script has been properly initialized

--- pdns/pdns_recursor.cc
+++ pdns/pdns_recursor.cc
@@ -3556,9 +3556,12 @@ try
         last_carbon = g_now.tv_sec;
       }
     }
-    if(g_now.tv_sec - last_lua_maintenance >= luaMaintenanceInterval) {
-      t_pdl->maintenance();
-      last_lua_maintenance = g_now.tv_sec;
+    if (t_pdl != nullptr) {
+      // lua-dns-script directive is present, call the maintenance callback if needed
+      if(g_now.tv_sec - last_lua_maintenance >= luaMaintenanceInterval) {
+        t_pdl->maintenance();
+        last_lua_maintenance = g_now.tv_sec;
+      }
     }
 
     t_fdm->run(&g_now);
