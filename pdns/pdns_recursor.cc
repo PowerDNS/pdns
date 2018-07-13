e6ec15bfe4c391a51eab7c51c38307c7e009768f
Remi Gacogne <remi.gacogne@powerdns.com>
2018-07-13 11:19:04 +0200

    rec: Delay the creation of RPZ threads until we have dropped privileges
    On Linux/glibc, calling `set*id()` from a thread results in the other
    threads being sent the `SIGRT_1` signal so they are aware that they
    should switch credentials too, because `POSIX` requires that all threads
    use the same credentials but Linux actually handles it per thread.
    The reception of the signal interrupts the current `syscall` with
    `EINTR`, causing the loading of the `RPZ` zone to fail.

--- pdns/pdns_recursor.cc
+++ pdns/pdns_recursor.cc
@@ -3189,9 +3189,10 @@ static int serviceMain(int argc, char*argv[])
 
   g_maxCacheEntries = ::arg().asNum("max-cache-entries");
   g_maxPacketCacheEntries = ::arg().asNum("max-packetcache-entries");
-  
+
+  luaConfigDelayedThreads delayedLuaThreads;
   try {
-    loadRecursorLuaConfig(::arg()["lua-config-file"], ::arg().mustDo("daemon"));
+    loadRecursorLuaConfig(::arg()["lua-config-file"], delayedLuaThreads);
   }
   catch (PDNSException &e) {
     g_log<<Logger::Error<<"Cannot load Lua configuration: "<<e.reason<<endl;
@@ -3354,7 +3355,6 @@ static int serviceMain(int argc, char*argv[])
     g_log<<Logger::Warning<<"Calling daemonize, going to background"<<endl;
     g_log.toConsole(Logger::Critical);
     daemonize();
-    loadRecursorLuaConfig(::arg()["lua-config-file"], false);
   }
   signal(SIGUSR1,usr1Handler);
   signal(SIGUSR2,usr2Handler);
@@ -3412,6 +3412,8 @@ static int serviceMain(int argc, char*argv[])
   makeControlChannelSocket( ::arg().asNum("processes") > 1 ? forks : -1);
 
   Utility::dropUserPrivs(newuid);
+
+  startLuaConfigDelayedThreads(delayedLuaThreads, g_luaconfs.getCopy().generation);
 
   makeThreadPipes();
 
