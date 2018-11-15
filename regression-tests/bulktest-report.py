7a0ea2915abda94934896858490d38a220d2fa7d
cclauss <cclauss@bluewin.ch>
2018-11-15 17:46:46 +0100

    Modernize Python 2 code to get ready for Python 3

--- regression-tests/bulktest-report.py
+++ regression-tests/bulktest-report.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import json, sys
 
 runs = json.load(sys.stdin)
@@ -26,6 +27,6 @@ selected.sort()
 names.discard('tag')
 
 fmt=''.join('%%%ds' % max(15, i+4) for i in [3]+map(len, sorted(names)))
-print fmt % tuple(['tag']+sorted(names))
+print(fmt % tuple(['tag']+sorted(names)))
 for tag, stats in selected:
-	print fmt % tuple([tag] + [stats.get(s) for s in sorted(names)])
+	print(fmt % tuple([tag] + [stats.get(s) for s in sorted(names)]))
