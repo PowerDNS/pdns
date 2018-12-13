668650586215bc4e7f945c514a49ab6d516ec584
bert hubert <bert.hubert@netherlabs.nl>
2018-12-13 23:17:56 +0100

    implement way more methods

--- modules/lmdbbackend/lmdbbackend.cc
+++ modules/lmdbbackend/lmdbbackend.cc
@@ -25,72 +25,56 @@
 #include "pdns/utility.hh"
 #include "pdns/dnsbackend.hh"
 #include "pdns/dns.hh"
-#include "pdns/dnsbackend.hh"
 #include "pdns/dnspacket.hh"
 #include "pdns/pdnsexception.hh"
 #include "pdns/logger.hh"
 #include "pdns/version.hh"
-#include <boost/algorithm/string.hpp>
+#include "pdns/arguments.hh"
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/string.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+
+#include <boost/iostreams/device/back_inserter.hpp>
 #include <sstream>
+#include "lmdbbackend.hh"
 
-#include "lmdb-safe.hh"
-/* FIRST PART */
-class LMDBBackend : public DNSBackend
+LMDBBackend::LMDBBackend(const std::string& suffix)
 {
-public:
-  LMDBBackend(const string &suffix="") :
-    d_env(getMDBEnv("./pdns.lmdb", MDB_NOSUBDIR, 0600)),
-    d_recordsdbi(d_env->openDB("records", MDB_CREATE | MDB_INTEGERKEY)),
-    d_recordsnameidxdbi(d_env->openDB("records_nameidx", MDB_CREATE | MDB_DUPSORT)),
-    d_recordsdomainididxdbi(d_env->openDB("records_domainidx", MDB_CREATE | MDB_INTEGERKEY | MDB_DUPSORT | MDB_INTEGERDUP)),
-    d_domainsdbi(d_env->openDB("domains", MDB_CREATE)),
-    d_domainsnameidxdbi(d_env->openDB("domains_nameidx", MDB_CREATE))
-
-  {
-    setArgPrefix("lmdb"+suffix);
-  }
-
-  bool list(const DNSName &target, int id, bool include_disabled) override;
-
-  bool getDomainInfo(const DNSName &domain, DomainInfo &di, bool getSerial=true) override;
-  bool createDomain(const DNSName &domain, const string &type, const string &masters, const string &account);
-
-  bool createDomain(const DNSName &domain) override;
+  setArgPrefix("lmdb"+suffix);
   
-  bool startTransaction(const DNSName &domain, int domain_id=-1) override;
-  bool commitTransaction() override;
-  bool abortTransaction() override;
-  bool feedRecord(const DNSResourceRecord &r, const DNSName &ordername) override;
-  bool replaceRRSet(uint32_t domain_id, const DNSName& qname, const QType& qt, const vector<DNSResourceRecord>& rrset) override;
-
-  void getAllDomains(vector<DomainInfo> *domains, bool include_disabled=false) override;
-  void lookup(const QType &type, const DNSName &qdomain, DNSPacket *p, int zoneId) override;
-  bool get(DNSResourceRecord &rr) override;
-  void getUnfreshSlaveInfos(vector<DomainInfo>* domains) override;
-
+  string syncMode = toLower(getArg("sync-mode"));
+  int asyncFlag = 0;
+  if(syncMode == "nosync")
+    asyncFlag = MDB_NOSYNC;
+  else if(syncMode == "nometasync")
+    asyncFlag = MDB_NOMETASYNC;
+  else if(syncMode == "mapasync")
+    asyncFlag = MDB_MAPASYNC;
+  else if(syncMode.empty())
+    asyncFlag = 0;
+  else
+    throw std::runtime_error("Unknown sync mode "+syncMode+" requested for LMDB backend");
   
-  bool setMaster(const DNSName &domain, const string &ip) override;
-  bool setKind(const DNSName &domain, const DomainInfo::DomainKind kind) override;
+  d_env = getMDBEnv(getArg("filename").c_str(), MDB_NOSUBDIR | asyncFlag | MDB_WRITEMAP, 0600);
   
-private:
-  int genChangeDomain(const DNSName& domain, std::function<void(DomainInfo&)> func);
-  bool get_list(DNSResourceRecord &rr);
-  bool get_lookup(DNSResourceRecord &rr);
-  bool d_inlist{false};
-  QType d_lookuptype;
-  uint32_t d_lookupdomainid;
-  shared_ptr<MDBEnv> d_env;
-  shared_ptr<MDBROTransaction> d_rotxn;
-  shared_ptr<MDBRWTransaction> d_rwtxn;
-  uint32_t d_recordid;
-  shared_ptr<MDBROCursor> d_cursor;
-  MDBDbi d_recordsdbi, d_recordsnameidxdbi, d_recordsdomainididxdbi;
-  MDBDbi d_domainsdbi, d_domainsnameidxdbi;
-};
+  d_recordsdbi=d_env->openDB("records", MDB_CREATE | MDB_INTEGERKEY);
+  d_recordsnameidxdbi=d_env->openDB("records_nameidx", MDB_CREATE | MDB_DUPSORT | MDB_DUPFIXED);
+  d_recordsdomainididxdbi=d_env->openDB("records_domainidx", MDB_CREATE | MDB_INTEGERKEY | MDB_DUPSORT | MDB_INTEGERDUP | MDB_DUPFIXED);
+
+  d_domainsdbi = d_env->openDB("domains", MDB_CREATE | MDB_INTEGERKEY);
+  d_domainsnameidxdbi = d_env->openDB("domains_nameidx", MDB_CREATE);
+
+  // d_metadbi: key is domain name, content is kind/content pairs
+
+  d_domainmetadbi = d_env->openDB("domainmetadata", MDB_CREATE | MDB_DUPSORT);
+  
+  d_dolog = ::arg().mustDo("query-logging");
+}
+
 
 static unsigned int getMaxID(MDBRWTransaction& txn, MDBDbi& dbi)
 {
@@ -107,10 +91,22 @@ static unsigned int getMaxID(MDBRWTransaction& txn, MDBDbi& dbi)
 template<typename T>
 std::string serToString(const T& t)
 {
+
+  std::string serial_str;
+  boost::iostreams::back_insert_device<std::string> inserter(serial_str);
+  boost::iostreams::stream<boost::iostreams::back_insert_device<std::string> > s(inserter);
+  boost::archive::binary_oarchive oa(s, boost::archive::no_header | boost::archive::no_codecvt);
+  
+  oa << t;
+  return serial_str;
+  /*
+
+  
   ostringstream oss;
-  boost::archive::binary_oarchive oa(oss,boost::archive::no_header );
+  boost::archive::binary_oarchive oa(oss,boost::archive::no_header |boost::archive::no_codecvt  );
   oa << t;
   return oss.str();
+  */
 }
 
 template<typename T>
@@ -118,7 +114,7 @@ void serFromString(const std::string& str, T& ret)
 {
   ret = T();
   std::istringstream istr{str};
-  boost::archive::binary_iarchive oi(istr,boost::archive::no_header );
+  boost::archive::binary_iarchive oi(istr,boost::archive::no_header|boost::archive::no_codecvt );
   oi >> ret;
 }
 
@@ -173,11 +169,11 @@ void serialize(Archive & ar, DomainInfo& g, const unsigned int version)
 template<class Archive>
 void serialize(Archive & ar, DNSResourceRecord& g, const unsigned int version)
 {
+  ar & g.qtype;
   ar & g.qname;
   ar & g.content;
   ar & g.ttl;
   ar & g.domain_id;
-  ar & g.qtype;
   ar & g.auth;
 }
 
@@ -189,19 +185,64 @@ BOOST_SERIALIZATION_SPLIT_FREE(DNSName);
 BOOST_SERIALIZATION_SPLIT_FREE(QType);
 //BOOST_SERIALIZATION_SPLIT_FREE(ComboAddress);  
 BOOST_IS_BITWISE_SERIALIZABLE(ComboAddress)
+
+void LMDBBackend::deleteDomainRecords(MDBRWTransaction& txn, uint32_t domain_id)
+{
+  auto cursor = txn.getCursor(d_recordsdomainididxdbi);
+  MDBInVal in(domain_id);
+  MDBOutVal id;
+  id.d_mdbval = in.d_mdbval;
+  MDBOutVal recordid;
+  bool first = true;
+
+  // should clear all records now
+  while(!cursor.get(id, recordid, first ? MDB_SET : MDB_NEXT_DUP)) {
+    first = false;
+    MDBOutVal record;
+    txn.get(d_recordsdbi, recordid, record);
+    DNSResourceRecord rr;
+    serFromString(record.get<string>(), rr);
+    txn.del(d_recordsnameidxdbi, rr.qname.toDNSStringLC(), recordid);
+    txn.del(d_recordsdbi, recordid);
+    cursor.del();
+  }
+  
+}
+
 bool LMDBBackend::startTransaction(const DNSName &domain, int domain_id)
 {
-  cout<<"Start transaction";
+  cout<<"Start transaction for domain "<<domain_id<<endl;
+  
   d_rwtxn = std::make_shared<MDBRWTransaction>(d_env->getRWTransaction());
-  // should clear all records now
-  // from records table & indexes (two, name, domain idx)
-  d_recordid = getMaxID(*d_rwtxn, d_recordsdbi);
+
+  if(domain_id >= 0) {
+    deleteDomainRecords(*d_rwtxn, domain_id);
+  }
+
+  d_newrecordid = getMaxID(*d_rwtxn, d_recordsdbi) + 1;
   return true;
 }
 
 bool LMDBBackend::commitTransaction()
 {
   cout<<"Commit transaction"<<endl;
+
+  if(!d_recordsdomainids.empty()) {
+    cout<<"Emitting domains table"<<endl;
+    auto cursor = d_rwtxn->getCursor(d_recordsdomainididxdbi);
+    MDBInVal in(d_newdomainid);
+    MDBOutVal key;
+    key.d_mdbval = in.d_mdbval;
+    MDB_val data[2];
+    data[0].mv_data = &*d_recordsdomainids.begin();
+    data[0].mv_size = 4;
+    data[1].mv_size = d_recordsdomainids.size();
+    int rc = mdb_cursor_put(cursor, &key.d_mdbval, data, MDB_MULTIPLE);
+    if(rc)
+      throw std::runtime_error("Error with multiple put: "+string(mdb_strerror(rc)));
+    cout << "Wrote "<<data[1].mv_size <<" entries out of "<<d_recordsdomainids.size()<<endl;
+  }
+  cout<<"Actual commit"<<endl;
   d_rwtxn->commit();
   d_rwtxn.reset();
   return true;
@@ -218,19 +259,15 @@ bool LMDBBackend::abortTransaction()
 
 bool LMDBBackend::feedRecord(const DNSResourceRecord &r, const DNSName &ordername)
 {
-  ++d_recordid;
-  if(!(d_recordid % 16384))
-    cout<<"Got feedrecord, "<<d_recordid<<endl;
-
-  d_rwtxn->put(d_recordsdbi, d_recordid, serToString(r), MDB_APPEND);
+  d_rwtxn->put(d_recordsdbi, d_newrecordid, serToString(r), MDB_APPEND);
   
   // insert name index
-  d_rwtxn->put(d_recordsnameidxdbi, r.qname.toDNSStringLC(), d_recordid);
-  
-  // insert domain_id index
-  d_rwtxn->put(d_recordsdomainididxdbi, r.domain_id, d_recordid, MDB_APPENDDUP);
-  
+  d_rwtxn->put(d_recordsnameidxdbi, r.qname.toDNSStringLC(), d_newrecordid);
 
+  d_recordsdomainids.push_back(d_newrecordid);
+  d_newdomainid = r.domain_id;
+  
+  ++d_newrecordid;
   return true;
 }
 
@@ -290,6 +327,30 @@ bool LMDBBackend::replaceRRSet(uint32_t domain_id, const DNSName& qname, const Q
   return true;
 }
 
+bool LMDBBackend::deleteDomain(const DNSName &domain)
+{
+  shared_ptr<MDBRWTransaction> txn;
+  if(d_rwtxn) {
+    txn = d_rwtxn;
+    cout<<"Reusing open transaction"<<endl;
+  }
+  else {
+    cout<<"Making a new RW txn for feed record"<<endl;
+    txn = std::make_shared<MDBRWTransaction>(d_env->getRWTransaction());
+  }
+  MDBOutVal id;
+  if(!txn->get(d_domainsnameidxdbi, domain.toDNSStringLC(), id)) {
+    deleteDomainRecords(*txn, id.get<uint32_t>());
+
+    txn->del(d_domainsdbi, id);
+    txn->del(d_domainmetadbi, domain.toDNSStringLC());
+    txn->del(d_domainsnameidxdbi, domain.toDNSStringLC());
+  }
+  if(!d_rwtxn)
+    txn->commit();
+  return true;
+}
+
 bool LMDBBackend::list(const DNSName &target, int id, bool include_disabled)
 {
   cout<<"In list"<<endl;
@@ -332,7 +393,7 @@ bool LMDBBackend::get_list(DNSResourceRecord& rr)
 
     return false;
   }
-  cout<<"Got a record id "<<id.get<uint32_t>()<<endl;
+  //  cout<<"Got a record id "<<id.get<uint32_t>()<<endl;
 
   MDBOutVal record;
   if(d_rotxn->get(d_recordsdbi, id, record)) {
@@ -349,72 +410,82 @@ bool LMDBBackend::get_list(DNSResourceRecord& rr)
     d_cursor.reset();
     d_rotxn.reset();
   }
-  else
-    cout<<"Got id "<< id.get<uint32_t>()<<" after MDB_NEXT_DUP"<<endl;
+  //  else
+  //  cout<<"Got id "<< id.get<uint32_t>()<<" after MDB_NEXT_DUP"<<endl;
   return true;
 }
 
 
 void LMDBBackend::lookup(const QType &type, const DNSName &qdomain, DNSPacket *p, int zoneId)
 {
-  cout << "Got lookup for "<<qdomain<<" in zone "<< zoneId<<endl;
+  if(d_dolog) {
+    g_log << Logger::Warning << "Got lookup for "<<qdomain<<"|"<<type.getName()<<" in zone "<< zoneId<<endl;
+    d_dtime.set();
+  }
   d_rotxn = std::make_shared<MDBROTransaction>(d_env->getROTransaction());
-  
   d_cursor = std::make_shared<MDBROCursor>(d_rotxn->getCursor(d_recordsnameidxdbi));
   MDBOutVal key, data;
   if(d_cursor->find(qdomain.toDNSStringLC(), key, data) == MDB_NOTFOUND) {
-    cout<<"Found nothing!"<<endl;
+    if(d_dolog)
+      g_log<<Logger::Warning<<"Found nothing!"<<endl;
     d_cursor.reset();
     d_rotxn.reset();
     return;
   }
-  
+
+  if(0 && d_dolog) {
+    g_log<<Logger::Warning<< "Query "<<((long)(void*)this)<<": "<<d_dtime.udiffNoReset()<<" usec to execute"<<endl;
+  }
+    
   d_inlist=false;
   d_lookuptype=type;
   d_lookupdomainid = zoneId;
+  d_firstrecord = true; // no need to go to next record in get_lookup
 }
 
 
 bool LMDBBackend::get_lookup(DNSResourceRecord& rr)
 {
-  if(!d_cursor) {
-    cout<<"Cursor not set, so get_lookup is done"<<endl;
+  if(!d_cursor)
     return false;
-  }
-
   for(;;) {
     MDBOutVal key, id;
-    if(d_cursor->get(key, id, MDB_GET_CURRENT)) {
-      cout<<"Could not get current!"<<endl;
+    int rc;
+    if(!d_firstrecord && (rc=d_cursor->get(key, id, MDB_NEXT_DUP))) {
+      if(rc != MDB_NOTFOUND)
+        cout<<"Got rc "<<mdb_strerror(rc)<<" on going to next dup"<<endl;
+      d_cursor.reset();
+      d_rotxn.reset();
+      auto udiff = d_dtime.udiffNoReset();
+      g_log<<Logger::Warning<< "Query "<<((long)(void*)this)<<": "<<udiff<<" total usec to last row"<<endl;
+
       return false;
     }
-    cout<<"Got a record id "<<id.get<uint32_t>()<<endl;
+    d_firstrecord = false;
+    
+
+    if(d_cursor->get(key, id, MDB_GET_CURRENT)) {
+      cout<<"Could not get current record id"<<endl;
+      return false;
+    }
+    //    cout<<"Got a record id "<<id.get<uint32_t>()<<endl;
     MDBOutVal record;
     if(d_rotxn->get(d_recordsdbi, id, record)) {
-      cout << "Could not find record id "<<id.get<uint32_t>()<<endl;
+      cout << "Could not find record with id "<<id.get<uint32_t>()<<endl;
       d_cursor.reset();
       d_rotxn.reset();
       return false;
     }
+
     serFromString(record.get<string>(), rr);
     rr.auth=1;
-    cout<<"Found: "<<rr.qname<<", "<<rr.qtype.getName()<<endl;
-    if(int rc=d_cursor->get(key, id, MDB_NEXT_DUP)) {
-      cout<<"Got rc "<<mdb_strerror(rc)<<" on going to next dup"<<endl;
-      d_cursor.reset();
-      d_rotxn.reset();
+    
+    if(d_lookuptype == QType::ANY || rr.qtype == d_lookuptype.getCode()) {
+      break;
     }
-    else
-      cout<<"Got id "<< id.get<uint32_t>()<<" after MDB_NEXT_DUP"<<endl;
-
-    if(d_lookuptype != QType::ANY && rr.qtype != d_lookuptype) { // this is not what we were looking for
-      cout << "Request was for "<<d_lookuptype.getName() <<", got "<<rr.qtype.getName()<<endl;
-      if(d_cursor)
-        continue;
-      else
-        return false;
-    }
-    break;
+    //    else 
+    //cout << "Request was for "<<d_lookuptype.getName() <<", got "<<rr.qtype.getName()<<", qname = "<<rr.qname<<endl;
+    // loop
   }
   return true;
 }
@@ -443,7 +514,7 @@ bool LMDBBackend::getDomainInfo(const DNSName &domain, DomainInfo &di, bool getS
 
 int LMDBBackend::genChangeDomain(const DNSName& domain, std::function<void(DomainInfo&)> func)
 {
-    shared_ptr<MDBRWTransaction> txn;
+  shared_ptr<MDBRWTransaction> txn;
   if(d_rwtxn) {
     txn = d_rwtxn;
     cout<<"Reusing open transaction"<<endl;
@@ -475,12 +546,63 @@ int LMDBBackend::genChangeDomain(const DNSName& domain, std::function<void(Domai
   return true;
 }
 
+int LMDBBackend::genChangeDomain(uint32_t id, std::function<void(DomainInfo&)> func)
+{
+  shared_ptr<MDBRWTransaction> txn;
+  if(d_rwtxn) {
+    txn = d_rwtxn;
+    cout<<"Reusing open transaction"<<endl;
+  }
+  else {
+    cout<<"Making a new RW txn record"<<endl;
+    txn = std::make_shared<MDBRWTransaction>(d_env->getRWTransaction());
+  }
+  MDBOutVal data;
+  if(txn->get(d_domainsdbi, id , data))
+    return false;
+
+  DomainInfo di;
+  serFromString(data.get<string>(), di);
+
+  func(di);
+  
+  txn->put(d_domainsdbi, id, serToString(di));
+
+  if(!d_rwtxn)
+    txn->commit();
+  return true;
+}
+
+
 bool LMDBBackend::setKind(const DNSName &domain, const DomainInfo::DomainKind kind)
 {
   return genChangeDomain(domain, [kind](DomainInfo& di) {
       di.kind = kind;
     });
 }
+
+bool LMDBBackend::setAccount(const DNSName &domain, const std::string& account)
+{
+  return genChangeDomain(domain, [account](DomainInfo& di) {
+      di.account = account;
+    });
+}
+
+
+void LMDBBackend::setFresh(uint32_t domain_id)
+{
+  genChangeDomain(domain_id, [](DomainInfo& di) {
+      di.last_check = time(0);
+    });
+}
+
+void LMDBBackend::setNotified(uint32_t domain_id, uint32_t serial)
+{
+  genChangeDomain(domain_id, [serial](DomainInfo& di) {
+      di.serial = serial;
+    });
+}
+
 
 bool LMDBBackend::setMaster(const DNSName &domain, const std::string& ips)
 {
@@ -507,7 +629,7 @@ bool LMDBBackend::createDomain(const DNSName &domain, const string &type, const 
   di.account = account;
   
   auto txn = d_env->getRWTransaction();
-  di.id = getMaxID(txn, d_domainsdbi);
+  di.id = getMaxID(txn, d_domainsdbi) + 1;
   cout<<"Assigned id "<<di.id<<endl;
   txn.put(d_domainsnameidxdbi, di.zone.toDNSStringLC(), di.id);
   cout<<"Stored in index"<<endl;
@@ -535,12 +657,66 @@ void LMDBBackend::getAllDomains(vector<DomainInfo> *domains, bool include_disabl
   }
 }
 
+
+
 void LMDBBackend::getUnfreshSlaveInfos(vector<DomainInfo>* domains)
 {
+  // LIES! Should check SOA serial XXX
   getAllDomains(domains);
 }
 
-/* SECOND PART */
+
+bool LMDBBackend::getAllDomainMetadata(const DNSName& name, std::map<std::string, std::vector<std::string> >& meta)
+{
+  meta.clear();
+  
+  auto txn = d_env->getROTransaction();
+  auto cursor = txn.getCursor(d_domainmetadbi);
+  bool first = true;
+  MDBInVal in(name.toDNSStringLC());
+  MDBOutVal key, data;
+  key.d_mdbval = in.d_mdbval;
+
+  // key = domain name, data is {kind,value}
+  while(!cursor.get(key, data, first ? MDB_SET : MDB_NEXT_DUP)) {
+    first = false;
+    std::pair<string, string> kvpair;
+    serFromString(data.get<string>(), kvpair);
+    meta[kvpair.first].push_back(kvpair.second);
+  }
+  return true;
+  
+}
+
+bool LMDBBackend::setDomainMetadata(const DNSName& name, const std::string& kind, const std::vector<std::string>& meta)
+{
+  auto txn = d_env->getRWTransaction();
+  auto cursor = txn.getCursor(d_domainmetadbi);
+  bool first = true;
+  MDBInVal in(name.toDNSStringLC());
+  MDBOutVal key, data;
+  key.d_mdbval = in.d_mdbval;
+
+  // key = domain name, data is {kind,value}
+  // remove the old 
+  while(!cursor.get(key, data, first ? MDB_SET : MDB_NEXT_DUP)) {
+    first = false;
+    std::pair<string, string> kvpair;
+    serFromString(data.get<string>(), kvpair);
+    if(kvpair.first == kind)
+      cursor.del();
+  }
+
+  // insert the new
+  for(const auto& m : meta) {
+    pair<string, string> kvpair{kind, m};
+    
+    txn.put(d_domainmetadbi, name.toDNSStringLC(), serToString(kvpair));
+  }
+  
+  txn.commit();
+  return true;
+}
 
 class LMDBFactory : public BackendFactory
 {
@@ -549,6 +725,7 @@ public:
   void declareArguments(const string &suffix="")
   {
     declare(suffix,"filename","Filename for lmdb","./pdns.lmdb");
+    declare(suffix,"sync-mode","Synchronisation mode: nosync, nometasync, mapasync","mapasync");
   }
   DNSBackend *make(const string &suffix="")
   {
