622644f749a0fdc72f1406505e8a37ec1e6ce535
Remi Gacogne <remi.gacogne@powerdns.com>
2018-12-13 16:42:01 +0100

    Add missing insertion operators for libcrypto's features

--- pdns/version.cc
+++ pdns/version.cc
@@ -90,16 +90,16 @@ void showBuildConfiguration()
     "fcontext " <<
 #endif
 #ifdef HAVE_LIBCRYPTO_ECDSA
-    "libcrypto-ecdsa "
+    "libcrypto-ecdsa " <<
 #endif
 #ifdef HAVE_LIBCRYPTO_ED25519
-    "libcrypto-ed25519 "
+    "libcrypto-ed25519 " <<
 #endif
 #ifdef HAVE_LIBCRYPTO_ED448
-    "libcrypto-ed448 "
+    "libcrypto-ed448 " <<
 #endif
 #ifdef HAVE_LIBCRYPTO_EDDSA
-    "libcrypto-eddsa "
+    "libcrypto-eddsa " <<
 #endif
 #ifdef HAVE_LIBDL
     "libdl " <<
