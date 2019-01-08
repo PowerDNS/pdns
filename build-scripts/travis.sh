421ea2f2766cfde85b4a7a46ff3bf92ca223d8a3
Josh Soref <jsoref@users.noreply.github.com>
2019-01-08 01:28:37 -0500

    drop deb bits

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -264,9 +264,7 @@ install_recursor() {
   run "cd .."
   run "wget http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip"
   run "unzip top-1m.csv.zip -d ${TRAVIS_BUILD_DIR}/regression-tests"
-  run 'echo -e "deb [arch=amd64] http://repo.powerdns.com/ubuntu $(lsb_release -cs)-auth-master main" | sudo tee /etc/apt/sources.list.d/pdns.list'
   run 'echo -e "Package: pdns-*\nPin: origin repo.powerdns.com\nPin-Priority: 9001" | sudo tee /etc/apt/preferences.d/pdns'
-  run 'curl https://repo.powerdns.com/CBC8B383-pub.asc | sudo apt-key add - '
   run 'sudo apt-get update'
   run 'sudo apt-get -y install pdns-server pdns-tools'
   run "sudo service pdns stop"
