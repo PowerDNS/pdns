f0c47afb608002eb21d0f774207093aaeac702d8
Josh Soref <jsoref@users.noreply.github.com>
2019-01-13 23:38:08 -0500

    BIND: avoid double //
    Fixes: Parsing file: ./zones//sub.test.dyndns, domain: sub.test.dyndns

--- pdns/test-bindparser_cc.cc
+++ pdns/test-bindparser_cc.cc
@@ -42,18 +42,18 @@ BOOST_AUTO_TEST_CASE(test_parser) {
                 BOOST_CHECK_EQUAL(domains[i].masters.size(), nmasters); \
         }
 
-        checkzone(0, example.com, "./zones//example.com", master, 0);
-        checkzone(1, test.com, "./zones//test.com", slave, 1);
+        checkzone(0, example.com, "./zones/example.com", master, 0);
+        checkzone(1, test.com, "./zones/test.com", slave, 1);
         BOOST_CHECK_EQUAL(domains[1].masters[0].toString(), ComboAddress("1.2.3.4", 5678).toString());
-        checkzone(2, test.dyndns, "./zones//test.dyndns", garblewarble, 0);
-        checkzone(3, wtest.com, "./zones//wtest.com", master, 0);
-        checkzone(4, nztest.com, "./zones//nztest.com", master, 0);
-        checkzone(5, dnssec-parent.com, "./zones//dnssec-parent.com", master, 0);
-        checkzone(6, delegated.dnssec-parent.com, "./zones//delegated.dnssec-parent.com", master, 0);
-        checkzone(7, secure-delegated.dnssec-parent.com, "./zones//secure-delegated.dnssec-parent.com", master, 0);
-        checkzone(8, minimal.com, "./zones//minimal.com", master, 0);
-        checkzone(9, tsig.com, "./zones//tsig.com", master, 0);
-        checkzone(10, stest.com, "./zones//stest.com", master, 0);
+        checkzone(2, test.dyndns, "./zones/test.dyndns", garblewarble, 0);
+        checkzone(3, wtest.com, "./zones/wtest.com", master, 0);
+        checkzone(4, nztest.com, "./zones/nztest.com", master, 0);
+        checkzone(5, dnssec-parent.com, "./zones/dnssec-parent.com", master, 0);
+        checkzone(6, delegated.dnssec-parent.com, "./zones/delegated.dnssec-parent.com", master, 0);
+        checkzone(7, secure-delegated.dnssec-parent.com, "./zones/secure-delegated.dnssec-parent.com", master, 0);
+        checkzone(8, minimal.com, "./zones/minimal.com", master, 0);
+        checkzone(9, tsig.com, "./zones/tsig.com", master, 0);
+        checkzone(10, stest.com, "./zones/stest.com", master, 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
