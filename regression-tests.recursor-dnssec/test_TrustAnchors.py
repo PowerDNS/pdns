247adec0cd9645ecc51bd20472f194d125e1ce2d
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2019-06-21 10:47:09 +0200

    Fix config template and remove old root trust anchor

--- regression-tests.recursor-dnssec/test_TrustAnchors.py
+++ regression-tests.recursor-dnssec/test_TrustAnchors.py
@@ -10,8 +10,10 @@ class testTrustAnchorsEnabled(RecursorTest):
     _confdir = 'TrustAnchorsEnabled'
     _roothints = None
     _root_DS = None
+    _config_template = """
+     allow-trust-anchor-query=yes
+"""
     _lua_config_file = """
-    allow-trust-anchor-query=yes
 addDS("powerdns.com", "44030 8 1 B763646757DF621DD1204AD3BFA0675B49BE3279")
 addNTA("example")
 addNTA("example.com", "some reason")
@@ -20,7 +22,7 @@ addNTA("example.com", "some reason")
     def testTrustanchorDotServer(self):
         expected = dns.rrset.from_text_list(
             'trustanchor.server.', 86400, dns.rdataclass.CH, 'TXT',
-            ['". 19036 20326"', '"powerdns.com. 44030"'])
+            ['". 20326"', '"powerdns.com. 44030"'])
         query = dns.message.make_query('trustanchor.server', 'TXT',
                                        dns.rdataclass.CH)
         result = self.sendUDPQuery(query)
