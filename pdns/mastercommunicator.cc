f159e13767c75d2671953d72e65f88460c7e748b
Kees Monshouwer <mind04@monshouwer.org>
2019-07-04 09:30:29 +0200

    auth: do not update identical notified serials

--- pdns/mastercommunicator.cc
+++ pdns/mastercommunicator.cc
@@ -120,8 +120,9 @@ bool CommunicatorClass::notifyDomain(const DNSName &domain, UeberBackend* B)
     return false;
   }
   queueNotifyDomain(di, B);
-  // call backend and tell them we sent out the notification - even though that is premature    
-  di.backend->setNotified(di.id, di.serial);
+  // call backend and tell them we sent out the notification - even though that is premature
+  if (di.serial != di.notified_serial)
+    di.backend->setNotified(di.id, di.serial);
 
   return true; 
 }
