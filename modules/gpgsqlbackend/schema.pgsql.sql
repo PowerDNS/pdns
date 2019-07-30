b2f3d1b699154e61c5b3c9e962c995cbee0dbe91
Peter van Dijk <peter.van.dijk@powerdns.com>
2019-07-30 15:44:07 +0200

    adjust gpgsql schema for advisory 2019-06

--- modules/gpgsqlbackend/schema.pgsql.sql
+++ modules/gpgsqlbackend/schema.pgsql.sql
@@ -4,7 +4,7 @@ CREATE TABLE domains (
   master                VARCHAR(128) DEFAULT NULL,
   last_check            INT DEFAULT NULL,
   type                  VARCHAR(6) NOT NULL,
-  notified_serial       INT DEFAULT NULL,
+  notified_serial       BIGINT DEFAULT NULL,
   account               VARCHAR(40) DEFAULT NULL,
   CONSTRAINT c_lowercase_name CHECK (((name)::TEXT = LOWER((name)::TEXT)))
 );
