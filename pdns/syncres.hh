90e8ea71abacb8879482fe27a5521d1f4d0ef76d
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2019-11-13 10:11:58 +0100

    ednsmap might be cleared while yielding; so reassign pointer.
    Switch away from a ref to a pointer because of above and use modern
    init for EDNSStatus.

--- pdns/syncres.hh
+++ pdns/syncres.hh
@@ -294,9 +294,8 @@ public:
 
   struct EDNSStatus
   {
-    EDNSStatus() : mode(UNKNOWN), modeSetAt(0) {}
-    enum EDNSMode { UNKNOWN=0, EDNSOK=1, EDNSIGNORANT=2, NOEDNS=3 } mode;
-    time_t modeSetAt;
+    time_t modeSetAt{0};
+    enum EDNSMode { UNKNOWN=0, EDNSOK=1, EDNSIGNORANT=2, NOEDNS=3 } mode{UNKNOWN};
   };
 
   enum class HardenNXD { No, DNSSEC, Yes };
