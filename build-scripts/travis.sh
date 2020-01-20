01c8c6da3bda05e400dfb6d41328bd2979898d8e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-01-20 12:13:28 -0800

    Use python3.4-venv for Travis.
    Ubuntu 14.04 Trusty Tahr doesn't have python3-venv, but does have
    python3.4-venv.

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -643,7 +643,7 @@ run "sudo apt-get -qq --no-install-recommends install \
   libluajit-5.1-dev \
   libedit-dev \
   libprotobuf-dev \
-  python3-venv \
+  python3.4-venv \
   protobuf-compiler"
 
 run "cd .."
