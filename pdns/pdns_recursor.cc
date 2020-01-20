22ca6a4b83cc7ee99b330680d63a06038c1251d4
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2020-01-20 14:42:16 +0100

    Better function name as suggested by rgacogne.
    (cherry picked from commit c6042a88ccf588e718daf1ddd35622d85b7f3dde)

--- pdns/pdns_recursor.cc
+++ pdns/pdns_recursor.cc
@@ -3812,12 +3812,12 @@ static void setupNODGlobal()
 }
 #endif /* NOD_ENABLED */
 
-static void checkDir(void)
+static void checkSocketDir(void)
 {
   struct stat st;
   string dir(::arg()["socket-dir"]);
   string msg;
-  
+
   if (stat(dir.c_str(), &st) == -1) {
     msg = "it does not exist or cannot access";
   }
@@ -4235,8 +4235,8 @@ static int serviceMain(int argc, char*argv[])
       g_log<<Logger::Info<<"Chrooted to '"<<::arg()["chroot"]<<"'"<<endl;
   }
 
-  checkDir();
-  
+  checkSocketDir();
+
   s_pidfname=::arg()["socket-dir"]+"/"+s_programname+".pid";
   if(!s_pidfname.empty())
     unlink(s_pidfname.c_str()); // remove possible old pid file
