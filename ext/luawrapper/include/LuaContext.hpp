33c19e1a8c8287e34140d5a51308ac9aa713376a
Peter van Dijk <peter.van.dijk@powerdns.com>
2020-03-17 16:49:32 +0100

    lua: don't destroy keys during table iteration
    via https://github.com/ahupowerdns/luawrapper/pull/38

--- ext/luawrapper/include/LuaContext.hpp
+++ ext/luawrapper/include/LuaContext.hpp
@@ -2649,11 +2649,21 @@ struct LuaContext::Reader<std::string>
     static auto read(lua_State* state, int index)
         -> boost::optional<std::string>
     {
+        std::string result;
+
+        // lua_tolstring might convert the variable that would confuse lua_next, so we
+        //   make a copy of the variable.
+        lua_pushvalue(state, index);
+
         size_t len;
-        const auto val = lua_tolstring(state, index, &len);
-        if (val == 0)
-            return boost::none;
-        return std::string(val, len);
+        const auto val = lua_tolstring(state, -1, &len);
+
+        if (val != 0)
+          result.assign(val, len);
+
+        lua_pop(state, 1);
+
+        return val != 0 ? boost::optional<std::string>{ std::move(result) } : boost::none;
     }
 };
 
