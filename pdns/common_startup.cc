297b769e456e8595fd99149ab2f96613a898761d
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2020-05-18 13:35:54 +0200

    "Adress" is actually prural in query-local-address
    Co-authored-by: Peter van Dijk <peter.van.dijk@powerdns.com>

--- pdns/common_startup.cc
+++ pdns/common_startup.cc
@@ -92,7 +92,7 @@ void declareArguments()
   ::arg().setSwitch("local-address-nonexist-fail","Fail to start if one or more of the local-address's do not exist on this server")="yes";
   ::arg().setSwitch("non-local-bind", "Enable binding to non-local addresses by using FREEBIND / BINDANY socket options")="no";
   ::arg().setSwitch("reuseport","Enable higher performance on compliant kernels by using SO_REUSEPORT allowing each receiver thread to open its own socket")="no";
-  ::arg().set("query-local-address","Source IP address for sending queries")="0.0.0.0 ::";
+  ::arg().set("query-local-address","Source IP addresses for sending queries")="0.0.0.0 ::";
   ::arg().set("query-local-address6","DEPRECATED: Use query-local-address. Source IPv6 address for sending queries")="";
   ::arg().set("overload-queue-length","Maximum queuelength moving to packetcache only")="0";
   ::arg().set("max-queue-length","Maximum queuelength before considering situation lost")="5000";
