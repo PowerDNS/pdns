a9bff9e82781547ab68a169bce9c131a5d2197bf
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2020-11-06 12:58:53 +0100

    If a.b.c CNAME x.a.b.c is encoutered, switch off QM, it will lead to great recursion depth without result.
    (cherry picked from commit 157d17730c4fa6b25c1e0f5e720b7e73ac7723aa)

--- pdns/syncres.cc
+++ pdns/syncres.cc
@@ -1350,6 +1350,13 @@ bool SyncRes::doCNAMECacheCheck(const DNSName &qname, const QType &qtype, vector
         throw ImmediateServFailException(msg);
       }
 
+      if (newTarget.isPartOf(qname)) {
+        // a.b.c. CNAME x.a.b.c will go to great depths with QM on
+        string msg = "got a CNAME referral (from cache) to child, switching off QM";
+        LOG(prefix<<qname<<": "<<msg<<endl);
+        setQNameMinimization(false);
+      }
+
       // Check to see if we already have seen the new target as a previous target
       if (scanForCNAMELoop(newTarget, ret)) {
         string msg = "got a CNAME referral (from cache) that causes a loop";
@@ -3433,6 +3440,12 @@ bool SyncRes::processAnswer(unsigned int depth, LWResult& lwr, const DNSName& qn
       ret.clear();
       *rcode = RCode::ServFail;
       return true;
+    }
+
+    if (newtarget.isPartOf(qname)) {
+      // a.b.c. CNAME x.a.b.c will go to great depths with QM on
+      LOG(prefix<<qname<<": status=got a CNAME referral to child, disabling QM"<<endl);
+      setQNameMinimization(false);
     }
 
     if(depth > 10) {
