43445d8490f1e7481633e4645249a7b128039e87
Remi Gacogne <remi.gacogne@powerdns.com>
2020-11-12 09:01:23 +0100

    Add a fuzzing target for YaHTTP

---
+++ pdns/fuzz_yahttp.cc
@@ -0,0 +1,47 @@
+/*
+ * This file is part of PowerDNS or dnsdist.
+ * Copyright -- PowerDNS.COM B.V. and its contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * In addition, for the avoidance of any doubt, permission is granted to
+ * link this program with OpenSSL and to (re)distribute the binaries
+ * produced as the result of such linking.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <yahttp/yahttp.hpp>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size);
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+  try {
+    YaHTTP::AsyncRequestLoader yarl;
+    YaHTTP::Request req;
+
+    yarl.initialize(&req);
+    bool finished = yarl.feed(std::string(reinterpret_cast<const char*>(data), size));
+    if (finished) {
+      yarl.finalize();
+    }
+  }
+  catch (const YaHTTP::ParseError& e) {
+  }
+  catch (const std::exception& e) {
+  }
+  catch (...) {
+  }
+
+  return 0;
+}
