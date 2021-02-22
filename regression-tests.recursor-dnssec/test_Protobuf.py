fe1a938994554b6c750c31ce49f587011df40f22
Otto <otto.moerbeek@open-xchange.com>
2021-02-22 12:02:31 +0100

    Add regress test for appliedPolicyKind

--- regression-tests.recursor-dnssec/test_Protobuf.py
+++ regression-tests.recursor-dnssec/test_Protobuf.py
@@ -194,16 +194,18 @@ class TestRecursorProtobuf(RecursorTest):
             self.assertEquals(record.ttl, rttl)
         self.assertTrue(record.HasField('rdata'))
 
-    def checkProtobufPolicy(self, msg, policyType, reason, trigger, hit):
+    def checkProtobufPolicy(self, msg, policyType, reason, trigger, hit, kind):
         self.assertEquals(msg.type, dnsmessage_pb2.PBDNSMessage.DNSResponseType)
         self.assertTrue(msg.response.HasField('appliedPolicyType'))
         self.assertTrue(msg.response.HasField('appliedPolicy'))
         self.assertTrue(msg.response.HasField('appliedPolicyTrigger'))
         self.assertTrue(msg.response.HasField('appliedPolicyHit'))
+        self.assertTrue(msg.response.HasField('appliedPolicyKind'))
         self.assertEquals(msg.response.appliedPolicy, reason)
         self.assertEquals(msg.response.appliedPolicyType, policyType)
         self.assertEquals(msg.response.appliedPolicyTrigger, trigger)
         self.assertEquals(msg.response.appliedPolicyHit, hit)
+        self.assertEquals(msg.response.appliedPolicyKind, kind)
 
     def checkProtobufTags(self, msg, tags):
         print(tags)
@@ -864,7 +866,7 @@ sub.test 3600 IN A 192.0.2.42
         # then the response
         msg = self.getFirstProtobufMessage()
         self.checkProtobufResponse(msg, dnsmessage_pb2.PBDNSMessage.UDP, res)
-        self.checkProtobufPolicy(msg, dnsmessage_pb2.PBDNSMessage.PolicyType.QNAME, 'zone.rpz.', '*.test.example.', 'sub.test.example')
+        self.checkProtobufPolicy(msg, dnsmessage_pb2.PBDNSMessage.PolicyType.QNAME, 'zone.rpz.', '*.test.example.', 'sub.test.example', dnsmessage_pb2.PBDNSMessage.PolicyKind.NoAction)
         self.assertEquals(len(msg.response.rrs), 1)
         rr = msg.response.rrs[0]
         # we have max-cache-ttl set to 15
@@ -931,7 +933,7 @@ sub.test 3600 IN A 192.0.2.42
         # then the response
         msg = self.getFirstProtobufMessage()
         self.checkProtobufResponse(msg, dnsmessage_pb2.PBDNSMessage.UDP, res)
-        self.checkProtobufPolicy(msg, dnsmessage_pb2.PBDNSMessage.PolicyType.QNAME, 'zone.rpz.', '*.test.example.', 'sub.test.example')
+        self.checkProtobufPolicy(msg, dnsmessage_pb2.PBDNSMessage.PolicyType.QNAME, 'zone.rpz.', '*.test.example.', 'sub.test.example', dnsmessage_pb2.PBDNSMessage.PolicyKind.NoAction)
         self.checkProtobufTags(msg, self._tags + self._tags_from_gettag + self._tags_from_rpz)
         self.assertEquals(len(msg.response.rrs), 1)
         rr = msg.response.rrs[0]
