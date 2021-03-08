c8567be6a05473c24409b1ca8965023452c2a10b
Remi Gacogne <remi.gacogne@powerdns.com>
2021-03-08 12:03:07 +0100

    rec: Fix the EDNS padding python implementation for dnspython 2.0.0

--- regression-tests.recursor-dnssec/paddingoption.py
+++ regression-tests.recursor-dnssec/paddingoption.py
@@ -14,10 +14,13 @@ class PaddingOption(dns.edns.Option):
         super(PaddingOption, self).__init__(12)
         self.numberOfBytes = numberOfBytes
 
-    def to_wire(self, file):
+    def to_wire(self, file=None):
         """Create EDNS packet as defined in rfc7830."""
 
-        file.write(bytes(self.numberOfBytes))
+        if file:
+            file.write(bytes(self.numberOfBytes))
+        else:
+            return bytes(self.numberOfBytes)
 
     def from_wire(cls, otype, wire, current, olen):
         """Read EDNS packet as defined in rfc7830.
@@ -31,6 +34,12 @@ class PaddingOption(dns.edns.Option):
         return cls(numberOfBytes)
 
     from_wire = classmethod(from_wire)
+
+    # needed in 2.0.0
+    @classmethod
+    def from_wire_parser(cls, otype, parser):
+        data = parser.get_remaining()
+        return cls(len(data))
 
     def __repr__(self):
         return '%s(%d)' % (
