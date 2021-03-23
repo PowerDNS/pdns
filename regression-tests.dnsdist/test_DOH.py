PowerDNS/pdns

commit 341d2553b74c579df9d9843959f3ca6f5c3dc954

dnsdist: Use vectors instead of C arrays as buffers

pdns/dnsdistdist/doh.cc
@@ -328,7 +328,7 @@ static const std::string& getReasonFromStatusCode(uint16_t statusCode)
 }
 
 /* Always called from the main DoH thread */
-static void handleResponse(DOHFrontend& df, st_h2o_req_t* req, uint16_t statusCode, const std::string& response, const std::vector<std::pair<std::string, std::string>>& customResponseHeaders, const std::string& contentType, bool addContentType)
+static void handleResponse(DOHFrontend& df, st_h2o_req_t* req, uint16_t statusCode, const std::vector<uint8_t>& response, const std::vector<std::pair<std::string, std::string>>& customResponseHeaders, const std::string& contentType, bool addContentType)
 {
   constexpr int overwrite_if_exists = 1;
   constexpr int maybe_token = 1;
@@ -352,7 +352,7 @@ static void handleResponse(DOHFrontend& df, st_h2o_req_t* req, uint16_t statusCo
     }
 
     if (df.d_sendCacheControlHeaders && !response.empty()) {
-      uint32_t minTTL = getDNSPacketMinTTL(response.data(), response.size());
+      uint32_t minTTL = getDNSPacketMinTTL(reinterpret_cast<const char*>(response.data()), response.size());
       if (minTTL != std::numeric_limits<uint32_t>::max()) {
         std::string cacheControlValue = "max-age=" + std::to_string(minTTL);
         /* we need to duplicate the header content because h2o keeps a pointer and we will be deleted before the response has been sent */
@@ -362,18 +362,19 @@ static void handleResponse(DOHFrontend& df, st_h2o_req_t* req, uint16_t statusCo
     }
 
     req->res.content_length = response.size();
-    h2o_send_inline(req, response.c_str(), response.size());
+    h2o_send_inline(req, reinterpret_cast<const char*>(response.data()), response.size());
   }
   else if (statusCode >= 300 && statusCode < 400) {
     /* in that case the response is actually a URL */
     /* we need to duplicate the URL because h2o uses it for the location header, keeping a pointer, and we will be deleted before the response has been sent */
-    h2o_iovec_t url = h2o_strdup(&req->pool, response.c_str(), response.size());
+    h2o_iovec_t url = h2o_strdup(&req->pool, reinterpret_cast<const char*>(response.data()), response.size());
     h2o_send_redirect(req, statusCode, getReasonFromStatusCode(statusCode).c_str(), url.base, url.len);
     ++df.d_redirectresponses;
   }
   else {
-    if (!response.empty()) {
-      h2o_send_error_generic(req, statusCode, getReasonFromStatusCode(statusCode).c_str(), response.c_str(), H2O_SEND_ERROR_KEEP_HEADERS);
+    // we need to make sure it's null-terminated */
+    if (!response.empty() && response.at(response.size() - 1) == 0) {
+      h2o_send_error_generic(req, statusCode, getReasonFromStatusCode(statusCode).c_str(), reinterpret_cast<const char*>(response.data()), H2O_SEND_ERROR_KEEP_HEADERS);
     }
     else {
       switch(statusCode) {
@@ -434,26 +435,25 @@ static int processDOHQuery(DOHUnit* du)
        rings for example */
     struct timespec queryRealTime;
     gettime(&queryRealTime, true);
-    uint16_t len = du->query.length();
-    /* We reserve at least 512 additional bytes to be able to add EDNS, but we also want
-       at least s_maxPacketCacheEntrySize bytes to be able to spoof the content or fill the answer from the packet cache */
-    du->query.resize(std::max(du->query.size() + 512, s_maxPacketCacheEntrySize));
-    size_t bufferSize = du->query.size();
-    auto query = const_cast<char*>(du->query.c_str());
-    struct dnsheader* dh = reinterpret_cast<struct dnsheader*>(query);
 
-    if (!checkQueryHeaders(dh)) {
-      du->status_code = 400;
-      return -1; // drop
+    {
+      /* don't keep that pointer around, it will be invalidated if the buffer is ever resized */
+      struct dnsheader* dh = reinterpret_cast<struct dnsheader*>(du->query.data());
+
+      if (!checkQueryHeaders(dh)) {
+        du->status_code = 400;
+        return -1; // drop
+      }
+
+      queryId = ntohs(dh->id);
     }
 
     uint16_t qtype, qclass;
-    unsigned int consumed = 0;
-    DNSName qname(query, len, sizeof(dnsheader), false, &qtype, &qclass, &consumed);
-    DNSQuestion dq(&qname, qtype, qclass, consumed, &du->dest, &du->remote, dh, bufferSize, len, false, &queryRealTime);
+    unsigned int qnameWireLength = 0;
+    DNSName qname(reinterpret_cast<const char*>(du->query.data()), du->query.size(), sizeof(dnsheader), false, &qtype, &qclass, &qnameWireLength);
+    DNSQuestion dq(&qname, qtype, qclass, &du->dest, &du->remote, du->query, false, &queryRealTime);
     dq.ednsAdded = du->ednsAdded;
     dq.du = du;
-    queryId = ntohs(dh->id);
     dq.sni = std::move(du->sni);
 
     std::shared_ptr<DownstreamState> ss{nullptr};
@@ -466,12 +466,13 @@ static int processDOHQuery(DOHUnit* du)
 
     if (result == ProcessQueryResult::SendAnswer) {
       if (du->response.empty()) {
-        du->response = std::string(reinterpret_cast<char*>(dq.dh), dq.len);
+        du->response = std::move(du->query);
       }
       /* increase the ref counter before sending the pointer */
       du->get();
 
       static_assert(sizeof(du) <= PIPE_BUF, "Writes up to PIPE_BUF are guaranteed not to be interleaved and to either fully succeed or fail");
+
       ssize_t sent = write(du->rsock, &du, sizeof(du));
       if (sent != sizeof(du)) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -533,8 +534,10 @@ static int processDOHQuery(DOHUnit* du)
     ids->du = du;
 
     ids->cs = &cs;
-    ids->origID = dh->id;
+    ids->origID = queryId;
     setIDStateFromDNSQuestion(*ids, dq, std::move(qname));
+
+    dq.getHeader()->id = idOffset;
 
     /* If we couldn't harvest the real dest addr, still
        write down the listening addr since it will be useful
@@ -551,8 +554,6 @@ static int processDOHQuery(DOHUnit* du)
       ids->destHarvested = false;
     }
 
-    dh->id = idOffset;
-
     if (ss->useProxyProtocol) {
       addProxyProtocol(dq);
     }
@@ -560,7 +561,7 @@ static int processDOHQuery(DOHUnit* du)
     int fd = pickBackendSocketForSending(ss);
     try {
       /* you can't touch du after this line, because it might already have been freed */
-      ssize_t ret = udpClientSendRequestToBackend(ss, fd, query, dq.len);
+      ssize_t ret = udpClientSendRequestToBackend(ss, fd, du->query);
 
       if(ret < 0) {
         /* we are about to handle the error, make sure that
@@ -653,12 +654,12 @@ static void on_generator_dispose(void *_self)
 /* This executes in the main DoH thread.
    We allocate a DOHUnit and send it to dnsdistclient() function in the doh client thread
    via a pipe */
-static void doh_dispatch_query(DOHServerConfig* dsc, h2o_handler_t* self, h2o_req_t* req, std::string&& query, const ComboAddress& local, const ComboAddress& remote, std::string&& path)
+static void doh_dispatch_query(DOHServerConfig* dsc, h2o_handler_t* self, h2o_req_t* req, std::vector<uint8_t>&& query, const ComboAddress& local, const ComboAddress& remote, std::string&& path)
 {
   try {
     /* we only parse it there as a sanity check, we will parse it again later */
     uint16_t qtype;
-    DNSName qname(query.c_str(), query.size(), sizeof(dnsheader), false, &qtype);
+    DNSName qname(reinterpret_cast<const char*>(query.data()), query.size(), sizeof(dnsheader), false, &qtype);
 
     auto du = std::unique_ptr<DOHUnit>(new DOHUnit);
     du->dsc = dsc;
@@ -830,9 +831,10 @@ try
       ++dsc->cs->tlsUnknownqueries;
   }
 
+  #warning turn these into string_view?
   string path(req->path.base, req->path.len);
-
   string pathOnly(req->path_normalized.base, req->path_normalized.len);
+
   if (dsc->paths.count(pathOnly) == 0) {
     h2o_send_error_404(req, "Not Found", "there is no endpoint configured for this path", 0);
     return 0;
@@ -853,11 +855,12 @@ try
     else
       ++dsc->df->d_http1Stats.d_nbQueries;
 
-    std::string query;
+    std::vector<uint8_t> query;
     /* We reserve at least 512 additional bytes to be able to add EDNS, but we also want
        at least s_maxPacketCacheEntrySize bytes to be able to fill the answer from the packet cache */
     query.reserve(std::max(req->entity.len + 512, s_maxPacketCacheEntrySize));
-    query.assign(req->entity.base, req->entity.len);
+    query.resize(req->entity.len);
+    memcpy(query.data(), req->entity.base, req->entity.len);
     doh_dispatch_query(dsc, self, req, std::move(query), local, remote, std::move(path));
   }
   else if(req->query_at != SIZE_MAX && (req->path.len - req->query_at > 5)) {
@@ -879,7 +882,8 @@ try
         break;
       }
 
-      string decoded;
+      std::string decoded;
+
       /* rough estimate so we hopefully don't need a new allocation later */
       /* We reserve at least 512 additional bytes to be able to add EDNS, but we also want
          at least s_maxPacketCacheEntrySize bytes to be able to fill the answer from the packet cache */
@@ -897,7 +901,9 @@ try
         else
           ++dsc->df->d_http1Stats.d_nbQueries;
 
-        doh_dispatch_query(dsc, self, req, std::move(decoded), local, remote, std::move(path));
+#warning FIXME: performance
+        auto vect = std::vector<uint8_t>(decoded.begin(), decoded.end());
+        doh_dispatch_query(dsc, self, req, std::move(vect), local, remote, std::move(path));
       }
     }
     else
@@ -1029,10 +1035,17 @@ std::string DOHUnit::getHTTPQueryString() const
   }
 }
 
-void DOHUnit::setHTTPResponse(uint16_t statusCode, const std::string& body_, const std::string& contentType_)
+void DOHUnit::setHTTPResponse(uint16_t statusCode, std::vector<uint8_t>&& body_, const std::string& contentType_)
 {
   status_code = statusCode;
-  response = body_;
+  response = std::move(body_);
+  if (!response.empty() && statusCode >= 400) {
+    // we need to make sure it's null-terminated */
+    if (response.at(response.size() - 1) != 0) {
+      response.push_back(0);
+    }
+  }
+
   contentType = contentType_;
 }
 
@@ -1068,14 +1081,14 @@ static void dnsdistclient(int qsock)
 
       // if there was no EDNS, we add it with a large buffer size
       // so we can use UDP to talk to the backend.
-      auto dh = const_cast<struct dnsheader*>(reinterpret_cast<const struct dnsheader*>(du->query.c_str()));
+      auto dh = const_cast<struct dnsheader*>(reinterpret_cast<const struct dnsheader*>(du->query.data()));
 
       if (!dh->arcount) {
         std::string res;
         generateOptRR(std::string(), res, 4096, 0, false);
 
-        du->query += res;
-        dh = const_cast<struct dnsheader*>(reinterpret_cast<const struct dnsheader*>(du->query.c_str())); // may have reallocated
+        du->query.insert(du->query.end(), res.begin(), res.end());
+        dh = const_cast<struct dnsheader*>(reinterpret_cast<const struct dnsheader*>(du->query.data())); // may have reallocated
         dh->arcount = htons(1);
         du->ednsAdded = true;
       }
@@ -1089,6 +1102,7 @@ static void dnsdistclient(int qsock)
         du->get();
 
         static_assert(sizeof(du) <= PIPE_BUF, "Writes up to PIPE_BUF are guaranteed not to be interleaved and to either fully succeed or fail");
+
         ssize_t sent = write(du->rsock, &du, sizeof(du));
         if (sent != sizeof(du)) {
           if (errno == EAGAIN || errno == EWOULDBLOCK) {

commit 80a6365917392df23a66e5853f650580c492b802

dnsdist: Fix the handling of DoH queries with a non-zero ID
rfc8484 states that clients "SHOULD use a DNS ID of 0 in every DNS
request", not MUST, so it does indeed happen.
The issue was introduced in 341d2553b74c579df9d9843959f3ca6f5c3dc954
when we moved to a safer PacketBuffer.

pdns/dnsdistdist/doh.cc
@@ -535,7 +535,7 @@ static int processDOHQuery(DOHUnit* du)
     ids->du = du;
 
     ids->cs = &cs;
-    ids->origID = queryId;
+    ids->origID = htons(queryId);
     setIDStateFromDNSQuestion(*ids, dq, std::move(qname));
 
     dq.getHeader()->id = idOffset;

regression-tests.dnsdist/test_DOH.py
@@ -216,6 +216,33 @@ class TestDOH(DNSDistDOHTest):
         self.assertEquals(response, receivedResponse)
         self.checkHasHeader('cache-control', 'max-age=3600')
 
+    def testDOHTransactionID(self):
+        """
+        DOH: Simple query with ID != 0
+        """
+        name = 'simple-with-non-zero-id.doh.tests.powerdns.com.'
+        query = dns.message.make_query(name, 'A', 'IN', use_edns=False)
+        query.id = 42
+        expectedQuery = dns.message.make_query(name, 'A', 'IN', use_edns=True, payload=4096)
+        expectedQuery.id = 0
+        response = dns.message.make_response(query)
+        rrset = dns.rrset.from_text(name,
+                                    3600,
+                                    dns.rdataclass.IN,
+                                    dns.rdatatype.A,
+                                    '127.0.0.1')
+        response.answer.append(rrset)
+
+        (receivedQuery, receivedResponse) = self.sendDOHQuery(self._dohServerPort, self._serverName, self._dohBaseURL, query, response=response, caFile=self._caCert)
+        self.assertTrue(receivedQuery)
+        self.assertTrue(receivedResponse)
+        receivedQuery.id = expectedQuery.id
+        self.assertEquals(expectedQuery, receivedQuery)
+        self.checkQueryEDNSWithoutECS(expectedQuery, receivedQuery)
+        self.assertEquals(response, receivedResponse)
+        # just to be sure the ID _is_ checked
+        self.assertEquals(response.id, receivedResponse.id)
+
     def testDOHSimplePOST(self):
         """
         DOH: Simple POST query