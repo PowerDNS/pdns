0ffb7a917ffd31f0e7b49e4dfefc654728a773e8
Otto <otto.moerbeek@open-xchange.com>
2021-03-29 10:40:37 +0200

    More fail-safe handling of NOD files
    (cherry picked from commit 7b4cc738ffaaec7173f39fc140799f2200d93e61)

--- pdns/recursordist/stable-bloom.hh
+++ pdns/recursordist/stable-bloom.hh
@@ -106,13 +106,28 @@ public:
     uint8_t k, p;
     uint32_t num_cells, bitstr_len;
     is.read((char*)&k, sizeof(k));
+    if (is.fail()) {
+      throw std::runtime_error("SBF: read failed (file too short?)");
+    }
     is.read((char*)&num_cells, sizeof(num_cells));
+    if (is.fail()) {
+      throw std::runtime_error("SBF: read failed (file too short?)");
+    }
     num_cells = ntohl(num_cells);
     is.read((char*)&p, sizeof(p));
+    if (is.fail()) {
+      throw std::runtime_error("SBF: read failed (file too short?)");
+    }
     is.read((char*)&bitstr_len, sizeof(bitstr_len));
+    if (is.fail()) {
+      throw std::runtime_error("SBF: read failed (file too short?)");
+    }
     bitstr_len = ntohl(bitstr_len);
     char* bitcstr = new char[bitstr_len];
     is.read((char*)bitcstr, bitstr_len);
+    if (is.fail()) {
+      throw std::runtime_error("SBF: read failed (file too short?)");
+    }
     std::string bitstr(bitcstr, bitstr_len);
     delete[] bitcstr;
     stableBF tempbf(k, num_cells, p, bitstr);
