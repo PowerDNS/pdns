4f57dec4a7f1b94c6da4c0d3fdfecf73781bbcb2
Peter van Dijk <peter.van.dijk@powerdns.com>
2021-04-01 14:34:36 +0200

    fix formatting

--- pdns/fuzz_dnslabeltext_parseRFC1035CharString.cc
+++ pdns/fuzz_dnslabeltext_parseRFC1035CharString.cc
@@ -24,7 +24,8 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size);
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
 
   std::string val;
 
