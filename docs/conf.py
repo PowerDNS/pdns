d169d15332e7dc8434acf53b3e8cb6cace0a241f
Otto <otto.moerbeek@open-xchange.com>
2021-04-22 08:34:42 +0200

    Determine copyright upper bound year by code

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ import glob
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import guzzle_sphinx_theme
+import datetime
 
 # -- General configuration ------------------------------------------------
 
@@ -51,7 +52,7 @@ master_doc = 'indexTOC'
 
 # General information about the project.
 project = 'PowerDNS Authoritative Server'
-copyright = '2001-2019, PowerDNS.COM BV'
+copyright = '2001-' + str(datetime.date.today().year) + ', PowerDNS.COM BV'
 author = 'PowerDNS.COM BV'
 
 # The version info for the project you're documenting, acts as replacement for
