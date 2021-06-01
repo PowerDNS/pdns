46c08b5150e014375d2fed0a194f1355807ac157
Pieter Lexis <pieter.lexis@powerdns.com>
2021-06-01 13:18:54 +0200

    Make some cookie sizes constant, fix nits

--- pdns/dnspacket.cc
+++ pdns/dnspacket.cc
@@ -291,7 +291,7 @@ void DNSPacket::wrapup()
 
   if (d_haveednscookie) {
     if (d_eco.isWellFormed()) {
-        optsize += 24;
+        optsize += EDNSCOOKIEOPTSIZE;
     }
   }
 
--- pdns/ednscookies.cc
+++ pdns/ednscookies.cc
@@ -19,10 +19,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+#include "config.h"
 #include "ednscookies.hh"
 #include "misc.hh"
 
-#include "config.h"
 #ifdef HAVE_CRYPTO_SHORTHASH
 #include <sodium.h>
 #endif
