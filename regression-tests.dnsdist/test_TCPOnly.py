f39b85091263ede6ee3c5b9ae464ecf9287b9232
Remi Gacogne <remi.gacogne@powerdns.com>
2021-06-09 12:06:52 +0200

    dnsdist: Fix an EDNS UDP payload size issue for UDP -> TCP only

---
+++ regression-tests.dnsdist/test_TCPOnly.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import dns
+
+from dnsdisttests import DNSDistTest
+
+class TestTCPOnly(DNSDistTest):
+
+    _config_template = """
+    newServer{address="127.0.0.1:%s", tcpOnly=true}
+    """
+
+    def testUDP(self):
+        """
+        TCP Only: UDP query is sent via TCP
+        """
+        name = 'udp.tcp-only.test.powerdns.com.'
+        query = dns.message.make_query(name, 'A', 'IN')
+        expectedResponse = dns.message.make_response(query)
+        rrset = dns.rrset.from_text(name,
+                                    60,
+                                    dns.rdataclass.IN,
+                                    dns.rdatatype.A,
+                                    '127.0.0.1')
+        expectedResponse.answer.append(rrset)
+
+        (receivedQuery, receivedResponse) = self.sendUDPQuery(query, expectedResponse)
+        self.assertEqual(query, receivedQuery)
+        self.assertEqual(receivedResponse, expectedResponse)
+
+        if 'UDP Responder' in self._responsesCounter:
+            self.assertEqual(self._responsesCounter['UDP Responder'], 0)
+        if 'TCP Responder' in self._responsesCounter:
+            self.assertEqual(self._responsesCounter['TCP Responder'], 1)
+
+    def testTCP(self):
+        """
+        TCP Only: TCP query is sent via TCP
+        """
+        name = 'tcp.tcp-only.test.powerdns.com.'
+        query = dns.message.make_query(name, 'A', 'IN')
+        expectedResponse = dns.message.make_response(query)
+        rrset = dns.rrset.from_text(name,
+                                    60,
+                                    dns.rdataclass.IN,
+                                    dns.rdatatype.A,
+                                    '127.0.0.1')
+        expectedResponse.answer.append(rrset)
+
+        (receivedQuery, receivedResponse) = self.sendTCPQuery(query, expectedResponse)
+        self.assertEqual(query, receivedQuery)
+        self.assertEqual(receivedResponse, expectedResponse)
+        if 'UDP Responder' in self._responsesCounter:
+            self.assertEqual(self._responsesCounter['UDP Responder'], 0)
+        if 'TCP Responder' in self._responsesCounter:
+            self.assertEqual(self._responsesCounter['TCP Responder'], 1)
