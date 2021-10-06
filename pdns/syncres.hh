95e42c87e39a5c12c53300b683a590cf4c3caad5
Otto <otto.moerbeek@open-xchange.com>
2021-10-06 10:37:28 +0200

    Minimal backport of #10632: match ordering of PacketID using the Birtdah vs non-Birtday comparator

--- pdns/syncres.hh
+++ pdns/syncres.hh
@@ -974,7 +974,7 @@ struct PacketID
     if( tie(remote, ourSock, type) > tie(b.remote, bSock, b.type))
       return false;
 
-    return tie(fd, id, domain) < tie(b.fd, b.id, b.domain);
+    return tie(domain, fd, id) < tie(b.domain, b.fd, b.id);
   }
 };
 
