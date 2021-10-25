d66aafa7d9509d0238de209f61094b6d63ebb06d
Otto <otto.moerbeek@open-xchange.com>
2021-10-25 15:20:18 +0200

    tweaks and undo error entry

--- docs/secpoll-check.sh
+++ docs/secpoll-check.sh
@@ -3,7 +3,7 @@ if [ $# -ne 1 ]; then
   echo usage: $0 file
   exit 1
 fi
-if ! egrep -v '^(@|;|$)' "$1" | egrep -v '(auth|recursor|dnsdist)-[0-9]\.[0-9]\.[0-9](-(alpha|beta|rc)[0-9]+)?[-_a-z0-9\.]*\.security-status +60 IN TXT "[1-3].*"' 
+if ! egrep -v '^(@|;|$)' "$1" | egrep -v 'ubuntu|debian|raspbian|fedora' | egrep -v '(auth|recursor|dnsdist)-[0-9]+\.[0-9]+\.[0-9]+(-(alpha|beta|rc)[0-9]+)?\.security-status +60 IN TXT "[1-3].*"' 
 then
 echo OK
 exit 0
