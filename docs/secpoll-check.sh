a10a1d5edef34cc2030403e12ad2d26faf9ce407
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2021-11-16 07:30:34 +0100

    indent

--- docs/secpoll-check.sh
+++ docs/secpoll-check.sh
@@ -5,8 +5,8 @@ if [ $# -ne 1 ]; then
 fi
 if egrep -v '^(@|;|$)' "$1" | egrep -v 'ubuntu|debian|raspbian|fedora' | egrep -v '(auth|recursor|dnsdist)-[0-9]+\.[0-9]+\.[0-9]+(-(alpha|beta|rc)[0-9]+)?\.security-status +60 IN TXT "[1-3].*"' 
 then
-echo Not OK
-exit 1
+  echo Not OK
+  exit 1
 fi
 echo OK
 exit 0
