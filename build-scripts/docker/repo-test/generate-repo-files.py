04a782dd915280be9af6c210f3bc5299446587b3
Erik Winkels <erik.winkels@powerdns.com>
2021-11-17 14:09:55 +0100

    Add `rec-45` for Debian Bullseye to repo test script.

--- build-scripts/docker/repo-test/generate-repo-files.py
+++ build-scripts/docker/repo-test/generate-repo-files.py
@@ -167,7 +167,7 @@ def write_release_files (release):
         write_list_file('ubuntu', 'focal', release)
 
     if release in ['auth-46', 'auth-master',
-                   'rec-46', 'rec-master',
+                   'rec-45', 'rec-46', 'rec-master',
                    'dnsdist-16', 'dnsdist-17', 'dnsdist-master']:
         write_dockerfile('debian', 'bullseye', release)
         write_list_file('debian', 'bullseye', release)
