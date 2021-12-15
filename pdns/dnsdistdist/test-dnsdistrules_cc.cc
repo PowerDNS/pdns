1f4950f4791975f8e6a0dd0573f0ed544070288a
Remi Gacogne <remi.gacogne@powerdns.com>
2021-12-15 17:05:07 +0100

    dnsdist: Fix the mockup implementation of checkParameterBound in our unit tests

--- pdns/dnsdistdist/test-dnsdistrules_cc.cc
+++ pdns/dnsdistdist/test-dnsdistrules_cc.cc
@@ -10,7 +10,7 @@
 void checkParameterBound(const std::string& parameter, uint64_t value, size_t max);
 void checkParameterBound(const std::string& parameter, uint64_t value, size_t max)
 {
-  if (value > std::numeric_limits<uint16_t>::max()) {
+  if (value > max) {
     throw std::runtime_error("The value passed to " + parameter + " is too large, the maximum is " + std::to_string(max));
   }
 }
