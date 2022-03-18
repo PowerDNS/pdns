d852034880f3186fa84462b276fc687566787a6a
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2022-03-18 15:47:39 +0100

    include <unordered_map>

--- pdns/doh.hh
+++ pdns/doh.hh
@@ -20,6 +20,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #pragma once
+
+#include <unordered_map>
+
 #include "iputils.hh"
 #include "libssl.hh"
 #include "noinitvector.hh"
