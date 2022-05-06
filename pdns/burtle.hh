86c49d2bec75f8aac83c1d9f768e0a4ca1fce8d7
Otto Moerbeek <otto@drijf.net>
2022-05-06 19:54:08 +0200

    make burtlemix an (inline) function

--- pdns/burtle.hh
+++ pdns/burtle.hh
@@ -24,36 +24,36 @@
 
 #include <cinttypes>
 
-#define burtlemix(a, b, c) \
-  {                        \
-    a -= b;                \
-    a -= c;                \
-    a ^= (c >> 13);        \
-    b -= c;                \
-    b -= a;                \
-    b ^= (a << 8);         \
-    c -= a;                \
-    c -= b;                \
-    c ^= (b >> 13);        \
-    a -= b;                \
-    a -= c;                \
-    a ^= (c >> 12);        \
-    b -= c;                \
-    b -= a;                \
-    b ^= (a << 16);        \
-    c -= a;                \
-    c -= b;                \
-    c ^= (b >> 5);         \
-    a -= b;                \
-    a -= c;                \
-    a ^= (c >> 3);         \
-    b -= c;                \
-    b -= a;                \
-    b ^= (a << 10);        \
-    c -= a;                \
-    c -= b;                \
-    c ^= (b >> 15);        \
-  }
+inline void burtlemix(uint32_t& a, uint32_t& b, uint32_t& c)
+{
+  a -= b;
+  a -= c;
+  a ^= (c >> 13);
+  b -= c;
+  b -= a;
+  b ^= (a << 8);
+  c -= a;
+  c -= b;
+  c ^= (b >> 13);
+  a -= b;
+  a -= c;
+  a ^= (c >> 12);
+  b -= c;
+  b -= a;
+  b ^= (a << 16);
+  c -= a;
+  c -= b;
+  c ^= (b >> 5);
+  a -= b;
+  a -= c;
+  a ^= (c >> 3);
+  b -= c;
+  b -= a;
+  b ^= (a << 10);
+  c -= a;
+  c -= b;
+  c ^= (b >> 15);
+}
 
 inline uint32_t burtle(const unsigned char* k, uint32_t length, uint32_t initval)
 {
