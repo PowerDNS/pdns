0de0770e47811b98fa722720e98abcc64aa15c64
Asgeir Storesund Nilsen <asgeir@twingine.no>
2022-08-04 19:46:06 +0200

    Also reconnect on ENETUNREACH.
    Ref. #4155
    
    (cherry picked from commit 63f2d2700d5c011df2270beabe92a87168ef3c66)

--- pdns/dnsdist.cc
+++ pdns/dnsdist.cc
@@ -1015,14 +1015,14 @@ ssize_t udpClientSendRequestToBackend(const std::shared_ptr<DownstreamState>& ss
 
   if (result == -1) {
     int savederrno = errno;
-    vinfolog("Error sending request to backend %s: %d", ss->remote.toStringWithPort(), savederrno);
+    vinfolog("Error sending request to backend %s: %d", ss->remote.toStringWithPort(), strerror(savederrno));
 
     /* This might sound silly, but on Linux send() might fail with EINVAL
        if the interface the socket was bound to doesn't exist anymore.
        We don't want to reconnect the real socket if the healthcheck failed,
        because it's not using the same socket.
     */
-    if (!healthCheck && (savederrno == EINVAL || savederrno == ENODEV)) {
+    if (!healthCheck && (savederrno == EINVAL || savederrno == ENODEV || savederrno == ENETUNREACH)) {
       ss->reconnect();
     }
   }
