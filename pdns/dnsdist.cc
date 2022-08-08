1f739408aacbd47c98e06abc467291c37c35f150
Asgeir Storesund Nilsen <asgeir@twingine.no>
2022-08-08 11:01:42 +0200

    Use stringerror
    Co-authored-by: Remi Gacogne <github@coredump.fr>
    (cherry picked from commit cd0d1c226596ae9ab35a8f9f7992755139294f6a)

--- pdns/dnsdist.cc
+++ pdns/dnsdist.cc
@@ -1015,7 +1015,7 @@ ssize_t udpClientSendRequestToBackend(const std::shared_ptr<DownstreamState>& ss
 
   if (result == -1) {
     int savederrno = errno;
-    vinfolog("Error sending request to backend %s: %d", ss->remote.toStringWithPort(), strerror(savederrno));
+    vinfolog("Error sending request to backend %s: %d", ss->remote.toStringWithPort(), stringerror(savederrno));
 
     /* This might sound silly, but on Linux send() might fail with EINVAL
        if the interface the socket was bound to doesn't exist anymore.
