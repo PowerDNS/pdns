e09d3a8fcdf5077ee3449bd94d27d37b8ba218a1
Remi Gacogne <remi.gacogne@powerdns.com>
2022-10-19 11:58:33 +0200

    dnsdist: Also handle XFR responses with a lower serial than the query
    As suggested by Håkan Lindqvist (thanks!).

--- pdns/dnsdistdist/dnsdist-tcp-downstream.cc
+++ pdns/dnsdistdist/dnsdist-tcp-downstream.cc
@@ -803,7 +803,7 @@ bool TCPConnectionToBackend::isXFRFinished(const TCPResponse& response, TCPQuery
         if (query.d_xfrMasterSerial == 0) {
           // store the first SOA in our client's connection metadata
           query.d_xfrMasterSerial = serial;
-          if (query.d_xfrMasterSerial == query.d_xfrQuerySerial) {
+          if (query.d_xfrMasterSerial <= query.d_xfrQuerySerial) {
             /* This is the first message with a master SOA:
                RFC 1995 Section 2:
                  If an IXFR query with the same or newer version number
