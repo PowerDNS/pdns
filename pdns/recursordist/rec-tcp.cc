a61133e30418f8f6f7db1498731b6fc493b1138d
Otto Moerbeek <otto@drijf.net>
2022-12-12 20:03:04 +0100

    Fix compilation on FreeBSD. reported by HellSpawn
    (cherry picked from commit 398bb0f1e7122d6de0e6c5ec3383ba49a446d580)

--- pdns/recursordist/rec-tcp.cc
+++ pdns/recursordist/rec-tcp.cc
@@ -1067,7 +1067,7 @@ void makeTCPServerSockets(deferredAdd_t& deferredAdds, std::set<int>& tcpSockets
     if (g_reusePort) {
 #if defined(SO_REUSEPORT_LB)
       try {
-        SSetsockopt(fd, SOL_SOCKET, SO_REUSEPORT_LB, 1);
+        SSetsockopt(socketFd, SOL_SOCKET, SO_REUSEPORT_LB, 1);
       }
       catch (const std::exception& e) {
         throw PDNSException(std::string("SO_REUSEPORT_LB: ") + e.what());
