a0daac592818a097fc8e60f23db7009b14cc6a64
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-01-18 15:52:57 +0100

    Add a test to check that we can use the specified stack size
    Actual test of the guard pages can be done, but is hard to fit in the test framework.
    To test manually decrease the headroom const to 0

--- pdns/recursordist/test-mtasker.cc
+++ pdns/recursordist/test-mtasker.cc
@@ -6,6 +6,7 @@
 #endif
 #include <boost/test/unit_test.hpp>
 #include "mtasker.hh"
+#include <fcntl.h>
 
 BOOST_AUTO_TEST_SUITE(mtasker_cc)
 
@@ -36,6 +37,43 @@ BOOST_AUTO_TEST_CASE(test_Simple)
     }
     if (mt.noProcesses())
       break;
+  }
+  BOOST_CHECK_EQUAL(g_result, o);
+}
+
+static const size_t stackSize = 8 * 1024;
+static const size_t headroom = 256; // Decrease to hit stackoverflow
+
+static void doAlmostStackoverflow(void* arg)
+{
+  auto* mt = reinterpret_cast<MTasker<>*>(arg);
+  int localvar[stackSize / sizeof(int) - headroom]; // expermimentally derived headroom
+  localvar[0] = 0;
+  localvar[sizeof(localvar) / sizeof(localvar[0]) - 1] = 12;
+  if (mt->waitEvent(localvar[sizeof(localvar) / sizeof(localvar[0]) - 1], &localvar[0]) == 1) {
+    g_result = localvar[0];
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_AlmostStackOverflow)
+{
+  MTasker<> mt(stackSize);
+  mt.makeThread(doAlmostStackoverflow, &mt);
+  struct timeval now;
+  gettimeofday(&now, 0);
+  bool first = true;
+  int o = 25;
+  for (;;) {
+    while (mt.schedule(&now)) {
+      ;
+    }
+    if (first) {
+      mt.sendEvent(12, &o);
+      first = false;
+    }
+    if (mt.noProcesses()) {
+      break;
+    }
   }
   BOOST_CHECK_EQUAL(g_result, o);
 }
