2b186cf1a8c60b6a98436abd44afdc05e9c24fed
Sander Hoentjen <shoentjen@antagonist.nl>
2023-02-20 16:51:07 +0100

    dnsdist-protocols.hh: include <cstdint>
    This fixes building dnsdist with gcc13:
    ```
    In file included from dnsdist-protocols.cc:26:
    dnsdist-protocols.hh:32:8: error: use of enum 'typeenum' without previous declaration
       32 |   enum typeenum : uint8_t
          |        ^~~~~~~~
    dnsdist-protocols.hh:32:19: error: 'uint8_t' was not declared in this scope
       32 |   enum typeenum : uint8_t
          |                   ^~~~~~~
    dnsdist-protocols.hh:25:1: note: 'uint8_t' is defined in header '<cstdint>'; did you forget to '#include <cstdint>'?
       24 | #include <vector>
      +++ |+#include <cstdint>
       25 | #include <string>
    ```
    
    (cherry picked from commit f407319cb7374baf06698786f4f39d431a36a3cc)

--- pdns/dnsdist-protocols.cc
+++ pdns/dnsdist-protocols.cc
@@ -22,6 +22,7 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <vector>
 
 #include "dnsdist-protocols.hh"
 
--- pdns/dnsdist-protocols.hh
+++ pdns/dnsdist-protocols.hh
@@ -21,7 +21,7 @@
  */
 #pragma once
 
-#include <vector>
+#include <cstdint>
 #include <string>
 
 namespace dnsdist
