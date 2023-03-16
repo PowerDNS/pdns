cd279418d3b3151ab3b489e68bb5354138220e2f
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-03-16 08:28:31 +0100

    PowerDNS Security Advisory 2023-02: Deterred spoofing attempts can lead to authoritative servers being marked unavailable (CVE-2023-26437)

--- pdns/syncres.cc
+++ pdns/syncres.cc
@@ -5193,6 +5193,12 @@ bool SyncRes::doResolveAtThisIP(const std::string& prefix, const DNSName& qname,
   }
 
   d_totUsec += lwr.d_usec;
+
+  if (resolveret == LWResult::Result::Spoofed) {
+    spoofed = true;
+    return false;
+  }
+
   accountAuthLatency(lwr.d_usec, remoteIP.sin4.sin_family);
   ++g_stats.authRCode.at(lwr.d_rcode);
 
@@ -5223,9 +5229,6 @@ bool SyncRes::doResolveAtThisIP(const std::string& prefix, const DNSName& qname,
       /* OS resource limit reached */
       LOG(prefix<<qname<<": hit a local resource limit resolving"<< (doTCP ? " over TCP" : "")<<", probable error: "<<stringerror()<<endl);
       g_stats.resourceLimits++;
-    }
-    else if (resolveret == LWResult::Result::Spoofed) {
-      spoofed = true;
     }
     else {
       /* LWResult::Result::PermanentError */
