425d65fba2670ad3d7ff805b8a5175cb6fe3b9cd
Erik Winkels <erik.winkels@open-xchange.com>
2023-03-21 10:28:13 +0100

    Add auth-48 to repo test script.

--- build-scripts/docker/repo-test/generate-repo-files.py
+++ build-scripts/docker/repo-test/generate-repo-files.py
@@ -44,7 +44,7 @@ def init_argparser():
     parser.add_argument('release', metavar='RELEASE',
                         choices=[# Authoritative Server
                                  'auth-44', 'auth-45', 'auth-46', 'auth-47',
-                                 'auth-master',
+                                 'auth-48', 'auth-master',
                                  # Recursor
                                  'rec-45', 'rec-46', 'rec-47', 'rec-48',
                                  'rec-master',
@@ -140,7 +140,7 @@ def write_release_files (release):
     if g_verbose:
         print("Writing release files...")
 
-    if release in ['auth-44', 'auth-45', 'auth-46', 'auth-47',
+    if release in ['auth-44', 'auth-45', 'auth-46', 'auth-47', 'auth-48',
                    'auth-master',
                    'rec-45', 'rec-46', 'rec-47', 'rec-48',
                    'rec-master',
@@ -160,19 +160,19 @@ def write_release_files (release):
         write_dockerfile('raspbian', 'buster', release)
         write_list_file('raspbian', 'buster', release)
 
-    if release in ['auth-46', 'auth-47', 'auth-master',
+    if release in ['auth-46', 'auth-47', 'auth-48', 'auth-master',
                    'rec-45', 'rec-46', 'rec-47', 'rec-48', 'rec-master',
                    'dnsdist-16', 'dnsdist-17', 'dnsdist-18', 'dnsdist-master']:
         write_dockerfile('debian', 'bullseye', release)
         write_list_file('debian', 'bullseye', release)
 
-    if release in ['auth-46', 'auth-47', 'auth-master',
+    if release in ['auth-46', 'auth-47', 'auth-48', 'auth-master',
                    'rec-46', 'rec-47', 'rec-48', 'rec-master',
                    'dnsdist-17', 'dnsdist-18', 'dnsdist-master']:
         write_dockerfile('ubuntu', 'jammy', release)
         write_list_file('ubuntu', 'jammy', release)
 
-    if release in ['auth-47', 'auth-master',
+    if release in ['auth-47', 'auth-48', 'auth-master',
                    'rec-47', 'rec-48', 'rec-master',
                    'dnsdist-17', 'dnsdist-18', 'dnsdist-master']:
         write_dockerfile('el', '9', release)
