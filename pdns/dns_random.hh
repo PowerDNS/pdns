03544d6f85a77b72705cd552f839daacd86ef3f5
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-06-13 10:49:45 +0200

    Reformat

--- pdns/dns_random.hh
+++ pdns/dns_random.hh
@@ -28,50 +28,52 @@ void dns_random_init(const std::string& data = "", bool force_reinit = false);
 uint32_t dns_random(uint32_t n);
 uint16_t dns_random_uint16();
 
-namespace pdns {
-  struct dns_random_engine {
+namespace pdns
+{
+struct dns_random_engine
+{
 
-    typedef uint32_t result_type;
+  typedef uint32_t result_type;
 
-    static constexpr result_type min()
-    {
-      return 0;
-    }
-
-    static constexpr result_type max()
-    {
-      return std::numeric_limits<result_type>::max() - 1;
-    }
-
-    result_type operator()()
-    {
-      return dns_random(std::numeric_limits<result_type>::max());
-    }
-  };
-
-  /* minimum value that a PRNG should return for this upper bound to avoid a modulo bias */
-  inline unsigned int random_minimum_acceptable_value(uint32_t upper_bound)
+  static constexpr result_type min()
   {
-    /* Parts of this code come from arc4random_uniform */
-    /* To avoid "modulo bias" for some methods, calculate
-       minimum acceptable value for random number to improve
-       uniformity.
-
-       On applicable rngs, we loop until the rng spews out
-       value larger than min, and then take modulo out of that.
-    */
-    unsigned int min;
-#if (ULONG_MAX > 0xffffffffUL)
-    min = 0x100000000UL % upper_bound;
-#else
-    /* Calculate (2**32 % upper_bound) avoiding 64-bit math */
-    if (upper_bound > 0x80000000)
-      min = 1 + ~upper_bound; /* 2**32 - upper_bound */
-    else {
-      /* (2**32 - (x * 2)) % x == 2**32 % x when x <= 2**31 */
-      min = ((0xffffffff - (upper_bound * 2)) + 1) % upper_bound;
-    }
-#endif
-    return min;
+    return 0;
   }
+
+  static constexpr result_type max()
+  {
+    return std::numeric_limits<result_type>::max() - 1;
+  }
+
+  result_type operator()()
+  {
+    return dns_random(std::numeric_limits<result_type>::max());
+  }
+};
+
+/* minimum value that a PRNG should return for this upper bound to avoid a modulo bias */
+inline unsigned int random_minimum_acceptable_value(uint32_t upper_bound)
+{
+  /* Parts of this code come from arc4random_uniform */
+  /* To avoid "modulo bias" for some methods, calculate
+     minimum acceptable value for random number to improve
+     uniformity.
+
+     On applicable rngs, we loop until the rng spews out
+     value larger than min, and then take modulo out of that.
+  */
+  unsigned int min;
+#if (ULONG_MAX > 0xffffffffUL)
+  min = 0x100000000UL % upper_bound;
+#else
+  /* Calculate (2**32 % upper_bound) avoiding 64-bit math */
+  if (upper_bound > 0x80000000)
+    min = 1 + ~upper_bound; /* 2**32 - upper_bound */
+  else {
+    /* (2**32 - (x * 2)) % x == 2**32 % x when x <= 2**31 */
+    min = ((0xffffffff - (upper_bound * 2)) + 1) % upper_bound;
+  }
+#endif
+  return min;
+}
 }
