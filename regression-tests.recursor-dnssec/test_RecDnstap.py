b255fe8d5d42839037ca5d50be9e20541f035b6a
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-06-14 11:12:44 +0200

    Improve python style
    Co-authored-by: Chris Hofstaedtler <zeha@users.noreply.github.com>

--- regression-tests.recursor-dnssec/test_RecDnstap.py
+++ regression-tests.recursor-dnssec/test_RecDnstap.py
@@ -186,7 +186,7 @@ class TestRecursorDNSTap(RecursorTest):
                 fstrm_handle_bidir_connection(conn, lambda data: \
                 param.queue.put(data, True, timeout=2.0))
             except socket.error as e:
-                if e.errno == errno.EBADF or e.errno == errno.EPIPE:
+                if e.errno in (errno.EBADF, errno.EPIPE):
                     break
                 sys.stderr.write("Unexpected socket error %s\n" % str(e))
                 sys.exit(1)
