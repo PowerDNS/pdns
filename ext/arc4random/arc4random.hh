760a7c99771e71d359ffddd8e119528f7bb72145
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-06-20 12:52:41 +0200

    Format

--- ext/arc4random/arc4random.hh
+++ ext/arc4random/arc4random.hh
@@ -2,8 +2,9 @@
 
 #include <inttypes.h>
 
-extern "C" {
-uint32_t arc4random(void);
-void arc4random_buf(void *buf, size_t nbytes);
-uint32_t arc4random_uniform(uint32_t upper_bound);
+extern "C"
+{
+  uint32_t arc4random(void);
+  void arc4random_buf(void *buf, size_t nbytes);
+  uint32_t arc4random_uniform(uint32_t upper_bound);
 }
