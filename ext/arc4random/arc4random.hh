4644f23733337f9ca481acd5c7243ed1ac464227
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-06-20 13:46:50 +0200

    Fuzzing target needs arc4random as well

--- ext/arc4random/arc4random.hh
+++ ext/arc4random/arc4random.hh
@@ -5,6 +5,6 @@
 extern "C"
 {
   uint32_t arc4random(void);
-  void arc4random_buf(void *buf, size_t nbytes);
+  void arc4random_buf(void* buf, size_t nbytes);
   uint32_t arc4random_uniform(uint32_t upper_bound);
 }
