e03b2c5c9de78a026da6d7040b03e688ea8dc80b
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-07-07 12:02:14 +0200

    Fix build: put chacha_private.h into _SOURCES
    Also fix redundant declaration warnings
    
    Followup to #12999

--- ext/arc4random/arc4random.hh
+++ ext/arc4random/arc4random.hh
@@ -5,7 +5,13 @@
 
 extern "C"
 {
+#ifndef HAVE_ARC4RANDOM
   uint32_t arc4random(void);
+#endif
+#ifndef HAVE_ARC4RANDOM_BUF
   void arc4random_buf(void* buf, size_t nbytes);
+#endif
+#ifndef HAVE_ARC4RANDOM_UNIFORM
   uint32_t arc4random_uniform(uint32_t upper_bound);
+#endif
 }
--- ext/arc4random/includes.h
+++ ext/arc4random/includes.h
@@ -11,8 +11,14 @@
 
 #define seed_from_prngd(a, b) -1
 
+#ifndef HAVE_ARC4RANDOM
 uint32_t arc4random(void);
+#endif
+#ifndef HAVE_ARC4RANDOM_BUF
 void arc4random_buf(void *buf, size_t nbytes);
+#endif
+#ifndef HAVE_ARC4RANDOM_UNIFORM
 uint32_t arc4random_uniform(uint32_t upper_bound);
+#endif
 
 #define DEF_WEAK(x)
