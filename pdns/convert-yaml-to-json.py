893078c84cf2033058ac30497b2874242fe25aa9
Fred Morcos <fred.morcos@open-xchange.com>
2023-08-17 16:59:37 +0200

    Cleanup convert-yaml-to-json.py

--- pdns/convert-yaml-to-json.py
+++ pdns/convert-yaml-to-json.py
@@ -1,5 +1,14 @@
-import sys, json, yaml
+"""Convert a YAML file to JSON."""
 
-with open(sys.argv[1], mode='r', encoding='utf-8') as f_in:
-    with open(sys.argv[2], mode='w', encoding='utf-8') as f_out:
-        json.dump(yaml.safe_load(f_in.read()), f_out, indent=2, separators=(',', ': '))
+import json
+import sys
+
+import yaml
+
+yaml_filename = sys.argv[1]
+json_filename = sys.argv[2]
+
+with open(yaml_filename, mode="r", encoding="utf-8") as f_in:
+    with open(json_filename, mode="w", encoding="utf-8") as f_out:
+        contents = yaml.safe_load(f_in.read())
+        json.dump(contents, f_out, indent=2, separators=(",", ": "))
