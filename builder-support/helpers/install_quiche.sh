4af3e47d464915f4c558e06a6fe5cc44ba5844a7
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-09-29 15:16:54 +0200

    Install quiche lib in proper location for macOS, and teach pkgconfig about it

--- builder-support/helpers/install_quiche.sh
+++ builder-support/helpers/install_quiche.sh
@@ -7,6 +7,17 @@ readonly QUICHE_TARBALL="${QUICHE_VERSION}.tar.gz"
 readonly QUICHE_TARBALL_URL="https://github.com/cloudflare/quiche/archive/${QUICHE_TARBALL}"
 readonly QUICHE_TARBALL_HASH='eb242a14c4d801a90b57b6021dd29f7a62099f3a4d7a7ba889e105f8328e6c1f'
 
+INSTALL_PREFIX=/usr
+SOEXT=so
+if [ $(uname) = Darwin ]; then
+  if [ $(id -u) = 0 ]; then
+    echo Do not run as root on macOS
+    exit 1
+  fi
+  INSTALL_PREFIX="${HOMEBREW_PREFIX}"
+  SOEXT=dylib
+fi
+
 cd /tmp
 echo $0: Downloading $QUICHE_TARBALL
 curl -L -o "${QUICHE_TARBALL}" "${QUICHE_TARBALL_URL}"
@@ -15,15 +26,20 @@ echo "${QUICHE_TARBALL_HASH}"  "${QUICHE_TARBALL}" | sha256sum -c -
 tar xf "${QUICHE_TARBALL}"
 cd "quiche-${QUICHE_VERSION}"
 RUST_BACKTRACE=1 cargo build --release --no-default-features --features ffi,boringssl-boring-crate --package quiche
-install -Dm644 quiche/include/quiche.h /usr/include
-install -Dm644 target/release/libquiche.so /usr/lib/libdnsdist-quiche.so
-install -Dm644 /dev/stdin /usr/lib/pkgconfig/quiche.pc <<PC
+
+install -m644 quiche/include/quiche.h "${INSTALL_PREFIX}"/include
+install -m644 target/release/libquiche.${SOEXT} "${INSTALL_PREFIX}"/lib/libdnsdist-quiche.${SOEXT}
+if [ ! -d "${INSTALL_PREFIX}"/lib/pkgconfig/ ]; then
+    mkdir "${INSTALL_PREFIX}"/lib/pkgconfig/
+fi
+install -m644 /dev/stdin "${INSTALL_PREFIX}"/lib/pkgconfig/quiche.pc <<PC
 # quiche
 Name: quiche
 Description: quiche library
 URL: https://github.com/cloudflare/quiche
 Version: ${QUICHE_VERSION}
-Libs: -ldnsdist-quiche
+Cflags: -I${INSTALL_PREFIX}/include
+Libs: -L${INSTALL_PREFIX}/lib -ldnsdist-quiche
 PC
 
 cd ..
