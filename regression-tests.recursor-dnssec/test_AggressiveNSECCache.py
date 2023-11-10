b199e480fb21892c20f428dd9e51314718225e87
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2023-11-10 14:42:24 +0100

    Fix regression test that used iter 100

--- regression-tests.recursor-dnssec/test_AggressiveNSECCache.py
+++ regression-tests.recursor-dnssec/test_AggressiveNSECCache.py
@@ -16,6 +16,7 @@ class AggressiveNSECCacheBase(RecursorTest):
     _config_template = """
     dnssec=validate
     aggressive-nsec-cache-size=10000
+    nsec3-max-iterations=150
     webserver=yes
     webserver-port=%d
     webserver-address=127.0.0.1
