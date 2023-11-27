ca0b6be134c0b17296abf7f42730b4dcc1cb51ff
Fred Morcos <fred.morcos@open-xchange.com>
2023-11-27 14:20:51 +0100

    Format ssql and ssqlite3

--- pdns/backends/gsql/ssql.hh
+++ pdns/backends/gsql/ssql.hh
@@ -30,7 +30,8 @@
 class SSqlException
 {
 public:
-  SSqlException(const string &reason) : d_reason(reason)
+  SSqlException(const string& reason) :
+    d_reason(reason)
   {
   }
 
@@ -38,6 +39,7 @@ public:
   {
     return d_reason;
   }
+
 private:
   string d_reason;
 };
@@ -48,44 +50,47 @@ public:
   typedef vector<string> row_t;
   typedef vector<row_t> result_t;
 
-  virtual SSqlStatement* bind(const string& name, bool value)=0;
-  virtual SSqlStatement* bind(const string& name, int value)=0;
-  virtual SSqlStatement* bind(const string& name, uint32_t value)=0;
-  virtual SSqlStatement* bind(const string& name, long value)=0;
-  virtual SSqlStatement* bind(const string& name, unsigned long value)=0;
-  virtual SSqlStatement* bind(const string& name, long long value)=0;;
-  virtual SSqlStatement* bind(const string& name, unsigned long long value)=0;
-  virtual SSqlStatement* bind(const string& name, const std::string& value)=0;
-  SSqlStatement* bind(const string& name, const DNSName& value) {
+  virtual SSqlStatement* bind(const string& name, bool value) = 0;
+  virtual SSqlStatement* bind(const string& name, int value) = 0;
+  virtual SSqlStatement* bind(const string& name, uint32_t value) = 0;
+  virtual SSqlStatement* bind(const string& name, long value) = 0;
+  virtual SSqlStatement* bind(const string& name, unsigned long value) = 0;
+  virtual SSqlStatement* bind(const string& name, long long value) = 0;
+  ;
+  virtual SSqlStatement* bind(const string& name, unsigned long long value) = 0;
+  virtual SSqlStatement* bind(const string& name, const std::string& value) = 0;
+  SSqlStatement* bind(const string& name, const DNSName& value)
+  {
     if (!value.empty()) {
       return bind(name, value.makeLowerCase().toStringRootDot());
     }
     return bind(name, string(""));
   }
-  virtual SSqlStatement* bindNull(const string& name)=0;
-  virtual SSqlStatement* execute()=0;;
-  virtual bool hasNextRow()=0;
-  virtual SSqlStatement* nextRow(row_t& row)=0;
-  virtual SSqlStatement* getResult(result_t& result)=0;
-  virtual SSqlStatement* reset()=0;
-  virtual const std::string& getQuery()=0;
+  virtual SSqlStatement* bindNull(const string& name) = 0;
+  virtual SSqlStatement* execute() = 0;
+  ;
+  virtual bool hasNextRow() = 0;
+  virtual SSqlStatement* nextRow(row_t& row) = 0;
+  virtual SSqlStatement* getResult(result_t& result) = 0;
+  virtual SSqlStatement* reset() = 0;
+  virtual const std::string& getQuery() = 0;
   virtual ~SSqlStatement();
 };
 
 class SSql
 {
 public:
-  virtual SSqlException sPerrorException(const string &reason)=0;
-  virtual std::unique_ptr<SSqlStatement> prepare(const string& query, int nparams)=0;
-  virtual void execute(const string& query)=0;
-  virtual void startTransaction()=0;
-  virtual void rollback()=0;
-  virtual void commit()=0;
-  virtual void setLog(bool /* state */){}
+  virtual SSqlException sPerrorException(const string& reason) = 0;
+  virtual std::unique_ptr<SSqlStatement> prepare(const string& query, int nparams) = 0;
+  virtual void execute(const string& query) = 0;
+  virtual void startTransaction() = 0;
+  virtual void rollback() = 0;
+  virtual void commit() = 0;
+  virtual void setLog(bool /* state */) {}
   virtual bool isConnectionUsable()
   {
     return true;
   }
-  virtual void reconnect() {};
+  virtual void reconnect(){};
   virtual ~SSql(){};
 };
