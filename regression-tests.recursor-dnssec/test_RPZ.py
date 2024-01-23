71ada8e045af192eeaf41e85353d78fcf46fa17f
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2024-01-23 10:22:11 +0100

    Make the refresh 1 again, to work around the new rate limiting

--- regression-tests.recursor-dnssec/test_RPZ.py
+++ regression-tests.recursor-dnssec/test_RPZ.py
@@ -385,7 +385,7 @@ class RPZXFRRecursorTest(RPZRecursorTest):
     global rpzServerPort
     _lua_config_file = """
     -- The first server is a bogus one, to test that we correctly fail over to the second one
-    rpzMaster({'127.0.0.1:9999', '127.0.0.1:%d'}, 'zone.rpz.', { refresh=3600, includeSOA=true})
+    rpzMaster({'127.0.0.1:9999', '127.0.0.1:%d'}, 'zone.rpz.', { refresh=1, includeSOA=true})
     """ % (rpzServerPort)
     _confdir = 'RPZXFR'
     _wsPort = 8042
