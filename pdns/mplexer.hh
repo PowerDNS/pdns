2759618132061d8cc543c1f25ff29b5db1936392
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2024-05-21 11:50:04 +0200

    Reformt

--- pdns/mplexer.hh
+++ pdns/mplexer.hh
@@ -81,7 +81,8 @@ public:
      minimum value of maxEventsHint and s_maxevents, to reduce memory usage. */
   static FDMultiplexer* getMultiplexerSilent(unsigned int maxEventsHint = s_maxevents);
 
-  struct InRun {
+  struct InRun
+  {
     InRun(const InRun&) = delete;
     InRun(InRun&&) = delete;
     InRun& operator=(const InRun&) = delete;
@@ -94,7 +95,8 @@ public:
       }
       d_inrun = true;
     }
-    ~InRun() {
+    ~InRun()
+    {
       d_inrun = false;
     }
     bool& d_inrun;
