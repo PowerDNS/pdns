379fc5b05c535f02baadc623a338efdbec0d300c
Stefan Schmidt <203977+ZaphodB@users.noreply.github.com>
2024-05-31 12:33:19 +0200

    rec: allow_no_rd - clarify older dig +trace topples over in case of NODATA

--- pdns/recursordist/settings/table.py
+++ pdns/recursordist/settings/table.py
@@ -205,8 +205,8 @@ Allow ``no recursion desired (RD=0) queries`` to query cache contents.
 If not set (the default), these queries are answered with rcode ``Refused``.
 
 Note that there is a short while after restarting that the cache has no authoritative data on the root,
-so it will answer with an NODATA (NOERROR and no answer records) in that period for RD=0 queries
-affecting for example older versions of dig when using its +trace option.
+so it will answer with an NODATA (NOERROR and no answer records) in that period for RD=0 queries.
+This affects for example older versions of dig when using its +trace option causing it to fail.
  ''',
     'versionadded': '5.0.0'
     },
