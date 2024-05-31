a6dc03fb1f649d72879f6209cea354e485a882d6
Stefan Schmidt <203977+ZaphodB@users.noreply.github.com>
2024-05-31 12:34:54 +0200

    rec: allow_no_rd - document cache contents on root on startup pertaining to dig +trace

--- pdns/recursordist/settings/table.py
+++ pdns/recursordist/settings/table.py
@@ -203,6 +203,10 @@ Like :ref:`setting-allow-notify-from`, except reading a sequence of `Subnet`_ fr
         'doc' : '''
 Allow ``no recursion desired (RD=0) queries`` to query cache contents.
 If not set (the default), these queries are answered with rcode ``Refused``.
+
+Note that there is a short while after restarting that the cache has no authoritative data on the root,
+so it will answer with an NODATA (NOERROR and no answer records) in that period for RD=0 queries
+affecting for example older versions of dig when using its +trace option.
  ''',
     'versionadded': '5.0.0'
     },
