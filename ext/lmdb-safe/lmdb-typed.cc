d582a29939b4d889c6f784ed3467a3cbdddb4605
Fred Morcos <fred.morcos@open-xchange.com>
2024-07-23 14:39:57 +0200

    Format lmdb-typed.cc

--- ext/lmdb-safe/lmdb-typed.cc
+++ ext/lmdb-safe/lmdb-typed.cc
@@ -6,7 +6,7 @@ unsigned int MDBGetMaxID(MDBRWTransaction& txn, MDBDbi& dbi)
   auto cursor = txn->getRWCursor(dbi);
   MDBOutVal maxidval, maxcontent;
   unsigned int maxid{0};
-  if(!cursor.get(maxidval, maxcontent, MDB_LAST)) {
+  if (!cursor.get(maxidval, maxcontent, MDB_LAST)) {
     maxid = maxidval.getNoStripHeader<unsigned int>();
   }
   return maxid;
@@ -16,13 +16,13 @@ unsigned int MDBGetRandomID(MDBRWTransaction& txn, MDBDbi& dbi)
 {
   auto cursor = txn->getRWCursor(dbi);
   unsigned int id;
-  for(int attempts=0; attempts<20; attempts++) {
+  for (int attempts = 0; attempts < 20; attempts++) {
     MDBOutVal key, content;
 
     // dns_random generates a random number in [0..signed_int_max-1]. We add 1 to avoid 0 and allow type_max.
     // 0 is avoided because the put() interface uses it to mean "please allocate a number for me"
     id = dns_random(std::numeric_limits<signed int>::max()) + 1;
-    if(cursor.find(MDBInVal(id), key, content)) {
+    if (cursor.find(MDBInVal(id), key, content)) {
       return id;
     }
   }
