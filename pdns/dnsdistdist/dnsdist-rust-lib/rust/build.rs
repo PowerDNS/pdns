e41cb6e0a624035aac84a9d04bd82c9e39dd0b44
Remi Gacogne <remi.gacogne@powerdns.com>
2024-08-26 17:49:27 +0200

    dnsdist: Properly re-generate C++ bindings when the Rust lib is modified

--- pdns/dnsdistdist/dnsdist-rust-lib/rust/build.rs
+++ pdns/dnsdistdist/dnsdist-rust-lib/rust/build.rs
@@ -3,4 +3,7 @@ fn main() {
         .flag_if_supported("-std=c++17")
         .flag("-Isrc")
         .compile("dnsdist_rust");
+
+    println!("cargo:rerun-if-changed=src/lib.rs");
+    println!("cargo:rerun-if-changed=src/helpers.rs");
 }
