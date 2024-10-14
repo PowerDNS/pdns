1294b2a0d1b6dc715492fb970d43aa6552c59bab
Remi Gacogne <remi.gacogne@powerdns.com>
2024-10-14 11:29:27 +0200

    dnsdist: Remove duplicated meson directories and files

--- pdns/dnsdistdist/meson/atomics/atomic_add_fetch.cc
+++
@@ -1,8 +0,0 @@
-#include <cstdint>
-
-int main()
-{
-  uint64_t val = 0;
-  __atomic_add_fetch(&val, 1, __ATOMIC_RELAXED);
-  return 0;
-}
--- pdns/dnsdistdist/meson/sanitizers/address-sanitizer-fiber-switching/sanitizer_finish_switch_fiber_3ptrs.cc
+++
@@ -1,7 +0,0 @@
-#include <sanitizer/common_interface_defs.h>
-
-int main()
-{
-  __sanitizer_finish_switch_fiber(nullptr, nullptr, nullptr);
-  return 0;
-}
