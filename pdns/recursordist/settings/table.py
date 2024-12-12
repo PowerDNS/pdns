7b8207b1c96b85579bb6d9e69e499be9c518ddae
zhaojs <604381586@qq.com>
2024-12-12 19:04:16 +0800

    sort udp_source_port_avoid  list

--- pdns/recursordist/settings/table.py
+++ pdns/recursordist/settings/table.py
@@ -2981,11 +2981,11 @@ See :ref:`setting-udp-source-port-min`.
         'name' : 'udp_source_port_avoid',
         'section' : 'outgoing',
         'type' : LType.ListStrings,
-        'default' : '11211,4791',
-        'help' : 'List of comma separated UDP port number to avoid',
+        'default' : '4791,11211',
+        'help' : 'List of comma separated UDP port numbers to avoid',
         'doc' : '''
 A list of comma-separated UDP port numbers to avoid when binding.
-Ex: `5300,11211,4791`
+Ex: `4791,5300,11211`
 
 See :ref:`setting-udp-source-port-min`.
  ''',
@@ -2996,9 +2996,9 @@ A sequence of UDP port numbers to avoid when binding. For example:
 
  outgoing:
    udp_source_port_avoid:
+   - 4791
    - 5300
    - 11211
-   - 4791
 
 See :ref:`setting-udp-source-port-min`.
  ''',
