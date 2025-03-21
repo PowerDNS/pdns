a65394d6baa79798ccab66987a8f57a9ddd0af82
Otto Moerbeek <otto@drijf.net>
2025-03-21 12:45:11 +0100

    Better tracing

--- pdns/recursordist/lwres.hh
+++ pdns/recursordist/lwres.hh
@@ -43,8 +43,8 @@
 #include "fstrm_logger.hh"
 #include "resolve-context.hh"
 #include "noinitvector.hh"
-
-#include "logging.hh"
+#include "logger.hh"
+#include "logr.hh"
 
 // Helper to be defined by main program: queue data and log based on return value of queueData()
 void remoteLoggerQueueData(RemoteLoggerInterface&, const std::string&);
@@ -98,7 +98,7 @@ LWResult::Result asendto(const void* data, size_t len, int flags, const ComboAdd
 LWResult::Result arecvfrom(PacketBuffer& packet, int flags, const ComboAddress& fromAddr, size_t& len, uint16_t qid,
                            const DNSName& domain, uint16_t qtype, int fileDesc, const std::optional<EDNSSubnetOpts>& ecs, const struct timeval& now);
 
-LWResult::Result asyncresolve(const ComboAddress& address, const DNSName& domain, int type, bool doTCP, bool sendRDQuery, int EDNS0Level, struct timeval* now, boost::optional<Netmask>& srcmask, const ResolveContext& context, const std::shared_ptr<std::vector<std::unique_ptr<RemoteLogger>>>& outgoingLoggers, const std::shared_ptr<std::vector<std::unique_ptr<FrameStreamLogger>>>& fstrmLoggers, const std::set<uint16_t>& exportTypes, LWResult* lwr, bool* chained);
+LWResult::Result asyncresolve(const OptLog& log, const ComboAddress& address, const DNSName& domain, int type, bool doTCP, bool sendRDQuery, int EDNS0Level, struct timeval* now, boost::optional<Netmask>& srcmask, const ResolveContext& context, const std::shared_ptr<std::vector<std::unique_ptr<RemoteLogger>>>& outgoingLoggers, const std::shared_ptr<std::vector<std::unique_ptr<FrameStreamLogger>>>& fstrmLoggers, const std::set<uint16_t>& exportTypes, LWResult* lwr, bool* chained);
 uint64_t dumpCookies(int fileDesc);
 std::string clearCookies();
 void pruneCookies(time_t cutoff);
