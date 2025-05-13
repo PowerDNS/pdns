PowerDNS/pdns

commit 99d1e8f044d43b9a75ffc70fed22ea674a9b7859

dnsdist: Use a power of two values for the number of shards, as suggested by Otto

pdns/dnsdistdist/dnsdist-concurrent-connections.cc
@@ -37,7 +37,7 @@
 namespace dnsdist
 {
 
-static constexpr size_t NB_SHARDS = 10;
+static constexpr size_t NB_SHARDS = 16;
 
 struct ClientActivity
 {

commit a2ac5762a270d05bb22b1fe1b20669a3610a1e59

dnsdist: Fix a crash in the TCP concurrent connections map
The bug was introduced in 99d1e8f044d43b9a75ffc70fed22ea674a9b7859.
Thanks to Robert Edmonds for finding, reporting and proposing a patch
fixing the issue!

pdns/dnsdistdist/dnsdist-concurrent-connections.cc
@@ -71,7 +71,7 @@ using map_t = boost::multi_index_container<
     boost::multi_index::ordered_non_unique<boost::multi_index::tag<TimeTag>,
                                            boost::multi_index::member<ClientEntry, time_t, &ClientEntry::d_lastSeen>>>>;
 
-static std::vector<LockGuarded<map_t>> s_tcpClientsConnectionMetrics{10};
+static std::vector<LockGuarded<map_t>> s_tcpClientsConnectionMetrics{NB_SHARDS};
 
 static AddressAndPortRange getRange(const ComboAddress& from)
 {