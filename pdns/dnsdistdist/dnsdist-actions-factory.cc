61f1b7cb77b225a2f8ce4a39be77003f61e88c20
Samir Aguiar <sjorgedeaguiar@netskope.com>
2025-05-27 15:43:43 +0000

    dnsdist: add SetEDNSOptionResponseAction

--- pdns/dnsdistdist/dnsdist-actions-factory.cc
+++ pdns/dnsdistdist/dnsdist-actions-factory.cc
@@ -1032,6 +1032,31 @@ private:
   std::string d_data;
 };
 
+class SetEDNSOptionResponseAction : public DNSResponseAction
+{
+public:
+  // this action does not stop the processing
+  SetEDNSOptionResponseAction(uint16_t code, std::string data) :
+    d_code(code), d_data(std::move(data))
+  {
+  }
+
+  DNSResponseAction::Action operator()(DNSResponse* response, std::string* ruleresult) const override
+  {
+    setEDNSOption(*response, d_code, d_data);
+    return Action::None;
+  }
+
+  [[nodiscard]] std::string toString() const override
+  {
+    return "add EDNS Option to response (code=" + std::to_string(d_code) + ")";
+  }
+
+private:
+  uint16_t d_code;
+  std::string d_data;
+};
+
 class SetNoRecurseAction : public DNSAction
 {
 public:
