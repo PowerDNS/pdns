278aaa4f35caf8f80db2cd1c352a1e57d95314a7
Remi Gacogne <remi.gacogne@powerdns.com>
2025-07-25 16:19:43 +0200

    dnsdist: Backend QPS limit refactoring
    Use `std::optional` to signal that the QPS limit is not set,
    instead of a relying of a special case version of the `QPSLimiter`
    that does pretty much nothing.
    
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>

--- pdns/dnsdistdist/dnsdist-web.cc
+++ pdns/dnsdistdist/dnsdist-web.cc
@@ -1092,7 +1092,7 @@ static void addServerToJSON(Json::array& servers, int identifier, const std::sha
     {"state", status},
     {"protocol", backend->getProtocol().toPrettyString()},
     {"qps", (double)backend->queryLoad},
-    {"qpsLimit", (double)backend->qps.getRate()},
+    {"qpsLimit", static_cast<double>(backend->getQPSLimit())},
     {"outstanding", (double)backend->outstanding},
     {"reuseds", (double)backend->reuseds},
     {"weight", (double)backend->d_config.d_weight},
