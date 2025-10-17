44ba2aa25ab791e22eb9dd95b4b0035481265840
Remi Gacogne <remi.gacogne@powerdns.com>
2025-10-17 16:35:34 +0200

    dnsdist: Fix query rules bypass after tagging from a dynblock
    In 2.0.0 we introduced the ability to set a tag when a dynamic
    block matches, making it possible to combine dynamic blocks with
    existing rules. Unfortunately the implementation turned out to
    bypass query rules after setting a tag, so the mechanism could
    only be used with the remaining rules chains (cache hit, cache-miss,
    cache inserted, self-answered and regular response rules).
    This commit fixes that to ensure that we can use tags with query
    rules as well.
    
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>
    (cherry picked from commit b2afaadbd8e3403a68a93fa82b7a25be9f89e385)

--- pdns/dnsdistdist/dnsdist.cc
+++ pdns/dnsdistdist/dnsdist.cc
@@ -1119,7 +1119,8 @@ static bool applyRulesToQuery(DNSQuestion& dnsQuestion, const timespec& now)
         const auto& tagValue = got->second.tagSettings->d_value;
         dnsQuestion.setTag(tagName, tagValue);
         vinfolog("Query from %s setting tag %s to %s because of dynamic block", dnsQuestion.ids.origRemote.toStringWithPort(), tagName, tagValue);
-        return true;
+        // do not return, the whole point it to set a Tag to be able to do further processing in rules
+        break;
       }
       default:
         updateBlockStats();
@@ -1193,7 +1194,8 @@ static bool applyRulesToQuery(DNSQuestion& dnsQuestion, const timespec& now)
         const auto& tagValue = got->tagSettings->d_value;
         dnsQuestion.setTag(tagName, tagValue);
         vinfolog("Query from %s setting tag %s to %s because of dynamic block", dnsQuestion.ids.origRemote.toStringWithPort(), tagName, tagValue);
-        return true;
+        // do not return, the whole point it to set a Tag to be able to do further processing in rules
+        break;
       }
       default:
         updateBlockStats();
