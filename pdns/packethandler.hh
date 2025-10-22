3906f4f2523e6701d0300f510324b8f01200aa30
Miod Vallat <miod.vallat@powerdns.com>
2025-10-22 13:59:20 +0200

    Stash more variables into the update context struct.
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>

--- pdns/packethandler.hh
+++ pdns/packethandler.hh
@@ -91,7 +91,6 @@ private:
   void emitNSEC(std::unique_ptr<DNSPacket>& r, const DNSName& name, const DNSName& next, int mode);
   void emitNSEC3(DNSPacket& p, std::unique_ptr<DNSPacket>& r, const NSEC3PARAMRecordContent &ns3prc, const DNSName& name, const string& namehash, const string& nexthash, int mode);
   int processUpdate(DNSPacket& p);
-  int forwardPacket(const string &msgPrefix, const DNSPacket& p, const DomainInfo& di);
 
   void makeNXDomain(DNSPacket& p, std::unique_ptr<DNSPacket>& r, const DNSName& target, const DNSName& wildcard);
   void makeNOError(DNSPacket& p, std::unique_ptr<DNSPacket>& r, const DNSName& target, const DNSName& wildcard, int mode);
