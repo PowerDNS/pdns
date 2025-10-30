1228c2e9dcb00b5844da88f88f972a370ff2a3d4
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2025-10-30 12:38:16 +0100

    Better words in comment
    Signed-off-by: Otto Moerbeek <otto.moerbeek@open-xchange.com>
    (cherry picked from commit 9dd5af8c6a44c76a163bc63434925659d47e59ef)

--- builder-support/helpers/update-rust-library-version.py
+++ builder-support/helpers/update-rust-library-version.py
@@ -15,7 +15,7 @@ def main():
     package_name = sys.argv[2]
     version = sys.argv[3]
 
-    # convert the serial so that it conforms to Rust rules: x.x.x-whatever
+    # convert the version so that it conforms to Rust rules: x.x.x-whatever
     version = re.sub(r'([0-9]+\.[0-9]+\.[0-9]+)\.', r'\1-', version)
     with tempfile.NamedTemporaryFile(mode='w+t', encoding='utf-8', delete=False) as generated_fp:
         with open(file_name, 'r', encoding='utf-8') as cargo_file:
