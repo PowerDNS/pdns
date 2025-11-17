964bce840a2672c975f735acc65b52dced339edb
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-17 08:19:33 -0500

    Mark `staticmethod` KeepOpenTCPTest.sendTCPQueryKeepOpen
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>
    Signed-off-by: Pieter Lexis <pieter.lexis@powerdns.com>

--- regression-tests.recursor-dnssec/test_KeepOpenTCP.py
+++ regression-tests.recursor-dnssec/test_KeepOpenTCP.py
@@ -30,7 +30,8 @@ auth-zones=authzone.example=configs/%s/authzone.zone"""
             )
         super(KeepOpenTCPTest, cls).generateRecursorConfig(confdir)
 
-    def sendTCPQueryKeepOpen(cls, sock, query, timeout=2.0):
+    @staticmethod
+    def sendTCPQueryKeepOpen(sock, query, timeout=2.0):
         try:
             wire = query.to_wire()
             sock.send(struct.pack("!H", len(wire)))
