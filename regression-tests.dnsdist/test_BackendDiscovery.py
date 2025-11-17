350b35a8cf7e0a4c2593672a17295240b43d648b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-17 08:27:06 -0500

    First argument should be `self` TestBackendDiscovery.UpgradeDoTDifferentAddr1Callback
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- regression-tests.dnsdist/test_BackendDiscovery.py
+++ regression-tests.dnsdist/test_BackendDiscovery.py
@@ -154,9 +154,9 @@ class TestBackendDiscovery(DNSDistTest):
         response.answer.append(rrset)
         return response.to_wire()
 
-    def UpgradeDoTDifferentAddr1Callback(request):
-        response = dns.message.make_response(request)
-        rrset = dns.rrset.from_text(request.question[0].name,
+    def UpgradeDoTDifferentAddr1Callback(self):
+        response = dns.message.make_response(self)
+        rrset = dns.rrset.from_text(self.question[0].name,
                                     60,
                                     dns.rdataclass.IN,
                                     dns.rdatatype.SVCB,
