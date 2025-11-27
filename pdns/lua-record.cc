500ea3e92ac34fc3a1113ec0b52b0d93eab25538
Miod Vallat <miod.vallat@powerdns.com>
2025-11-27 13:01:35 +0100

    Add missing catch block for STL exceptions in createForward.
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>
    (cherry picked from commit f3d9786ec28a797bb479a7834777f19b55785183)

--- pdns/lua-record.cc
+++ pdns/lua-record.cc
@@ -1093,7 +1093,9 @@ static string lua_createForward()
       }
     }
     return allZerosIP;
-  } catch (const PDNSException &e) {
+  } catch (const PDNSException &) {
+    return allZerosIP;
+  } catch (const std::exception &) { // thrown by std::stol
     return allZerosIP;
   }
 }
