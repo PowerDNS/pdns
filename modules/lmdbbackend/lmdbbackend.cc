a532c0f5ca88f8904525325d8c3d0b9b34bb0e2c
Miod Vallat <miod.vallat@powerdns.com>
2026-02-12 16:41:36 +0100

    Fix polarity of setting description.
    This was forgotten after this setting changed name and polarity.
    
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>
    (cherry picked from commit de2ce3557f88a638c67b4d6d52c0605d1a9e55d8)

--- modules/lmdbbackend/lmdbbackend.cc
+++ modules/lmdbbackend/lmdbbackend.cc
@@ -2882,7 +2882,7 @@ public:
     declare(suffix, "random-ids", "Numeric IDs inside the database are generated randomly instead of sequentially", "no");
     declare(suffix, "map-size", "LMDB map size in megabytes", (sizeof(void*) == 4) ? "100" : "16000");
     declare(suffix, "flag-deleted", "Flag entries on deletion instead of deleting them", "no");
-    declare(suffix, "write-notification-update", "Do not update domain table upon notification", "yes");
+    declare(suffix, "write-notification-update", "Update domain table upon notification", "yes");
     declare(suffix, "lightning-stream", "Run in Lightning Stream compatible mode", "no");
   }
   DNSBackend* make(const string& suffix = "") override
