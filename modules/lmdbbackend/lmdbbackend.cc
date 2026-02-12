d884579a5ad331bccbf73049db9aa63dce7d1d21
Miod Vallat <miod.vallat@powerdns.com>
2026-02-12 16:42:21 +0100

    Perform DomainInfo consolidation before filtering.
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>

--- modules/lmdbbackend/lmdbbackend.cc
+++ modules/lmdbbackend/lmdbbackend.cc
@@ -2284,8 +2284,8 @@ void LMDBBackend::getAllDomainsFiltered(vector<DomainInfo>* domains, const std::
     }
 
     for (auto& [k, v] : zonemap) {
+      consolidateDomainInfo(v);
       if (allow(v)) {
-        consolidateDomainInfo(v);
         domains->push_back(std::move(v));
       }
     }
@@ -2296,8 +2296,8 @@ void LMDBBackend::getAllDomainsFiltered(vector<DomainInfo>* domains, const std::
       di.id = iter.getID();
       di.backend = this;
 
+      consolidateDomainInfo(di);
       if (allow(di)) {
-        consolidateDomainInfo(di);
         domains->push_back(di);
       }
     }
