06d3e98b4f0da79988fd00ae02809a8b0e2fdb03
Remi Gacogne <remi.gacogne@powerdns.com>
2026-02-19 12:02:43 +0100

    dnsdist: Use a smart pointer for DoH3 events
    I don't see any issue right away but let's make sure we don't leak.
    
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>

--- pdns/dnsdistdist/doh3.cc
+++ pdns/dnsdistdist/doh3.cc
@@ -891,11 +891,13 @@ static void processH3Events(ClientState& clientState, DOH3Frontend& frontend, H3
     if (streamID < 0) {
       break;
     }
+    std::unique_ptr<quiche_h3_event, decltype(&quiche_h3_event_free)> eventPtr(event, quiche_h3_event_free);
+    event = nullptr;
     conn.d_headersBuffers.try_emplace(streamID, dnsdist::doh3::h3_headers_t{});
 
-    switch (quiche_h3_event_type(event)) {
+    switch (quiche_h3_event_type(eventPtr.get())) {
     case QUICHE_H3_EVENT_HEADERS: {
-      processH3HeaderEvent(clientState, frontend, conn, client, serverConnID, streamID, event);
+      processH3HeaderEvent(clientState, frontend, conn, client, serverConnID, streamID, eventPtr.get());
       break;
     }
     case QUICHE_H3_EVENT_DATA: {
@@ -908,8 +910,6 @@ static void processH3Events(ClientState& clientState, DOH3Frontend& frontend, H3
     case QUICHE_H3_EVENT_GOAWAY:
       break;
     }
-
-    quiche_h3_event_free(event);
   }
 }
 
