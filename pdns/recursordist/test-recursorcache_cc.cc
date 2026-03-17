03f791f20064d8ea3c6fa5c192aa93894108105a
Remi Gacogne <remi.gacogne@powerdns.com>
2026-03-17 12:32:27 +0100

    rec: Actually test the deserialized cache content in the unit test
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>
    (cherry picked from commit 3bec58f4ee608d6f8dc078a74e901875cd7c4827)

--- pdns/recursordist/test-recursorcache_cc.cc
+++ pdns/recursordist/test-recursorcache_cc.cc
@@ -1300,7 +1300,7 @@ BOOST_AUTO_TEST_CASE(test_RecursorCacheDumpAndRestore)
   dr.d_name = DNSName("hi");
   dr.d_type = QType::AAAA;
   dr.d_ttl = 3600;
-  dr.setContent(std::make_shared<ARecordContent>(ComboAddress("1::2:3:4")));
+  dr.setContent(std::make_shared<AAAARecordContent>(ComboAddress("1::2:3:4")));
   authRecords.emplace_back(std::make_shared<DNSRecord>(dr));
 
   std::vector<std::shared_ptr<const RRSIGRecordContent>> signatures;
@@ -1325,18 +1325,18 @@ BOOST_AUTO_TEST_CASE(test_RecursorCacheDumpAndRestore)
   const ComboAddress somebody("::1");
   const time_t ttl_time = 90;
 
+  const size_t expected = 100;
+
+  for (size_t counter = 0; counter < expected; ++counter) {
+    DNSName a = DNSName("hello ") + DNSName(std::to_string(counter));
+    BOOST_CHECK_EQUAL(DNSName(a.toString()), a);
+
+    MRC.replace(now, a, QType(QType::A), rset0, signatures, authRecords, true, authZone, boost::none, boost::none, vState::Insecure, somebody, false, ttl_time);
+  }
+
+  BOOST_CHECK_EQUAL(MRC.size(), expected);
+
   auto checker = [&] {
-    const size_t expected = 100;
-
-    for (size_t counter = 0; counter < expected; ++counter) {
-      DNSName a = DNSName("hello ") + DNSName(std::to_string(counter));
-      BOOST_CHECK_EQUAL(DNSName(a.toString()), a);
-
-      MRC.replace(now, a, QType(QType::A), rset0, signatures, authRecords, true, authZone, boost::none, boost::none, vState::Insecure, somebody, false, ttl_time);
-    }
-
-    BOOST_CHECK_EQUAL(MRC.size(), expected);
-
     size_t matches = 0;
 
     for (size_t counter = 0; counter < expected + 10; counter++) {
