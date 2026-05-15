1d15a024b72073f7bb3110bfc7d80a175e09b56a
Remi Gacogne <remi.gacogne@powerdns.com>
2026-05-15 16:51:11 +0200

    dnsdist: Keep concurrent connection entries for live connections
    Otherwise clients that manage to keep their connections around for
    a long time can bypass the limit.
    
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>
    (cherry picked from commit 74afa35cedb1f1698d638df8d227fd50b912f783)

--- pdns/dnsdistdist/dnsdist-concurrent-connections.cc
+++ pdns/dnsdistdist/dnsdist-concurrent-connections.cc
@@ -163,6 +163,11 @@ void IncomingConcurrentTCPConnectionsManager::cleanup(time_t now)
     auto clients = shard.lock();
     auto& index = clients->get<TimeTag>();
     for (auto entry = index.begin(); entry != index.end();) {
+      if (entry->d_concurrentConnections > 0) {
+        /* we need to keep this around as we still have open connections */
+        ++entry;
+        continue;
+      }
       if (entry->d_lastSeen >= cutOff) {
         /* this index is ordered on timestamps,
            so the first valid entry we see means we are done */
