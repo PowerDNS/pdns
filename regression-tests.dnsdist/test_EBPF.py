64bb98d92fb12b2856a7e93dc2607fc19fb70d1f
Remi Gacogne <remi.gacogne@powerdns.com>
2026-05-18 14:48:22 +0200

    dnsdist: Really fix Python indentation
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>

--- regression-tests.dnsdist/test_EBPF.py
+++ regression-tests.dnsdist/test_EBPF.py
@@ -185,6 +185,7 @@ class TestSimpleEBPF(DNSDistTest):
         stats = self.sendConsoleCommand("bpf:getStats()")
         self.assertIn("0.0.0.0: 0", stats)
 
+
 @unittest.skipUnless("ENABLE_SUDO_TESTS" in os.environ, "sudo is not available")
 class TestEBPFRange(DNSDistTest):
     _config_template = """
