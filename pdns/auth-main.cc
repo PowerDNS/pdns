27c388790cb49a11229732ee658c047bcdec9c96
Miod Vallat <miod.vallat@powerdns.com>
2026-05-20 09:42:30 +0200

    Backport "Fix locking and concurrency issues in GSS code."
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>

--- pdns/auth-main.cc
+++ pdns/auth-main.cc
@@ -343,6 +343,7 @@ static void declareArguments()
 
 #ifdef ENABLE_GSS_TSIG
   ::arg().setSwitch("enable-gss-tsig", "Enable GSS TSIG processing") = "no";
+  ::arg().set("gss-max-contexts", "The maximum number of simultaneous GSS contexts allowed") = "1000";
 #endif
 
   ::arg().setSwitch("views", "Enable views (variants) of zones, for backends which support them") = "no";
@@ -730,6 +731,9 @@ static void mainthread()
 #endif
 #ifdef ENABLE_GSS_TSIG
   g_doGssTSIG = ::arg().mustDo("enable-gss-tsig");
+  if (g_doGssTSIG) {
+    GssContext::s_maxGssContexts = ::arg().asNum("gss-max-contexts");
+  }
 #endif
   g_views = ::arg().mustDo("views");
 
