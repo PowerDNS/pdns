eaa7033ab3a98bacbe4226833d3f0c4043e8fd0e
Remi Gacogne <remi.gacogne@powerdns.com>
2026-05-21 11:49:41 +0200

    changelog-from-pr: Do not confuse PR numbers with issues
    Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>

--- build-scripts/changelog-from-pr.py
+++ build-scripts/changelog-from-pr.py
@@ -6,6 +6,15 @@ import argparse
 import re
 import getpass
 
+def isIssue(session, number):
+    try:
+        res = session.get(f"https://api.github.com/repos/PowerDNS/pdns/issues/{number}")
+        issue_info = res.json()
+        # GitHub's REST API considers every pull request an issue, but not every issue is a pull request. For this reason, "Issues" endpoints may return both issues and pull requests in the response. You can identify pull requests by the pull_request key.
+        return not 'pull_request' in issue_info
+    except (requests.exceptions.HTTPError, ValueError):
+        return False
+
 argp = argparse.ArgumentParser()
 argp.add_argument("--oneline", action="store_true", help="Make one-lined changelog entries (for 4.0 and older)")
 argp.add_argument(
@@ -51,7 +60,12 @@ for pr in sorted(arguments.pullrequest):
             print("{}".format(pr_info["message"]))
             sys.exit(1)
         elif body:
-            tickets = re.findall(ticket_regex, body)
+            tickets = []
+            candidateTicketNumbers = re.findall(ticket_regex, body)
+            for ticket in candidateTicketNumbers:
+                if isIssue(session, ticket):
+                    tickets.append(ticket)
+
             if len(tickets):
                 out += "    :tickets: {}\n".format(", ".join(tickets))
         out += "\n    {}".format(pr_info["title"][0].capitalize() + pr_info["title"][1:])
