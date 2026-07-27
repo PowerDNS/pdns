a8e67140306bf76ffa0b6c3a7a986f112ae59988
Ensar Sarajčić <dev@ensarsarajcic.com>
2026-07-27 16:18:34 +0200

    dnsdist: add backend redirections (EDSR)
    This implements [EDSR][EDSR] for backends. This applies both to encrypted
    backends and ones that were upgraded with `autoUpgrade` flag.
    
    [EDSR]: https://datatracker.ietf.org/doc/draft-ietf-add-encrypted-dns-server-redirection/00/
    
    Signed-off-by: Ensar Sarajčić <dev@ensarsarajcic.com>

--- pdns/dnsdistdist/dnsdist-discovery.hh
+++ pdns/dnsdistdist/dnsdist-discovery.hh
@@ -23,6 +23,7 @@
 #include <vector>
 #include <thread>
 
+#include "dnsdist-tcp.hh"
 #include "dnsname.hh"
 #include "dnsdist-protocols.hh"
 #include "iputils.hh"
@@ -37,10 +38,38 @@ class Logger;
 namespace dnsdist
 {
 
+class RedirectionQuerySender : public TCPQuerySender
+{
+public:
+  [[nodiscard]] bool active() const override
+  {
+    return true;
+  }
+
+  void handleResponse([[maybe_unused]] const struct timeval& now, TCPResponse&& response) override
+  {
+    d_response = response.d_buffer;
+  }
+
+  void handleXFRResponse([[maybe_unused]] const struct timeval& now, [[maybe_unused]] TCPResponse&& response) override
+  {
+    handleResponse(now, std::move(response));
+  }
+
+  void notifyIOError([[maybe_unused]] const struct timeval& now, [[maybe_unused]] TCPResponse&& response) override
+  {
+    d_error = true;
+  }
+
+  PacketBuffer d_response;
+  bool d_error{false};
+};
+
 class ServiceDiscovery
 {
 public:
-  static bool addUpgradeableServer(std::shared_ptr<DownstreamState>& server, uint32_t interval, std::string poolAfterUpgrade, uint16_t dohSVCKey, bool keepAfterUpgrade);
+  static bool addUpgradeableServer(std::shared_ptr<DownstreamState>& server, uint32_t interval, std::string poolAfterUpgrade, uint16_t dohSVCKey, bool keepAfterUpgrade, bool enableRedirection, uint32_t redirectInterval, std::string poolAfterRedirect, uint16_t redirectDohSVCKey, bool keepAfterRedirect, uint32_t redirectMaxFollowCount);
+  static bool addRedirectableServer(std::shared_ptr<DownstreamState>& server, uint32_t interval, std::string poolAfterUpgrade, uint16_t dohSVCKey, bool keepAfterRedirect, uint32_t maxFollowCount);
 
   /* starts a background thread if needed */
   static bool run();
@@ -52,6 +81,12 @@ public:
     std::string d_dohPath;
     uint16_t d_port{0};
     dnsdist::Protocol d_protocol;
+    uint32_t d_ttl;
+
+    bool operator==(const DiscoveredResolverConfig& rhs) const
+    {
+      return d_addr == rhs.d_addr && d_subjectName == rhs.d_subjectName && d_dohPath == rhs.d_dohPath && d_port == rhs.d_port && d_protocol == rhs.d_protocol && d_ttl == rhs.d_ttl;
+    }
   };
 
   static const uint16_t s_defaultDoHSVCKey;
@@ -68,15 +103,53 @@ private:
     uint32_t d_interval;
     uint16_t d_dohKey;
     bool keepAfterUpgrade;
+    bool d_enableRedirect;
+    std::string d_poolAfterRedirect;
+    uint32_t d_redirectInterval;
+    uint16_t d_redirectDohKey;
+    bool d_keepAfterRedirect;
+    uint32_t d_redirectMaxFollowCount;
   };
 
-  static bool getDiscoveredConfig(const Logr::Logger& logger, const UpgradeableBackend& backend, DiscoveredResolverConfig& config);
-  static bool tryToUpgradeBackend(const Logr::Logger& logger, const UpgradeableBackend& backend);
+  struct RedirectableBackend
+  {
+    std::shared_ptr<DownstreamState> d_origDs;
+    std::shared_ptr<DownstreamState> d_currentDs;
+    std::string d_poolAfterRedirect;
+    time_t d_nextCheck;
+    uint32_t d_interval;
+    uint16_t d_dohKey;
+    bool d_keepAfterRedirect;
+    uint32_t d_maxFollowCount;
+  };
+
+  static bool getDiscoveredConfig(const Logr::Logger& logger, const std::shared_ptr<DownstreamState>& backend, uint16_t dohKey, DiscoveredResolverConfig& config);
+  static bool tryToUpgradeBackend(const Logr::Logger& logger, const UpgradeableBackend& backend, std::shared_ptr<DownstreamState>& upgradedBackend);
+  static bool tryToRedirectBackend(const Logr::Logger& logger, RedirectableBackend& backend, uint32_t& ttl);
 
   static void worker();
 
   static LockGuarded<std::vector<std::shared_ptr<UpgradeableBackend>>> s_upgradeableBackends;
+  static LockGuarded<std::vector<std::shared_ptr<ServiceDiscovery::RedirectableBackend>>> s_redirectableBackends;
   static std::thread s_thread;
 };
 
+}
+
+namespace std
+{
+template <>
+struct hash<dnsdist::ServiceDiscovery::DiscoveredResolverConfig>
+{
+  auto operator()(const dnsdist::ServiceDiscovery::DiscoveredResolverConfig& config) const
+  {
+    size_t currentHash = ComboAddress::addressOnlyHash{}(config.d_addr);
+    boost::hash_combine(currentHash, std::hash<std::string>{}(config.d_subjectName));
+    boost::hash_combine(currentHash, std::hash<std::string>{}(config.d_dohPath));
+    boost::hash_combine(currentHash, std::hash<uint16_t>{}(config.d_port));
+    boost::hash_combine(currentHash, std::hash<uint8_t>{}(config.d_protocol.toNumber()));
+    boost::hash_combine(currentHash, std::hash<uint32_t>{}(config.d_ttl));
+    return currentHash;
+  }
+};
 }
