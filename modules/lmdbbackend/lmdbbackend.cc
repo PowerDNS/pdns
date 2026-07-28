3b7ca83eb98e6ac28253ff500bc76e000f006961
Miod Vallat <miod.vallat@powerdns.com>
2026-07-28 08:35:02 +0200

    Replace use of startTransaction with the appropriate startDomain*Transaction.
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>

--- modules/lmdbbackend/lmdbbackend.cc
+++ modules/lmdbbackend/lmdbbackend.cc
@@ -1999,6 +1999,7 @@ bool LMDBBackend::deleteDomain(const ZoneName& domain)
 
   for (auto id : idvec) {
 
+    // NOLINTNEXTLINE(bugprone-narrowing-conversions,cppcoreguidelines-narrowing-conversions)
     startDomainCreationTransaction(domain, id);
 
     { // Remove metadata
@@ -2039,8 +2040,12 @@ bool LMDBBackend::deleteDomain(const ZoneName& domain)
     txn.commit();
   }
 
-  // Use startTransaction() here as transactionDomainId might be UnknownDomainID
-  startTransaction(transactionDomain, transactionDomainId);
+  if (transactionDomainId == UnknownDomainID) {
+    startDomainModificationTransaction(transactionDomain);
+  }
+  else {
+    startDomainCreationTransaction(transactionDomain, transactionDomainId);
+  }
 
   return true;
 }
