4f954a68ed64208eebb67aee48aa624fc28ce289
Pieter Lexis <pieter.lexis@powerdns.com>
2026-09-01 10:02:35 +0200

    fix(auth): stop using getpid in the FD count fallback code
    (cherry picked from commit 34aa8241cb999419da5f2264e84c4fca0285025a)

--- pdns/misc.cc
+++ pdns/misc.cc
@@ -1397,8 +1397,9 @@ DNSName getTSIGAlgoName(TSIGHashEnum& algoEnum)
 uint64_t getOpenFileDescriptors(const std::string&)
 {
 #ifdef __linux__
+  const auto* const dirName = "/proc/self/fd";
   struct stat status; // NOLINT(cppcoreguidelines-pro-type-member-init)
-  auto ret = stat("/proc/self/fd", &status);
+  auto ret = stat(dirName, &status);
   if (ret != 0) {
     return 0;
   }
@@ -1408,7 +1409,6 @@ uint64_t getOpenFileDescriptors(const std::string&)
 
   // This can lead to performance issues with *many* open fds
   uint64_t nbFileDescriptors = 0;
-  const auto dirName = "/proc/" + std::to_string(getpid()) + "/fd/";
   auto directoryError = pdns::visit_directory(dirName, [&nbFileDescriptors]([[maybe_unused]] ino_t inodeNumber, const std::string_view& name) {
     uint32_t num{};
     try {
