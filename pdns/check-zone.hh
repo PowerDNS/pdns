a52eac8718ba7c0e87c6702f38ac679982d2b7ae
Miod Vallat <miod.vallat@powerdns.com>
2026-09-10 18:49:51 +0200

    Move zone checking code from pdnsutil to the common checking code.
    This is purely mechanical and there are no other changes in there.
    
    Signed-off-by: Miod Vallat <miod.vallat@powerdns.com>

--- pdns/check-zone.hh
+++ pdns/check-zone.hh
@@ -64,4 +64,9 @@ enum RRSetFlags : unsigned int
 // thus will not perform checks which require the complete zone knowledge.
 void checkRRSet(const std::vector<DNSResourceRecord>& oldrrs, std::vector<DNSResourceRecord>& newrrs, const ZoneName& zone, RRSetFlags flags, std::vector<diag>& diagnostics);
 
+// Check a complete zone contents and metadata.
+// NOTE: sorts records [allrrs] in in-place due to the invocation of checkRRSet
+// above.
+void checkZone(Logr::log_t slog, std::vector<DNSResourceRecord>& allrrs, const ZoneName& zone, RRSetFlags flags, std::vector<diag>& diagnostics);
+
 } // namespace Check
