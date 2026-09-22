PowerDNS/pdns

commit a6e3d23f379d1bc0a59456f6bfb982cb48e3fb98

dnsdist: Gracefully handle errors in DynBlock's suffix match policy
There is no need to stop the whole processing if we choked on one entry,
or even could not properly insert one new rule.

Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>
(cherry picked from commit 54649c58c3948ef5a3b128f333745c357e00043e)

pdns/dnsdistdist/dnsdist-dynblocks.cc
@@ -130,26 +130,31 @@ void DynBlockRulesGroup::applySMT(const struct timespec& now, StatNode& statNode
   StatNode::Stat node;
   std::unordered_map<DNSName, SMTBlockParameters> namesToBlock;
   statNodeRoot.visit([this, &namesToBlock](const StatNode* node_, const StatNode::Stat& self, const StatNode::Stat& children) {
-    bool block = false;
-    SMTBlockParameters blockParameters;
-    if (d_smtVisitorFFI) {
-      dnsdist_ffi_stat_node_t tmp(*node_, self, children, blockParameters);
-      block = d_smtVisitorFFI(&tmp);
-    }
-    else {
-      auto ret = d_smtVisitor(*node_, self, children);
-      block = std::get<0>(ret);
-      if (block) {
-        if (boost::optional<std::string> tmp = std::get<1>(ret)) {
-          blockParameters.d_reason = std::move(*tmp);
+    try {
+      bool block = false;
+      SMTBlockParameters blockParameters;
+      if (d_smtVisitorFFI) {
+        dnsdist_ffi_stat_node_t tmp(*node_, self, children, blockParameters);
+        block = d_smtVisitorFFI(&tmp);
+      }
+      else {
+        auto ret = d_smtVisitor(*node_, self, children);
+        block = std::get<0>(ret);
+        if (block) {
+          if (boost::optional<std::string> tmp = std::get<1>(ret)) {
+            blockParameters.d_reason = std::move(*tmp);
+          }
+          if (boost::optional<int> tmp = std::get<2>(ret)) {
+            blockParameters.d_action = static_cast<DNSAction::Action>(*tmp);
+          }
         }
-        if (boost::optional<int> tmp = std::get<2>(ret)) {
-          blockParameters.d_action = static_cast<DNSAction::Action>(*tmp);
+        if (block) {
+          namesToBlock.insert({DNSName(node_->fullname), std::move(blockParameters)});
         }
       }
     }
-    if (block) {
-      namesToBlock.insert({DNSName(node_->fullname), std::move(blockParameters)});
+    catch (const std::exception& exp) {
+      warnlog("Error while executing the Dynamic Block Suffix Match policy: %s", exp.what());
     }
   },
                      node);
@@ -480,7 +485,12 @@ void DynBlockRulesGroup::processResponseRules(counts_t& counts, StatNode& root, 
 
       if (suffixMatchRuleMatches) {
         const bool hit = ringEntry.isACacheHit();
-        root.submit(ringEntry.name, ((ringEntry.dh.rcode == 0 && ringEntry.usec == std::numeric_limits<unsigned int>::max()) ? -1 : ringEntry.dh.rcode), ringEntry.size, hit, std::nullopt);
+        try {
+          root.submit(ringEntry.name, ((ringEntry.dh.rcode == 0 && ringEntry.usec == std::numeric_limits<unsigned int>::max()) ? -1 : ringEntry.dh.rcode), ringEntry.size, hit, std::nullopt);
+        }
+        catch (const std::exception& exp) {
+          warnlog("Error submitting name %s to Dynamic Block Suffix Match Rule policy: %s", ringEntry.name, exp.what());
+        }
       }
     }
   }

commit 20abb5f5e801b7f66f34f337f1e1f86dee14c62d

dnsdist-2.0: Fix dynamically blocking a suffix via the FFI interface
This has been broken since a6e3d23f379d1bc0a59456f6bfb982cb48e3fb98.

Signed-off-by: Remi Gacogne <remi.gacogne@powerdns.com>

pdns/dnsdistdist/dnsdist-dynblocks.cc
@@ -148,9 +148,9 @@ void DynBlockRulesGroup::applySMT(const struct timespec& now, StatNode& statNode
             blockParameters.d_action = static_cast<DNSAction::Action>(*tmp);
           }
         }
-        if (block) {
-          namesToBlock.insert({DNSName(node_->fullname), std::move(blockParameters)});
-        }
+      }
+      if (block) {
+        namesToBlock.insert({DNSName(node_->fullname), std::move(blockParameters)});
       }
     }
     catch (const std::exception& exp) {